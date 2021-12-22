7a6d71bd40a7b436a5eb070952ae872b1255b3c4
petterwildhagen <pewi@equinor.com>
2021-12-22 12:05:17 +0100

    Add authentication through frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react'
 import './styles.css'
-
+import { loginRequest } from './authConfig'
 import SideMenu from './Components/SideMenu/SideMenu'
 import Header from './Components/Header'
 
+const ProfileContent = () => {
+    const { instance, accounts } = useMsal()
+    const [graphData, setGraphData] = useState(null)
+
+    /*  function RequestProfileData() { */
+    // Silently acquires an access token which is then attached to a request for MS Graph data
+    instance
+        .acquireTokenSilent({
+            ...loginRequest,
+            account: accounts[0],
+        })
+        .then(response => {
+            console.log(response)
+            //callMsGraph(response.accessToken).then(response => setGraphData(response));
+        })
+        .catch(err => {
+            console.log('Error')
+            console.log(err)
+            // handle error
+        })
+    /*  } */
+
+    return (
+        <>
+            <h5 className="card-title">Welcome to DCD {accounts[0].name}!</h5>
+            <div className="App" style={{ display: 'flex', flexDirection: 'column', height: '100vh', width: '100vw' }}>
+                <Header />
+                <div style={{ display: 'flex', flexDirection: 'row', flexGrow: 1 }}>
+                    <SideMenu />
+                    <Outlet />
+                </div>
+            </div>
+        </>
+    )
+}
+
 function App() {
     return (
-        <div className="App" style={{ display: 'flex', flexDirection: 'column', height: '100vh', width: '100vw' }}>
-            <Header />
-            <div style={{ display: 'flex', flexDirection: 'row', flexGrow: 1 }}>
-                <SideMenu />
-                <Outlet />
-            </div>
+        <div className="App">
+            <AuthenticatedTemplate>
+                <ProfileContent />
+            </AuthenticatedTemplate>
+
+            <UnauthenticatedTemplate>
+                <h5 className="card-title">Please sign-in to DCD.</h5>
+            </UnauthenticatedTemplate>
         </div>
     )
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,17 +6,25 @@ import DashboardView from './Views/DashboardView'
 import ProjectView from './Views/ProjectView'
 import CaseView from './Views/CaseView'
 
+import { PublicClientApplication } from '@azure/msal-browser'
+import { MsalProvider } from '@azure/msal-react'
+import { msalConfig } from './authConfig'
+
+const msalInstance = new PublicClientApplication(msalConfig)
+
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />}>
-                    <Route index element={<DashboardView />} />
-                    <Route path="project/:projectId" element={<ProjectView />} />
-                    <Route path="project/:projectId/case/:caseId" element={<CaseView />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <MsalProvider instance={msalInstance}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />}>
+                        <Route index element={<DashboardView />} />
+                        <Route path="project/:projectId" element={<ProjectView />} />
+                        <Route path="project/:projectId/case/:caseId" element={<CaseView />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </MsalProvider>
     </React.StrictMode>,
     document.getElementById('root')
 )
