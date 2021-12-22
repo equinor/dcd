d1f85bae425968f32e9086800ce70e9e30e2fff3
petterwildhagen <pewi@equinor.com>
2021-12-22 13:49:38 +0100

    Add support/visit.ts and support/index.ts (#33)

---
+++ frontend/cypress/support/commands/visit.ts
@@ -0,0 +1,18 @@
+Cypress.Commands.add('visitApplication', () => {
+    const frontendUrl = Cypress.env('FRONTEND_URL') || 'http://localhost:3000'
+
+    cy.visit(`${frontendUrl}`)
+})
+
+export {}
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            /**
+             * Visit application
+             * @example cy.visitApplication(user)
+             */
+            visitApplication(): Chainable
+        }
+    }
+}
