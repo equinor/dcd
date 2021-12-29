ad177cfff1284b17fae1e4af67420d7624a7683c
Maia Tømmerbakk <37103233+maiaht@users.noreply.github.com>
2021-12-29 10:26:59 +0100

    Use app config client in frontend (#42)
    * Use app config client in frontend
    
    * Rename client -> appConfigClient
    
    Co-authored-by: Kristian Reed <krr@equinor.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
-import './styles.css'
+import { AppConfigurationClient } from '@azure/app-configuration'
 
+import './styles.css'
 import SideMenu from './Components/SideMenu/SideMenu'
 import Header from './Components/Header'
 
 function App() {
+    const connection_string = process.env.REACT_APP_AZURE_APP_CONFIG_CONNECTION_STRING
+    const appConfigClient = new AppConfigurationClient(connection_string!)
+
+    useEffect(() => {
+        appConfigClient.getConfigurationSetting({ key: 'PoC2' }).then(response => {
+            console.log(response.value)
+        })
+    })
+
     return (
         <div className="App" style={{ display: 'flex', flexDirection: 'column', height: '100vh', width: '100vw' }}>
             <Header />
