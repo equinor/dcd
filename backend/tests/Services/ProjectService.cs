3a54722456979ac5bea0c3dfe4fe8aba76558c9b
petterwildhagen <pewi@equinor.com>
2022-01-13 14:02:19 +0100

    Create unit test for project service

--- backend/tests/Services/ProjectService.cs
+++ backend/tests/Services/ProjectService.cs
@@ -22,10 +22,15 @@ namespace tests
         [Fact]
         public void RetrieveAllProjects()
         {
+            var projects = TestDataGenerator.initialize().Projects.OrderBy(p => p.ProjectName);
             ProjectService projectService = new ProjectService(fixture.context);
-            IEnumerable<Project> projects = projectService.GetAll();
-
-            Assert.True(projects.Count() > 0);
+            var projectsFromService = projectService.GetAll().OrderBy(p => p.ProjectName);
+            var projectsSourceAndTarget = projects.Zip(projectsFromService);
+            Assert.Equal(projects.Count(), projectsFromService.Count());
+            foreach (var projectPair in projectsSourceAndTarget)
+            {
+                compareProject(projectPair.First, projectPair.Second);
+            }
         }
 
         [Fact]
@@ -37,9 +42,80 @@ namespace tests
 
             Project projectGotten = projectService.GetProject(projectExsists.Id);
 
-            Assert.Equal(projectExsists, projectGotten);
+            compareProject(projectExsists, projectGotten);
+        }
+        void compareProject(Project x, Project y)
+        {
+            Assert.Equal(x.ProjectName, y.ProjectName);
+            Assert.Equal(x.ProjectPhase, y.ProjectPhase);
+            Assert.Equal(x.Cases.Count(), y.Cases.Count());
+            var casesSourceAndTarget = x.Cases.OrderBy(c => c.Name).Zip(y.Cases.OrderBy(c => c.Name));
+            foreach (var casePair in casesSourceAndTarget)
+            {
+                compareCase(casePair.First, casePair.Second);
+            }
         }
 
+        void compareCase(Case x, Case y)
+        {
+            Assert.Equal(x.Name, y.Name);
+            Assert.Equal(x.Description, y.Description);
+            Assert.Equal(x.ReferenceCase, y.ReferenceCase);
+            Assert.Equal(x.ProducerCount, y.ProducerCount);
+            Assert.Equal(x.GasInjectorCount, y.GasInjectorCount);
+            Assert.Equal(x.WaterInjectorCount, y.WaterInjectorCount);
+            Assert.Equal(x.RiserCount, y.RiserCount);
+            Assert.Equal(x.TemplateCount, y.TemplateCount);
+            Assert.Equal(x.FacilitiesAvailability, y.FacilitiesAvailability);
+            Assert.Equal(x.ArtificialLift, y.ArtificialLift);
+            compareCosts(x.CessationCost, y.CessationCost);
+            compareDrainageStrategy(x.DrainageStrategy, y.DrainageStrategy);
+        }
+
+        void compareCosts<T>(TimeSeriesCost<T> x, TimeSeriesCost<T> y)
+        {
+            if (x == null || y == null)
+            {
+                Assert.Equal(x, null);
+                Assert.Equal(y, null);
+            }
+            else
+            {
+                compareYearValues(x, y);
+                Assert.Equal(x.Currency, y.Currency);
+            }
+        }
+        void compareDrainageStrategy(DrainageStrategy x, DrainageStrategy y)
+        {
+            if (x == null || y == null)
+            {
+                Assert.Equal(x, null);
+                Assert.Equal(y, null);
+            }
+            else
+            {
+                Assert.Equal(x.NGLYield, y.NGLYield);
+                compareYearValues(x.ProductionProfileOil, y.ProductionProfileOil);
+                compareYearValues(x.ProductionProfileGas, y.ProductionProfileGas);
+            }
+        }
+        void compareYearValues<T>(TimeSeriesBase<T> x, TimeSeriesBase<T> y)
+        {
+            if (x == null || y == null)
+            {
+                Assert.Equal(x, null);
+                Assert.Equal(y, null);
+            }
+            else
+            {
+                var z = x.YearValues.OrderBy(v => v.Year).Zip(y.YearValues.OrderBy(v => v.Year));
+                foreach (var pair in z)
+                {
+                    Assert.Equal(pair.First.Year, pair.Second.Year);
+                    Assert.Equal(pair.First.Value, pair.Second.Value);
+                }
+            }
+        }
         [Fact]
         public void GetDoesNotExist()
         {
