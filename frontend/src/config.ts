d359e81cb32cb5b89e9c3134cfab0181d25a2f98
Kristian Reed <krr@equinor.com>
2022-01-26 14:43:26 +0100

    Add redirect URI as variable (#148)

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,7 @@
 export const AzureAd = {
     clientId: '9b125a0c-4907-43b9-8db2-ff405d6b0524',
     authority: 'https://login.microsoftonline.com/3aa4a235-b6e2-48d5-9195-7fcf05b459b0',
-    redirectUri: 'https://ase-dcd-frontend-dev.azurewebsites.net',
+    redirectUri: process.env.REACT_APP_REDIRECT_URI,
 }
 
 export const appInsightsInstrumentationKey = 'f218caa9-c1a4-4c31-973b-c787a90af4ce'
