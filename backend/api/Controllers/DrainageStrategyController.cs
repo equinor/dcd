2918a3753056a07629cabea9c61d01db45c3086d
ericnorway <ericgeneral76@gmail.com>
2022-01-27 10:07:33 +0100

    wip

--- backend/api/Controllers/DrainageStrategyController.cs
+++ backend/api/Controllers/DrainageStrategyController.cs
@@ -1,3 +1,5 @@
+using api.Adapters;
+using api.Dtos;
 using api.Models;
 using api.Services;
 
@@ -15,11 +17,13 @@ namespace api.Controllers
     {
         private DrainageStrategyService _drainageStrategyService;
         private readonly ILogger<DrainageStrategyController> _logger;
+        private readonly DrainageStrategyAdapter _drainageStrategyAdapter;
 
-        public DrainageStrategyController(ILogger<DrainageStrategyController> logger, DrainageStrategyService drainageStrategyService)
+        public DrainageStrategyController(ILogger<DrainageStrategyController> logger, DrainageStrategyService drainageStrategyService, ProjectService projectService)
         {
             _logger = logger;
             _drainageStrategyService = drainageStrategyService;
+            _drainageStrategyAdapter = new DrainageStrategyAdapter(projectService);
         }
 
         [HttpGet("Project/{drainageStrategyId}", Name = "GetDrainageStrategy")]
@@ -41,8 +45,9 @@ namespace api.Controllers
         }
 
         [HttpPost(Name = "CreateDrainageStrategy")]
-        public DrainageStrategy CreateDrainageStrategy([FromBody] DrainageStrategy drainageStrategy)
+        public DrainageStrategy CreateDrainageStrategy([FromBody] DrainageStrategyDto drainageStrategyDto)
         {
+            var drainageStrategy = _drainageStrategyAdapter.Convert(drainageStrategyDto);
             return _drainageStrategyService.CreateDrainageStrategy(drainageStrategy);
         }
     }
