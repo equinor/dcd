3539fbc176f55af2131f72df0434619ab6643246
Valentin Krasontovitsch <vkra@equinor.com>
2022-01-27 14:50:44 +0100

    Add create exploration action

--- backend/api/Controllers/ExplorationController.cs
+++ backend/api/Controllers/ExplorationController.cs
@@ -14,18 +14,29 @@ namespace api.Controllers
     public class ExplorationController : ControllerBase
     {
         private ExplorationService _explorationService;
+        private ProjectService _projectService;
         private readonly ILogger<ExplorationController> _logger;
 
-        public ExplorationController(ILogger<ExplorationController> logger, ExplorationService explorationProjectService)
+        public ExplorationController(ILogger<ExplorationController> logger,
+                ExplorationService explorationProjectService,
+                ProjectService projectService)
         {
             _logger = logger;
             _explorationService = explorationProjectService;
+            _projectService = projectService;
         }
 
         [HttpGet("{projectId}", Name = "GetExplorations")]
         public IEnumerable<Exploration> GetExplorations(Guid projectId)
         {
             return _explorationService.GetExplorations(projectId);
+        }
+
+        [HttpPost(Name = "CreateExploration")]
+        public Exploration CreateExploration([FromBody] Exploration
+                exploration)
+        {
+            return _explorationService.CreateExploration(exploration);
         }
     }
 }
