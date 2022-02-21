8c0b968b5720c9f698429b022ac1ea910a3ce910
Aron Høyer <aron@hoyer.com>
2022-02-21 11:12:34 +0100

    Updated project view

--- frontend/src/Views/ProjectView.tsx
+++ frontend/src/Views/ProjectView.tsx
@@ -1,12 +1,15 @@
-import { Typography } from '@equinor/eds-core-react'
-import { useEffect, useState } from 'react'
+import { add, delete_to_trash, edit } from "@equinor/eds-icons"
+import { Button, EdsProvider, Icon, Tooltip, Typography } from '@equinor/eds-core-react'
+import { useEffect, useMemo, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 
-import CasesTable from '../Components/CasesTable/CasesTable'
 import BarChart from '../Components/BarChart'
-import { projectService } from '../Services/ProjectService'
-import { GetDrainageStrategy, StoreRecentProject } from '../Utils/common'
+
+import { Project } from '../models/Project'
+import { ProjectService } from '../Services/ProjectService'
+
+import { StoreRecentProject } from '../Utils/common'
 
 const Wrapper = styled.div`
     margin: 2rem;
@@ -14,59 +17,81 @@ const Wrapper = styled.div`
     flex-direction: column;
 `
 
-const OverviewHeader = styled(Typography)`
-    margin-bottom: 2rem;
+const Header = styled.header`
+    display: flex;
+    align-items: center;
+
+    > *:first-child {
+        margin-right: 2rem;
+    }
 `
 
-const CasesHeader = styled(Typography)`
-    margin-bottom: 1rem;
+const ActionsContainer = styled.div`
+    > *:not(:last-child) {
+        margin-right: 0.5rem;
+    }
 `
 
-const Charts = styled.div`
+const ChartsContainer = styled.div`
     display: flex;
 `
 
 const ProjectView = () => {
     let params = useParams()
-    const [project, setProject] = useState<Components.Schemas.ProjectDto>()
+    const [project, setProject] = useState<Project>()
 
     useEffect(() => {
-        if (projectService) {
-            (async () => {
-                try {
-                    const res = await projectService.getProjectByID(params.projectId!)
-                    setProject(res)
-                } catch (error) {
-                    console.error(`[ProjectView] Error while fetching project ${params.projectId}`, error)
-                }
-            })()
-        }
+        (async () => {
+            try {
+                const res = await ProjectService.getProjectByID(params.projectId!)
+                console.log('[ProjectView]', res)
+                setProject(res)
+            } catch (error) {
+                console.error(`[ProjectView] Error while fetching project ${params.projectId}`, error)
+            }
+        })()
     }, [])
+
+    const chartData = useMemo(() => {
+        return project ? {
+            x: project?.cases.map((c) => c.name),
+            y: project?.cases.map((c) => c.capex),
+        } : { x: [], y: [] }
+    }, [project])
 
     if (!project) return null
 
-    StoreRecentProject(project.projectId!)
-
-    const dataX: string[] = []
-    let dataProdProfileGas: number[] = []
-    let dataProdProfileOil: number[] = []
-
-    project.cases?.forEach(c => {
-        const drainageStrategy = GetDrainageStrategy(project, c.drainageStrategyLink);
-        dataProdProfileGas = drainageStrategy?.productionProfileGas?.values?.map(v => v)!
-        dataProdProfileOil = drainageStrategy?.productionProfileOil?.values?.map(v => v)!
-        dataX.push(c.name!)
-    })
+    StoreRecentProject(project.id!)
 
     return (
         <Wrapper>
-            <OverviewHeader variant="h2">{project.name} - Overview</OverviewHeader>
-            <Charts>
-                <BarChart data={{ x: dataX, y: dataProdProfileGas }} title="Production profile gas" />
-                <BarChart data={{ x: dataX, y: dataProdProfileOil }} title="Production profile oil" />
-            </Charts>
-            <CasesHeader variant="h3">Cases</CasesHeader>
-            <CasesTable key={project.projectId} project={project} cases={project.cases!} />
+            <Header>
+                <Typography variant="h2">{project.name}</Typography>
+
+                <EdsProvider density="compact">
+                    <ActionsContainer>
+                        <Tooltip title={`Edit ${project.name}`}>
+                            <Button variant="ghost_icon" aria-label={`Edit ${project.name}`}>
+                                <Icon data={edit} />
+                            </Button>
+                        </Tooltip>
+                        <Tooltip title="Add a case">
+                            <Button variant="ghost_icon" aria-label="Add a case">
+                                <Icon data={add} />
+                            </Button>
+                        </Tooltip>
+                        <Tooltip title={`Delete ${project.name}`}>
+                            <Button variant="ghost_icon" color="danger" aria-label={`Delete ${project.name}`}>
+                                <Icon data={delete_to_trash} />
+                            </Button>
+                        </Tooltip>
+                    </ActionsContainer>
+                </EdsProvider>
+            </Header>
+
+            <ChartsContainer>
+                <BarChart data={chartData!} title="Capex / case" />
+            </ChartsContainer>
         </Wrapper>
     )
 }
