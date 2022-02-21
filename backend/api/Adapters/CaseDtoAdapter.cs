1906f5e8f9ee7bfa20c50b6424a679375951715c
petterwildhagen <pewi@equinor.com>
2022-02-21 10:44:32 +0100

    Only use Dtos for calculation

--- backend/api/Adapters/CaseDtoAdapter.cs
+++ backend/api/Adapters/CaseDtoAdapter.cs
@@ -27,38 +27,5 @@ namespace api.Adapters
 
             return caseDto;
         }
-
-        public static void AddCapexToCases(ICollection<CaseDto> caseDtos, WellProjectService wellProjectService, SubstructureService substructureService,
-        SurfService surfService, TopsideService topsideService, TransportService transportService, ExplorationService explorationService)
-        {
-            foreach (CaseDto c in caseDtos)
-            {
-                c.Capex = 0;
-                if (c.WellProjectLink != Guid.Empty)
-                {
-                    c.Capex += wellProjectService.GetWellProject(c.WellProjectLink).CostProfile.Sum;
-                }
-                if (c.SubstructureLink != Guid.Empty)
-                {
-                    c.Capex += substructureService.GetSubstructure(c.SubstructureLink).CostProfile.Sum;
-                }
-                if (c.SurfLink != Guid.Empty)
-                {
-                    c.Capex += surfService.GetSurf(c.SurfLink).CostProfile.Sum;
-                }
-                if (c.TopsideLink != Guid.Empty)
-                {
-                    c.Capex += topsideService.GetTopside(c.TopsideLink).CostProfile.Sum;
-                }
-                if (c.TransportLink != Guid.Empty)
-                {
-                    c.Capex += transportService.GetTransport(c.TransportLink).CostProfile.Sum;
-                }
-                if (c.ExplorationLink != Guid.Empty)
-                {
-                    c.Capex += explorationService.GetExploration(c.ExplorationLink).CostProfile.Sum;
-                }
-            }
-        }
     }
 }
