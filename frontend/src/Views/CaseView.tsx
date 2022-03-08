b4210f0edc554d43cc561b13fbfef18f43e8bf31
BOHME Daniel <daniel.boehme@soprasteria.com>
2022-03-08 11:26:52 +0100

    Case 363 and 365

--- frontend/src/Views/CaseView.tsx
+++ frontend/src/Views/CaseView.tsx
@@ -1,17 +1,30 @@
-import { Tabs, Typography } from "@equinor/eds-core-react"
-import { useEffect, useState } from "react"
+import {
+    Tabs,
+    Typography,
+    Button,
+    EdsProvider,
+    TextField,
+    Tooltip,
+    NativeSelect,
+    Icon,
+} from "@equinor/eds-core-react"
+import {
+    useEffect,
+    useState,
+    ChangeEventHandler,
+    MouseEventHandler,
+} from "react"
 import { useParams } from "react-router-dom"
 import styled from "styled-components"
-
+import { add, link } from "@equinor/eds-icons"
 import { Project } from "../models/Project"
 import { Case } from "../models/Case"
-import DrainageStrategyView from "./DrainageStrategyView"
-import ExplorationView from "./ExplorationView"
-import OverviewView from "./OverviewView"
 import { ProjectService } from "../Services/ProjectService"
+import DescriptionView from "./DescriptionView"
+import { Modal } from "../Components/Modal"
 
 const {
-    List, Tab, Panels, Panel,
+    Panels, Panel,
 } = Tabs
 
 const CaseViewDiv = styled.div`
@@ -22,6 +35,29 @@ const CaseViewDiv = styled.div`
 
 const CaseHeader = styled(Typography)`
     margin-bottom: 2rem;
+    display: flex;
+
+    > *:first-child {
+        margin-right: 2rem;
+    }
+`
+
+const ActionsContainer = styled.div`
+    > *:not(:last-child) {
+        margin-right: 0.5rem;
+    }
+`
+
+const CreateAssetForm = styled.form`
+    width: 30rem;
+
+    > * {
+        margin-bottom: 1.5rem;
+    }
+`
+
+const AssetDropdown = styled(NativeSelect)`
+    width: 30rem;
 `
 
 function CaseView() {
@@ -29,6 +65,10 @@ function CaseView() {
     const [caseItem, setCase] = useState<Case>()
     const [activeTab, setActiveTab] = useState<number>(0)
     const params = useParams()
+    const [createAssetModalIsOpen, setCreateAssetModalIsOpen] = useState<boolean>(false)
+    const [createAssetFormData, setCreateAssetFormData] = useState<Record<string, any>>({})
+    const [submitIsDisabled, setSubmitIsDisabled] = useState<boolean>(false)
+    const [linkAssetModalIsOpen, setLinkAssetModalIsOpen] = useState<boolean>(false)
 
     useEffect(() => {
         (async () => {
@@ -47,37 +87,146 @@ function CaseView() {
         setActiveTab(index)
     }
 
+    const toggleCreateAssetModal = () => setCreateAssetModalIsOpen(!createAssetModalIsOpen)
+
+    const toggleLinkAssetModal = () => setLinkAssetModalIsOpen(!linkAssetModalIsOpen)
+
+    const handleCreateAssetFormFieldChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setCreateAssetFormData({
+            ...createAssetFormData,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const submitCreateAssetForm: MouseEventHandler<HTMLButtonElement> = async (e) => {
+        e.preventDefault()
+        setSubmitIsDisabled(true)
+
+        try {
+            setSubmitIsDisabled(false)
+            toggleCreateAssetModal()
+        } catch (error) {
+            setSubmitIsDisabled(false)
+            console.error("[ProjectView] error while submitting form data", error)
+        }
+    }
+
+    const submitLinkAssetForm: MouseEventHandler<HTMLButtonElement> = async (e) => {
+        e.preventDefault()
+        setSubmitIsDisabled(true)
+
+        try {
+            setSubmitIsDisabled(false)
+            toggleLinkAssetModal()
+        } catch (error) {
+            setSubmitIsDisabled(false)
+            console.error("[ProjectView] error while submitting form data", error)
+        }
+    }
+
     if (!project) return null
 
     return (
         <CaseViewDiv>
-            <CaseHeader variant="h2">
-                {project.name}
-                {" "}
-                -
-                {caseItem?.name}
+            <CaseHeader>
+                <Typography variant="h2">{caseItem?.name}</Typography>
+                <EdsProvider density="compact">
+                    <ActionsContainer>
+                        <Tooltip title="Create an asset">
+                            <Button variant="ghost_icon" aria-label="Create an asset" onClick={toggleCreateAssetModal}>
+                                <Icon data={add} />
+                            </Button>
+                        </Tooltip>
+                        <Tooltip title="Link to asset">
+                            <Button variant="ghost_icon" aria-label="Link to asset" onClick={toggleLinkAssetModal}>
+                                <Icon data={link} />
+                            </Button>
+                        </Tooltip>
+                    </ActionsContainer>
+                </EdsProvider>
             </CaseHeader>
+            <Modal isOpen={createAssetModalIsOpen} title="Create an asset" shards={[]}>
+                <CreateAssetForm>
+                    <AssetDropdown
+                        label="Asset type"
+                        id="asset"
+                        name="asset"
+                        placeholder="Choose an asset"
+                    />
+
+                    <TextField
+                        label="Name"
+                        id="name"
+                        name="name"
+                        placeholder="Name"
+                        onChange={handleCreateAssetFormFieldChange}
+                    />
+
+                    <div>
+                        <Button
+                            type="submit"
+                            onClick={submitCreateAssetForm}
+                            disabled={submitIsDisabled}
+                        >
+                            Create asset
+                        </Button>
+                        <Button
+                            type="button"
+                            color="secondary"
+                            variant="ghost"
+                            onClick={toggleCreateAssetModal}
+                        >
+                            Cancel
+                        </Button>
+                    </div>
+                </CreateAssetForm>
+            </Modal>
+            <Modal isOpen={linkAssetModalIsOpen} title="Link to asset" shards={[]}>
+                <CreateAssetForm>
+                    <AssetDropdown
+                        label="Asset type"
+                        id="asset"
+                        name="asset"
+                        placeholder="Choose an asset"
+                    />
+
+                    <AssetDropdown
+                        label="Name"
+                        id="name"
+                        name="name"
+                        placeholder="Name"
+                    />
+
+                    <div>
+                        <Button
+                            type="submit"
+                            onClick={submitLinkAssetForm}
+                            disabled={submitIsDisabled}
+                        >
+                            Link to asset
+                        </Button>
+                        <Button
+                            type="button"
+                            color="secondary"
+                            variant="ghost"
+                            onClick={toggleLinkAssetModal}
+                        >
+                            Cancel
+                        </Button>
+                    </div>
+                </CreateAssetForm>
+            </Modal>
             <Tabs activeTab={activeTab} onChange={handleTabChange}>
-                <List>
-                    <Tab>Overview</Tab>
-                    <Tab>Input data</Tab>
-                    <Tab>Technical input</Tab>
-                    <Tab>Satelite production</Tab>
-                    <Tab>Pipelines umbilicals</Tab>
-                    <Tab>Flexible riser</Tab>
-                </List>
                 <Panels>
                     <Panel>
-                        <OverviewView />
+                        <DescriptionView />
                     </Panel>
+                </Panels>
+                <Panels>
                     <Panel>
-                        <DrainageStrategyView />
+                        <Typography>DG4</Typography>
+                        <Typography variant="h4">DD/MM/YYYY</Typography>
                     </Panel>
-                    <Panel>
-                        <ExplorationView />
-                    </Panel>
-                    <Panel>Satelite production</Panel>
-                    <Panel>Pipelines umbilicals</Panel>
                 </Panels>
             </Tabs>
         </CaseViewDiv>
