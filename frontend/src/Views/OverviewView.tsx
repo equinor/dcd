5632f70d7117166ad3e2c6709a113aecc1e23266
Manveer Singh Pandher <Manveer.Pandher@Soprasteria.com>
2022-03-09 09:58:34 +0100

    Fixed somethign

--- frontend/src/Views/OverviewView.tsx
+++ frontend/src/Views/OverviewView.tsx
@@ -1,25 +1,15 @@
-<<<<<<< HEAD
 import React from 'react'
 import styled from 'styled-components'
 import { Typography } from '@equinor/eds-core-react'
 import { useTranslation } from "react-i18next";
-=======
-import React from "react"
-import styled from "styled-components"
-import { Typography } from "@equinor/eds-core-react"
->>>>>>> main
 
 const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
 `
-
-<<<<<<< HEAD
 const OverviewView = () => {
     const { t } = useTranslation();
-=======
-function OverviewView() {
->>>>>>> main
+
     return (
         <Wrapper>
             <Typography variant="h3">{t('OverviewView.Overview')}</Typography>
