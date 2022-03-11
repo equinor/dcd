bb1a4d8b17ce802f672d5a3bed86ec831fa830d0
Fredrik Leknes <flek@equinor.com>
2022-03-11 11:09:50 +0100

    Added dg dates to frontend model aswell

--- frontend/src/models/Case.ts
+++ frontend/src/models/Case.ts
@@ -2,6 +2,9 @@ export class Case {
     capex?: number
     createdAt?: Date | null
     description?: string
+    firstDecisionGate?: Date | null
+    secondDecisionGate?: Date | null
+    thirdDecisionGate?: Date | null
     lastDecisionGate?: Date | null
     id?: string
     updatedAt?: Date | null
@@ -13,6 +16,9 @@ export class Case {
         this.capex = data.capex
         this.createdAt = data.createTime ? new Date(data.createTime) : null
         this.description = data.description ?? ""
+        this.firstDecisionGate = data.dG1Date ? new Date(data.dG1Date) : null
+        this.secondDecisionGate = data.dG2Date ? new Date(data.dG2Date) : null
+        this.thirdDecisionGate = data.dG3Date ? new Date(data.dG3Date) : null
         this.lastDecisionGate = data.dG4Date ? new Date(data.dG4Date) : null
         this.id = data.id
         this.updatedAt = data.modifyTime ? new Date(data.modifyTime) : null
