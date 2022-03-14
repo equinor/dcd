fce201f0b6c78cc5850b09123fa723e90c70cd79
petterwildhagen <pewi@equinor.com>
2022-03-14 15:53:33 +0100

    Fixed create case bug

--- frontend/src/Services/CaseService.ts
+++ frontend/src/Services/CaseService.ts
@@ -3,6 +3,8 @@ import { __BaseService } from "./__BaseService"
 import { Project } from "../models/Project"
 import { config } from "./config"
 
+import { LoginAccessTokenKey, GetToken } from "../Utils/common"
+
 class __CaseService extends __BaseService {
     public async createCase(data: Components.Schemas.CaseDto): Promise<Project> {
         const res = await this.post("", { body: data })
@@ -14,3 +16,10 @@ export const CaseService = new __CaseService({
     ...config.CaseService,
     accessToken: window.sessionStorage.getItem("loginAccessToken")!,
 })
+
+export function GetCaseService() {
+    return new __CaseService({
+        ...config.CaseService,
+        accessToken: GetToken(LoginAccessTokenKey)!,
+    })
+}
