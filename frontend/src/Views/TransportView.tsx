1329db445f59c587f2529b8e55d6440880c1b1f1
Manveer Singh Pandher <Manveer.Pandher@Soprasteria.com>
2022-03-17 13:53:36 +0100

    Removed imports from Transportview

--- frontend/src/Views/TransportView.tsx
+++ frontend/src/Views/TransportView.tsx
@@ -1,32 +1,13 @@
 import React from "react"
 import styled from "styled-components"
-import { Button, Scrim, Typography } from "@equinor/eds-core-react"
+import { Typography } from "@equinor/eds-core-react"
 
-import ExcelImport from "../Components/ExcelImport/ExcelImport"
 import DataTable, { CellValue } from "../Components/DataTable/DataTable"
-import { generateNewGrid, replaceOldData } from "../Components/DataTable/helpers"
+import { replaceOldData } from "../Components/DataTable/helpers"
 
 const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
-`
-
-const ImportButton = styled(Button)`
-    width: 6rem;
-    align-self: flex-end;
-`
-
-const ScrimBackground = styled(Scrim)`
-    width: 100%;
-`
-
-const Buttons = styled.div`
-    align-self: flex-end;
-    margin-top: 3rem;
-`
-
-const CancelButton = styled(Button)`
-    margin-right: 1rem;
 `
 
 // TODO: This data will have to be generated from the format received from the API
@@ -110,72 +91,22 @@ const initialGridData = [
     ],
 ]
 
-const rowTitles = [
-    "Production profile oil",
-    "Production profile gas",
-    "Production profile water",
-    "Production profile water injection",
-    "Fuel flaring and losses",
-    "Net sales gas",
-    "CO2 emissions",
-]
-
 const columnTitles = ["2022", "2023", "2024", "2025", "2026"]
 
 function TransportView() {
-    const [isImportOpen, setIsImportOpen] = React.useState<boolean>(false)
-    const [dataIsChanged, setDataIsChanged] = React.useState<boolean>(false)
     const [columns, setColumns] = React.useState<string[]>(columnTitles)
     const [gridData, setGridData] = React.useState<CellValue[][]>(initialGridData)
-
-    const closeImportView = () => setIsImportOpen(false)
-    const openImportView = () => setIsImportOpen(true)
 
     const onCellsChanged = (changes: any[]) => {
         const newGridData = replaceOldData(gridData, changes)
         setGridData(newGridData)
-        setDataIsChanged(true)
-    }
-
-    const onImport = (data: { [key: string]: string }[]) => {
-        const { newGridData, newColumns } = generateNewGrid(data, rowTitles)
-
-        setColumns(newColumns)
-        setGridData(newGridData)
-        setDataIsChanged(true)
-
-        closeImportView()
-    }
-
-    const revertChange = () => {
-        setGridData(initialGridData)
         setColumns(columnTitles)
-        setDataIsChanged(false)
-    }
-
-    const saveDataImport = () => {
-        // TODO CODE TO SAVE DATA HERE
-        setDataIsChanged(false)
     }
 
     return (
         <Wrapper>
-            <ImportButton onClick={openImportView}>Import...</ImportButton>
             <Typography variant="h3">Drainage Strategy</Typography>
             <DataTable columns={columns} gridData={gridData} onCellsChanged={onCellsChanged} />
-            {isImportOpen && (
-                <ScrimBackground isDismissable onClose={closeImportView}>
-                    <ExcelImport onClose={closeImportView} onImport={onImport} />
-                </ScrimBackground>
-            )}
-            {dataIsChanged && (
-                <Buttons>
-                    <CancelButton variant="outlined" onClick={revertChange}>
-                        Cancel change
-                    </CancelButton>
-                    <Button onClick={saveDataImport}>Save new data</Button>
-                </Buttons>
-            )}
         </Wrapper>
     )
 }
