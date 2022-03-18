b530186287918edf924a56feb3a210b7cbe45739
Tord Austad <toaus@equinor.com>
2022-03-18 10:24:22 +0100

    Added dropdowns for linking assets (#409)
    * Added dropdowns for linking assets
    
    * Add buttons for creating and opening assets
    
    * Removed create asset functionality

--- frontend/src/Views/CaseView.tsx
+++ frontend/src/Views/CaseView.tsx
@@ -1,36 +1,21 @@
 import {
     Tabs,
     Typography,
-    Button,
-    EdsProvider,
-    TextField,
     Input,
-    Tooltip,
-    NativeSelect,
-    Icon,
 } from "@equinor/eds-core-react"
 import {
     useEffect,
     useState,
     ChangeEventHandler,
-    MouseEventHandler,
 } from "react"
 import { useParams } from "react-router-dom"
 import styled from "styled-components"
-import { add, link } from "@equinor/eds-icons"
 import { Project } from "../models/Project"
 import { Case } from "../models/Case"
 import { GetProjectService } from "../Services/ProjectService"
 import DescriptionView from "./DescriptionView"
-import { Modal } from "../Components/Modal"
-import DrainageStrategyView from "./DrainageStrategyView"
-import ExplorationView from "./ExplorationView"
-import SubstructureView from "./SubstructureView"
-import SurfView from "./SurfView"
-import TopsideView from "./TopsideView"
-import TransportView from "./TransportView"
-import WellprojectView from "./WellprojectView"
 import { GetCaseService } from "../Services/CaseService"
+import CaseAsset from "../Components/CaseAsset"
 
 const {
     Panels, Panel,
@@ -42,7 +27,7 @@ const CaseViewDiv = styled.div`
     flex-direction: column;
 `
 
-const CaseHeader = styled(Typography)`
+const CaseHeader = styled.div`
     margin-bottom: 2rem;
     display: flex;
 
@@ -51,25 +36,7 @@ const CaseHeader = styled(Typography)`
     }
 `
 
-const ActionsContainer = styled.div`
-    > *:not(:last-child) {
-        margin-right: 0.5rem;
-    }
-`
-
-const CreateAssetForm = styled.form`
-    width: 30rem;
-
-    > * {
-        margin-bottom: 1.5rem;
-    }
-`
-
-const AssetDropdown = styled(NativeSelect)`
-    width: 30rem;
-`
-
-const Dg4Field = styled(Typography)`
+const Dg4Field = styled.div`
     margin-bottom: 2rem;
     width: 10rem;
     display: flex;
@@ -80,10 +47,6 @@ function CaseView() {
     const [caseItem, setCase] = useState<Case>()
     const [activeTab, setActiveTab] = useState<number>(0)
     const params = useParams()
-    const [createAssetModalIsOpen, setCreateAssetModalIsOpen] = useState<boolean>(false)
-    const [createAssetFormData, setCreateAssetFormData] = useState<Record<string, any>>({})
-    const [submitIsDisabled, setSubmitIsDisabled] = useState<boolean>(false)
-    const [linkAssetModalIsOpen, setLinkAssetModalIsOpen] = useState<boolean>(false)
     const [dg4DateRec, setDg4DateRec] = useState<Record<string, any>>({})
 
     useEffect(() => {
@@ -103,17 +66,6 @@ function CaseView() {
         setActiveTab(index)
     }
 
-    const toggleCreateAssetModal = () => setCreateAssetModalIsOpen(!createAssetModalIsOpen)
-
-    const toggleLinkAssetModal = () => setLinkAssetModalIsOpen(!linkAssetModalIsOpen)
-
-    const handleCreateAssetFormFieldChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-        setCreateAssetFormData({
-            ...createAssetFormData,
-            [e.target.name]: e.target.value,
-        })
-    }
-
     const handleDg4FieldChange: ChangeEventHandler<HTMLInputElement> = async (e) => {
         setDg4DateRec({
             dg4DateRec,
@@ -130,32 +82,6 @@ function CaseView() {
         setProject(newProject)
     }
 
-    const submitCreateAssetForm: MouseEventHandler<HTMLButtonElement> = async (e) => {
-        e.preventDefault()
-        setSubmitIsDisabled(true)
-
-        try {
-            setSubmitIsDisabled(false)
-            toggleCreateAssetModal()
-        } catch (error) {
-            setSubmitIsDisabled(false)
-            console.error("[CaseView] error while submitting form data", error)
-        }
-    }
-
-    const submitLinkAssetForm: MouseEventHandler<HTMLButtonElement> = async (e) => {
-        e.preventDefault()
-        setSubmitIsDisabled(true)
-
-        try {
-            setSubmitIsDisabled(false)
-            toggleLinkAssetModal()
-        } catch (error) {
-            setSubmitIsDisabled(false)
-            console.error("[CaseView] error while submitting form data", error)
-        }
-    }
-
     const dg4ReturnDate = () => {
         const dg4DateGet = caseItem?.DG4Date?.toLocaleDateString("en-CA")
         if (dg4DateGet !== "0001-01-01") {
@@ -170,92 +96,7 @@ function CaseView() {
         <CaseViewDiv>
             <CaseHeader>
                 <Typography variant="h2">{caseItem?.name}</Typography>
-                <EdsProvider density="compact">
-                    <ActionsContainer>
-                        <Tooltip title="Create an asset">
-                            <Button variant="ghost_icon" aria-label="Create an asset" onClick={toggleCreateAssetModal}>
-                                <Icon data={add} />
-                            </Button>
-                        </Tooltip>
-                        <Tooltip title="Link to asset">
-                            <Button variant="ghost_icon" aria-label="Link to asset" onClick={toggleLinkAssetModal}>
-                                <Icon data={link} />
-                            </Button>
-                        </Tooltip>
-                    </ActionsContainer>
-                </EdsProvider>
             </CaseHeader>
-            <Modal isOpen={createAssetModalIsOpen} title="Create an asset" shards={[]}>
-                <CreateAssetForm>
-                    <AssetDropdown
-                        label="Asset type"
-                        id="asset"
-                        name="asset"
-                        placeholder="Choose an asset"
-                    />
-
-                    <TextField
-                        label="Name"
-                        id="name"
-                        name="name"
-                        placeholder="Name"
-                        onChange={handleCreateAssetFormFieldChange}
-                    />
-
-                    <div>
-                        <Button
-                            type="submit"
-                            onClick={submitCreateAssetForm}
-                            disabled={submitIsDisabled}
-                        >
-                            Create asset
-                        </Button>
-                        <Button
-                            type="button"
-                            color="secondary"
-                            variant="ghost"
-                            onClick={toggleCreateAssetModal}
-                        >
-                            Cancel
-                        </Button>
-                    </div>
-                </CreateAssetForm>
-            </Modal>
-            <Modal isOpen={linkAssetModalIsOpen} title="Link to asset" shards={[]}>
-                <CreateAssetForm>
-                    <AssetDropdown
-                        label="Asset type"
-                        id="asset"
-                        name="asset"
-                        placeholder="Choose an asset"
-                    />
-
-                    <AssetDropdown
-                        label="Name"
-                        id="name"
-                        name="name"
-                        placeholder="Name"
-                    />
-
-                    <div>
-                        <Button
-                            type="submit"
-                            onClick={submitLinkAssetForm}
-                            disabled={submitIsDisabled}
-                        >
-                            Link to asset
-                        </Button>
-                        <Button
-                            type="button"
-                            color="secondary"
-                            variant="ghost"
-                            onClick={toggleLinkAssetModal}
-                        >
-                            Cancel
-                        </Button>
-                    </div>
-                </CreateAssetForm>
-            </Modal>
             <Tabs activeTab={activeTab} onChange={handleTabChange}>
                 <Panels>
                     <Panel>
@@ -277,34 +118,13 @@ function CaseView() {
                         </Dg4Field>
                     </Panel>
                 </Panels>
-                <Panel>
-                    {project.drainageStrategies.length > 0 ? <DrainageStrategyView />
-                        : <p>No Drainage Strategy for case</p> }
-                </Panel>
-                <Panel>
-                    {project.explorations.length > 0 ? <ExplorationView />
-                        : <p>No Explorations for case</p>}
-                </Panel>
-                <Panel>
-                    {project.substructures.length > 0 ? <SubstructureView />
-                        : <p>No Subsctructures for case</p>}
-                </Panel>
-                <Panel>
-                    {project.surfs.length > 0 ? <SurfView />
-                        : <p>No Surfs for case</p>}
-                </Panel>
-                <Panel>
-                    {project.topsides.length > 0 ? <TopsideView />
-                        : <p>No Topsides for case</p>}
-                </Panel>
-                <Panel>
-                    {project.transports.length > 0 ? <TransportView />
-                        : <p>No Transports for case</p>}
-                </Panel>
-                <Panel>
-                    {project.wellProjects.length > 0 ? <WellprojectView />
-                        : <p>No Wellprojects for case</p>}
-                </Panel>
+                <CaseAsset
+                    caseItem={caseItem}
+                    project={project}
+                    setProject={setProject}
+                    setCase={setCase}
+                    caseId={params.caseId}
+                />
             </Tabs>
         </CaseViewDiv>
     )
