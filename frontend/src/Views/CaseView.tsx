158ef7c3811f3d12c43054715dc839f2b59639dd
Fredrik Leknes <flek@equinor.com>
2022-03-18 14:57:46 +0100

    Prevent submit when empty text

--- frontend/src/Views/CaseView.tsx
+++ frontend/src/Views/CaseView.tsx
@@ -196,7 +196,7 @@ function CaseView() {
                         <Button
                             type="submit"
                             onClick={submitUpdateDescription}
-                            // disabled={submitIsDisabled}
+                            disabled={caseDescription === ""}
                         >
                             Submit
                         </Button>
