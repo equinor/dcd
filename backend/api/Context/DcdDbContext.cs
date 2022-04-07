432b8e7071deb96c33f4232ee1538946d61b0b35
Ahmed Abdi <ahmab@equinor.com>
2022-04-07 11:18:13 +0200

    Sett TopsideCostProfile nullable in DcdDbContext.cs

--- backend/api/Context/DcdDbContext.cs
+++ backend/api/Context/DcdDbContext.cs
@@ -21,7 +21,7 @@ namespace api.Context
 
         public DbSet<Topside>? Topsides { get; set; }
 
-        public DbSet<TopsideCostProfile> TopsideCostProfiles { get; set; }
+        public DbSet<TopsideCostProfile>? TopsideCostProfiles { get; set; }
 
         public DbSet<Transport>? Transports { get; set; }
 
