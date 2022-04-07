30af97c514ea742c2c9521aa3cbb7a55af8402a3
Manveer Singh Pandher <Manveer.Pandher@Soprasteria.com>
2022-04-07 12:17:57 +0200

    Forgot last file

---
+++ frontend/src/models/assets/surf/SurfCessasionCostProfile.ts
@@ -0,0 +1,26 @@
+export class SurfCessasionCostProfile implements Components.Schemas.SurfCessationCostProfileDto {
+    id?: string
+    startYear?: number | undefined
+    values?: number [] | null
+    epaVersion?: string | null
+    currency?: Components.Schemas.Currency | undefined
+    sum?: number | undefined
+
+    constructor(data?: Components.Schemas.SurfCessationCostProfileDto) {
+        if (data !== undefined && data !== null) {
+            this.id = data?.id
+            this.startYear = data?.startYear
+            this.values = data?.values ?? []
+            this.epaVersion = data?.epaVersion ?? ""
+            this.currency = data?.currency
+            this.sum = data?.sum
+        }
+    }
+
+    static fromJSON(data?: Components.Schemas.SurfCessationCostProfileDto): SurfCessasionCostProfile | undefined {
+        if (data === undefined || data === null) {
+            return undefined
+        }
+        return new SurfCessasionCostProfile(data)
+    }
+}
