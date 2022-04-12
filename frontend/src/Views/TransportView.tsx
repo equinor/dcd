79249d61d8147a67e2bc52261dbcb517921bb661
Tord Austad <toaus@equinor.com>
2022-04-12 14:51:02 +0200

    Create TimeSeries component (#534)
    Reason for case:
    * During development of MVP, duplication had been done in order to get fast progress. This caused alot of duplication, which Commit will handle.
    
    What has been done:
    * Created TimeSeries as a Domain Model name, which represents the different Assets.
    * Created Re-Useable TimeSeries.tsx file, and Enums for each of the different assets.
    * Minor refactoring of force-unwrapping.
    
    Github Case:
    * https://github.com/equinor/dcd/issues/456
    
    Co-authored-by: Manveer Singh Pandher <100352897+ManveerPandher@users.noreply.github.com>

--- frontend/src/Views/TransportView.tsx
+++ frontend/src/Views/TransportView.tsx
@@ -1,164 +1,73 @@
 import {
-    Button, Input, Label, Typography,
+    Input, Label, Typography,
 } from "@equinor/eds-core-react"
 import { ChangeEventHandler, useEffect, useState } from "react"
 import {
     useLocation, useNavigate, useParams,
 } from "react-router"
-import styled from "styled-components"
-import DataTable, { CellValue } from "../Components/DataTable/DataTable"
-import {
-    buildGridData, getColumnAbsoluteYears, replaceOldData,
-} from "../Components/DataTable/helpers"
-import Import from "../Components/Import/Import"
+import TimeSeries from "../Components/TimeSeries"
+import TimeSeriesEnum from "../models/assets/TimeSeriesEnum"
 import { Transport } from "../models/assets/transport/Transport"
-import { TransportCostProfile } from "../models/assets/transport/TransportCostProfile"
 import { Case } from "../models/Case"
 import { Project } from "../models/Project"
 import { GetProjectService } from "../Services/ProjectService"
 import { GetTransportService } from "../Services/TransportService"
-
-const AssetHeader = styled.div`
-    margin-bottom: 2rem;
-    display: flex;
-
-    > *:first-child {
-        margin-right: 2rem;
-    }
-`
-
-const AssetViewDiv = styled.div`
-    margin: 2rem;
-    display: flex;
-    flex-direction: column;
-`
-
-const Wrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-`
-
-const WrapperColumn = styled.div`
-    display: flex;
-    flex-direction: column;
-`
-
-const ImportButton = styled(Button)`
-    margin-left: 2rem;
-    &:disabled {
-        margin-left: 2rem;
-    }
-`
-
-const SaveButton = styled(Button)`
-    margin-top: 5rem;
-    margin-left: 2rem;
-    &:disabled {
-        margin-left: 2rem;
-        margin-top: 5rem;
-    }
-`
-
-const Dg4Field = styled.div`
-    margin-left: 1rem;
-    margin-bottom: 2rem;
-    width: 10rem;
-    display: flex;
-`
+import { EMPTY_GUID } from "../Utils/constants"
+import {
+    AssetHeader, AssetViewDiv, Dg4Field, SaveButton, Wrapper, WrapperColumn,
+} from "./Asset/StyledAssetComponents"
 
 const TransportView = () => {
-    const [, setProject] = useState<Project>()
+    const [project, setProject] = useState<Project>()
     const [caseItem, setCase] = useState<Case>()
     const [transport, setTransport] = useState<Transport>()
-    const [columns, setColumns] = useState<string[]>([""])
-    const [gridData, setGridData] = useState<CellValue[][]>([[]])
-    const [costProfileDialogOpen, setCostProfileDialogOpen] = useState(false)
     const [hasChanges, setHasChanges] = useState(false)
     const [transportName, setTransportName] = useState<string>("")
     const params = useParams()
     const navigate = useNavigate()
     const location = useLocation()
 
-    const emptyGuid = "00000000-0000-0000-0000-000000000000"
-
     useEffect(() => {
         (async () => {
             try {
                 const projectResult = await GetProjectService().getProjectByID(params.projectId!)
                 setProject(projectResult)
-                const caseResult = projectResult.cases.find((o) => o.id === params.caseId)
+            } catch (error) {
+                console.error(`[CaseView] Error while fetching project ${params.projectId}`, error)
+            }
+        })()
+    }, [])
+
+    useEffect(() => {
+        (async () => {
+            if (project !== undefined) {
+                const caseResult = project.cases.find((o) => o.id === params.caseId)
                 setCase(caseResult)
-                let newTransport = projectResult.transports.find((s) => s.id === params.transportId)
+                let newTransport = project.transports.find((s) => s.id === params.transportId)
                 if (newTransport !== undefined) {
                     setTransport(newTransport)
                 } else {
                     newTransport = new Transport()
                     setTransport(newTransport)
                 }
-                setTransportName(newTransport.name!)
-                const newColumnTitles = getColumnAbsoluteYears(caseResult, newTransport?.costProfile)
-                setColumns(newColumnTitles)
-                const newGridData = buildGridData(newTransport?.costProfile)
-                setGridData(newGridData)
-            } catch (error) {
-                console.error(`[CaseView] Error while fetching project ${params.projectId}`, error)
+                setTransportName(newTransport?.name!)
             }
         })()
-    }, [params.projectId, params.caseId])
-
-    const onCellsChanged = (changes: { cell: { value: number }; col: number; row: number; value: string }[]) => {
-        const newGridData = replaceOldData(gridData, changes)
-        setGridData(newGridData)
-        setColumns(getColumnAbsoluteYears(caseItem, transport?.costProfile))
-        setHasChanges(true)
-    }
-
-    const updateInsertTransportCostProfile = (input: string, year: number) => {
-        const newTransport = new Transport(transport!)
-        const newCostProfile = new TransportCostProfile()
-        newTransport.id = newTransport.id ?? emptyGuid
-        newTransport.costProfile = newTransport.costProfile ?? newCostProfile
-        newTransport.costProfile!.values = input.replace(/(\r\n|\n|\r)/gm, "")
-            .split("\t").map((i) => parseFloat(i))
-        newTransport.costProfile!.startYear = year
-        newTransport.costProfile!.epaVersion = newTransport.costProfile.epaVersion ?? ""
-        return newTransport
-    }
-
-    const onImport = (input: string, year: number) => {
-        const newTransport = updateInsertTransportCostProfile(input, year)
-        setTransport(newTransport)
-        const newColumnTitles = getColumnAbsoluteYears(caseItem, newTransport?.costProfile)
-        setColumns(newColumnTitles)
-        const newGridData = buildGridData(newTransport?.costProfile)
-        setGridData(newGridData)
-        setCostProfileDialogOpen(!costProfileDialogOpen)
-        if (transportName !== "") {
-            setHasChanges(true)
-        }
-    }
+    }, [project])
 
     const handleSave = async () => {
         const transportDto = new Transport(transport!)
         transportDto.name = transportName
-        if (transport?.id === emptyGuid) {
+        if (transport?.id === EMPTY_GUID) {
             transportDto.projectId = params.projectId
-            const updatedProject = await GetTransportService().createTransport(params.caseId!, transportDto!)
-            const updatedCase = updatedProject.cases.find((c) => c.id === params.caseId)
-            const newTransport = updatedProject.transports.at(-1)
-            const newUrl = location.pathname.replace(emptyGuid, newTransport!.id!)
-            setProject(updatedProject)
-            setCase(updatedCase)
-            setTransport(newTransport)
+            const newProject = await GetTransportService().createTransport(params.caseId!, transportDto!)
+            const newTransport = newProject.transports.at(-1)
+            const newUrl = location.pathname.replace(EMPTY_GUID, newTransport!.id!)
             navigate(`${newUrl}`, { replace: true })
+            setProject(newProject)
         } else {
-            transportDto.projectId = params.projectId
             const newProject = await GetTransportService().updateTransport(transportDto!)
             setProject(newProject)
-            const newCase = newProject.cases.find((c) => c.id === params.caseId)
-            setCase(newCase)
-            const newTransport = newProject.transports.find((t) => t.id === params.transportId)
-            setTransport(newTransport)
         }
         setHasChanges(false)
     }
@@ -170,19 +79,6 @@ const TransportView = () => {
         } else {
             setHasChanges(false)
         }
-    }
-
-    const deleteCostProfile = () => {
-        const transportCopy = new Transport(transport)
-        transportCopy.costProfile = undefined
-        if (transportName !== "") {
-            setHasChanges(true)
-        } else {
-            setHasChanges(false)
-        }
-        setColumns([])
-        setGridData([[]])
-        setTransport(transportCopy)
     }
 
     return (
@@ -206,27 +102,16 @@ const TransportView = () => {
                     <Input disabled defaultValue={caseItem?.DG4Date?.toLocaleDateString("en-CA")} type="date" />
                 </Dg4Field>
             </Wrapper>
-            <Wrapper>
-                <Typography variant="h4">Cost profile</Typography>
-                <ImportButton onClick={() => { setCostProfileDialogOpen(true) }}>Import</ImportButton>
-                <ImportButton
-                    disabled={transport?.costProfile === undefined}
-                    color="danger"
-                    onClick={deleteCostProfile}
-                >
-                    Delete
-                </ImportButton>
-            </Wrapper>
-            <WrapperColumn>
-                <DataTable
-                    columns={columns}
-                    gridData={gridData}
-                    onCellsChanged={onCellsChanged}
-                    dG4Year={caseItem?.DG4Date?.getFullYear().toString()!}
-                />
-            </WrapperColumn>
-            {!costProfileDialogOpen ? null
-                : <Import onClose={() => { setCostProfileDialogOpen(!costProfileDialogOpen) }} onImport={onImport} />}
+            <TimeSeries
+                caseItem={caseItem}
+                setAsset={setTransport}
+                setHasChanges={setHasChanges}
+                asset={transport}
+                timeSeriesType={TimeSeriesEnum.costProfile}
+                assetName={transportName}
+                timeSeriesTitle="Cost profile"
+            />
+            {" "}
             <Wrapper><SaveButton disabled={!hasChanges} onClick={handleSave}>Save</SaveButton></Wrapper>
         </AssetViewDiv>
     )
--- frontend/src/Views/WellProjectView.tsx
+++ frontend/src/Views/WellProjectView.tsx
@@ -1,209 +1,74 @@
 import {
-    Button, Input, Typography, Label,
+    Input, Typography, Label,
 } from "@equinor/eds-core-react"
 import { ChangeEventHandler, useEffect, useState } from "react"
 import {
     useLocation, useNavigate, useParams,
 } from "react-router"
-import styled from "styled-components"
-import DataTable, { CellValue } from "../Components/DataTable/DataTable"
-import {
-    buildGridData, getColumnAbsoluteYears, replaceOldData,
-} from "../Components/DataTable/helpers"
-import Import from "../Components/Import/Import"
-import { DrillingScheduleDto } from "../models/assets/wellproject/DrillingScheduleDto"
+import TimeSeries from "../Components/TimeSeries"
+import TimeSeriesEnum from "../models/assets/TimeSeriesEnum"
 import { WellProject } from "../models/assets/wellproject/WellProject"
-import { WellProjectCostProfileDto } from "../models/assets/wellproject/WellProjectCostProfileDto"
 import { Case } from "../models/Case"
 import { Project } from "../models/Project"
 import { GetProjectService } from "../Services/ProjectService"
 import { GetWellProjectService } from "../Services/WellProjectService"
-
-const AssetHeader = styled.div`
-    margin-bottom: 2rem;
-    display: flex;
-
-    > *:first-child {
-        margin-right: 2rem;
-    }
-`
-
-const AssetViewDiv = styled.div`
-    margin: 2rem;
-    display: flex;
-    flex-direction: column;
-`
-
-const Wrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-    margin-top: 1rem;
-`
-
-const WrapperColumn = styled.div`
-    display: flex;
-    flex-direction: column;
-`
-
-const ImportButton = styled(Button)`
-    margin-left: 2rem;
-    &:disabled {
-        margin-left: 2rem;
-    }
-`
-
-const SaveButton = styled(Button)`
-    margin-top: 5rem;
-    margin-left: 2rem;
-    &:disabled {
-        margin-left: 2rem;
-        margin-top: 5rem;
-    }
-`
-
-const Dg4Field = styled.div`
-    margin-left: 1rem;
-    margin-bottom: 1rem;
-    width: 10rem;
-    display: flex;
-`
+import { EMPTY_GUID } from "../Utils/constants"
+import {
+    AssetHeader, AssetViewDiv, Dg4Field, SaveButton, Wrapper, WrapperColumn,
+} from "./Asset/StyledAssetComponents"
 
 function WellProjectView() {
-    const [, setProject] = useState<Project>()
+    const [project, setProject] = useState<Project>()
     const [caseItem, setCase] = useState<Case>()
     const [wellProject, setWellProject] = useState<WellProject>()
-    const [columns, setColumns] = useState<string[]>([""])
-    const [gridData, setGridData] = useState<CellValue[][]>([[]])
-    const [costProfileDialogOpen, setCostProfileDialogOpen] = useState(false)
-    const [drillingColumns, setDrillingColumns] = useState<string[]>([""])
-    const [gridDrillingData, setGridDrillingData] = useState<CellValue[][]>([[]])
-    const [drillingDialogOpen, setDrillingDialogOpen] = useState(false)
     const [hasChanges, setHasChanges] = useState(false)
     const [wellProjectName, setWellProjectName] = useState<string>("")
     const params = useParams()
     const navigate = useNavigate()
     const location = useLocation()
 
-    const emptyGuid = "00000000-0000-0000-0000-000000000000"
-
     useEffect(() => {
         (async () => {
             try {
                 const projectResult = await GetProjectService().getProjectByID(params.projectId!)
                 setProject(projectResult)
-                const caseResult = projectResult.cases.find((o) => o.id === params.caseId)
+            } catch (error) {
+                console.error(`[CaseView] Error while fetching project ${params.projectId}`, error)
+            }
+        })()
+    }, [])
+
+    useEffect(() => {
+        (async () => {
+            if (project !== undefined) {
+                const caseResult = project.cases.find((o) => o.id === params.caseId)
                 setCase(caseResult)
-                let newWellProject = projectResult.wellProjects.find((s) => s.id === params.wellProjectId)
+                let newWellProject = project.wellProjects.find((s) => s.id === params.wellProjectId)
                 if (newWellProject !== undefined) {
                     setWellProject(newWellProject)
                 } else {
                     newWellProject = new WellProject()
                     setWellProject(newWellProject)
                 }
-                setWellProjectName(newWellProject.name!)
-                const newColumnTitles = getColumnAbsoluteYears(caseResult, newWellProject?.costProfile)
-                setColumns(newColumnTitles)
-                const newGridData = buildGridData(newWellProject?.costProfile)
-                setGridData(newGridData)
-                const newDrillingColumnTitles = getColumnAbsoluteYears(caseResult, newWellProject?.drillingSchedule)
-                setDrillingColumns(newDrillingColumnTitles)
-                const newDrillingGridData = buildGridData(newWellProject?.drillingSchedule)
-                setGridDrillingData(newDrillingGridData)
-            } catch (error) {
-                console.error(`[CaseView] Error while fetching project ${params.projectId}`, error)
+                setWellProjectName(newWellProject?.name!)
             }
         })()
-    }, [params.projectId, params.caseId])
-
-    const onCellsChanged = (changes: { cell: { value: number }; col: number; row: number; value: string }[]) => {
-        const newGridData = replaceOldData(gridData, changes)
-        setGridData(newGridData)
-        setColumns(getColumnAbsoluteYears(caseItem, wellProject?.costProfile))
-    }
-
-    const updateInsertWellProjectCostProfile = (input: string, year: number) => {
-        const newWellProject = new WellProject(wellProject!)
-        const newCostProfile = new WellProjectCostProfileDto()
-        newWellProject.id = newWellProject.id ?? emptyGuid
-        newWellProject.costProfile = newWellProject.costProfile ?? newCostProfile
-        newWellProject.costProfile!.values = input.replace(/(\r\n|\n|\r)/gm, "")
-            .split("\t").map((i) => parseFloat(i))
-        newWellProject.costProfile!.startYear = year
-        newWellProject.costProfile!.epaVersion = newWellProject.costProfile.epaVersion ?? ""
-        return newWellProject
-    }
-
-    const updateInsertWellProjectDrilling = (input: string, year: number) => {
-        const newWellProject = new WellProject(wellProject!)
-        const newDrillingSchedule = new DrillingScheduleDto()
-        newWellProject.id = newWellProject.id ?? emptyGuid
-        newWellProject.drillingSchedule = newWellProject.drillingSchedule ?? newDrillingSchedule
-        newWellProject.drillingSchedule!.values = input.replace(/(\r\n|\n|\r)/gm, "")
-            .split("\t").map((i) => parseFloat(i))
-        newWellProject.drillingSchedule!.startYear = year
-        return newWellProject
-    }
-
-    const onImport = (input: string, year: number) => {
-        const newWellProject = updateInsertWellProjectCostProfile(input, year)
-        if (newWellProject.costProfile === undefined) {
-            newWellProject.costProfile = new WellProjectCostProfileDto()
-        }
-        newWellProject.id = newWellProject.id ?? emptyGuid
-        newWellProject.costProfile!.startYear = year
-        // eslint-disable-next-line max-len
-        newWellProject.costProfile!.values = input.replace(/(\r\n|\n|\r)/gm, "").split("\t").map((i) => parseFloat(i))
-        setWellProject(newWellProject)
-        const newColumnTitles = getColumnAbsoluteYears(caseItem, newWellProject?.costProfile)
-        setColumns(newColumnTitles)
-        const newGridData = buildGridData(newWellProject?.costProfile)
-        setGridData(newGridData)
-        setCostProfileDialogOpen(!costProfileDialogOpen)
-        if (wellProjectName !== "") {
-            setHasChanges(true)
-        }
-    }
-
-    const onDrillingImport = (input: string, year: number) => {
-        const newWellProject = updateInsertWellProjectDrilling(input, year)
-        if (newWellProject.drillingSchedule === undefined) {
-            newWellProject.drillingSchedule = new DrillingScheduleDto()
-        }
-        newWellProject.drillingSchedule!.startYear = year
-        // eslint-disable-next-line max-len
-        newWellProject.drillingSchedule!.values = input.replace(/(\r\n|\n|\r)/gm, "").split("\t").map((i) => parseInt(i, 10))
-        setWellProject(newWellProject)
-        const newColumnTitles = getColumnAbsoluteYears(caseItem, newWellProject?.drillingSchedule)
-        setDrillingColumns(newColumnTitles)
-        const newGridData = buildGridData(newWellProject?.drillingSchedule)
-        setGridDrillingData(newGridData)
-        setDrillingDialogOpen(!drillingDialogOpen)
-        if (wellProjectName !== "") {
-            setHasChanges(true)
-        }
-    }
+    }, [project])
 
     const handleSave = async () => {
         const wellProjectDto = new WellProject(wellProject!)
         wellProjectDto.name = wellProjectName
-        if (wellProject?.id === emptyGuid) {
+        if (wellProject?.id === EMPTY_GUID) {
             wellProjectDto.projectId = params.projectId
             const updatedProject: Project = await
             GetWellProjectService().createWellProject(params.caseId!, wellProjectDto!)
-            const updatedCase = updatedProject.cases.find((o) => o.id === params.caseId)
             const newWellProject = updatedProject.wellProjects.at(-1)
-            const newUrl = location.pathname.replace(emptyGuid, newWellProject!.id!)
-            setCase(updatedCase)
-            setWellProject(newWellProject)
+            const newUrl = location.pathname.replace(EMPTY_GUID, newWellProject!.id!)
             navigate(`${newUrl}`, { replace: true })
+            setWellProject(newWellProject)
         } else {
-            wellProjectDto.projectId = params.projectId
             const newProject = await GetWellProjectService().updateWellProject(wellProjectDto!)
             setProject(newProject)
-            const newCase = newProject.cases.find((o) => o.id === params.caseId)
-            setCase(newCase)
-            const newWellProject = newProject.wellProjects.find((s) => s.id === params.wellProjectId)
-            setWellProject(newWellProject)
         }
         setHasChanges(false)
     }
@@ -215,32 +80,6 @@ function WellProjectView() {
         } else {
             setHasChanges(false)
         }
-    }
-
-    const deleteCostProfile = () => {
-        const wellProjectCopy = new WellProject(wellProject)
-        wellProjectCopy.costProfile = undefined
-        if (wellProjectName !== "") {
-            setHasChanges(true)
-        } else {
-            setHasChanges(false)
-        }
-        setColumns([])
-        setGridData([[]])
-        setWellProject(wellProjectCopy)
-    }
-
-    const deleteDrillingSchedule = () => {
-        const wellProjectCopy = new WellProject(wellProject)
-        wellProjectCopy.drillingSchedule = undefined
-        if (wellProjectName !== "") {
-            setHasChanges(true)
-        } else {
-            setHasChanges(false)
-        }
-        setDrillingColumns([])
-        setGridDrillingData([[]])
-        setWellProject(wellProjectCopy)
     }
 
     return (
@@ -264,48 +103,24 @@ function WellProjectView() {
                     <Input disabled defaultValue={caseItem?.DG4Date?.toLocaleDateString("en-CA")} type="date" />
                 </Dg4Field>
             </Wrapper>
-            <Wrapper>
-                <Typography variant="h4">Cost profile</Typography>
-                <ImportButton onClick={() => { setCostProfileDialogOpen(true) }}>Import</ImportButton>
-                <ImportButton
-                    disabled={wellProject?.costProfile === undefined}
-                    color="danger"
-                    onClick={deleteCostProfile}
-                >
-                    Delete
-                </ImportButton>
-            </Wrapper>
-            <WrapperColumn>
-                <DataTable
-                    columns={columns}
-                    gridData={gridData}
-                    onCellsChanged={onCellsChanged}
-                    dG4Year={caseItem?.DG4Date?.getFullYear().toString()!}
-                />
-            </WrapperColumn>
-            {!costProfileDialogOpen ? null
-                : <Import onClose={() => { setCostProfileDialogOpen(!costProfileDialogOpen) }} onImport={onImport} />}
-            <Wrapper>
-                <Typography variant="h4">Drilling schedule</Typography>
-                <ImportButton onClick={() => { setDrillingDialogOpen(true) }}>Import</ImportButton>
-                <ImportButton
-                    disabled={wellProject?.drillingSchedule === undefined}
-                    color="danger"
-                    onClick={deleteDrillingSchedule}
-                >
-                    Delete
-                </ImportButton>
-            </Wrapper>
-            <WrapperColumn>
-                <DataTable
-                    columns={drillingColumns}
-                    gridData={gridDrillingData}
-                    onCellsChanged={onCellsChanged}
-                    dG4Year={caseItem?.DG4Date?.getFullYear().toString()!}
-                />
-            </WrapperColumn>
-            {!drillingDialogOpen ? null
-                : <Import onClose={() => { setDrillingDialogOpen(!drillingDialogOpen) }} onImport={onDrillingImport} />}
+            <TimeSeries
+                caseItem={caseItem}
+                setAsset={setWellProject}
+                setHasChanges={setHasChanges}
+                asset={wellProject}
+                timeSeriesType={TimeSeriesEnum.costProfile}
+                assetName={wellProjectName}
+                timeSeriesTitle="Cost profile"
+            />
+            <TimeSeries
+                caseItem={caseItem}
+                setAsset={setWellProject}
+                setHasChanges={setHasChanges}
+                asset={wellProject}
+                timeSeriesType={TimeSeriesEnum.drillingSchedule}
+                assetName={wellProjectName}
+                timeSeriesTitle="Drilling schedule"
+            />
             <Wrapper><SaveButton disabled={!hasChanges} onClick={handleSave}>Save</SaveButton></Wrapper>
 
         </AssetViewDiv>
