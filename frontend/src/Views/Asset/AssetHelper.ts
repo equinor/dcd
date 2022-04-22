718fb0562b4a363ebcfbc74310ff2bc2d1fc4dcb
Tord Austad <toaus@equinor.com>
2022-04-22 16:00:46 +0200

    Fix lint error

--- frontend/src/Views/Asset/AssetHelper.ts
+++ frontend/src/Views/Asset/AssetHelper.ts
@@ -1,24 +1,36 @@
-/* eslint-disable max-len */
 import React from "react"
 import { IAsset } from "../../models/assets/IAsset"
 import { ITimeSeries } from "../../models/ITimeSeries"
 
-const SetYears = (years: [number, number], timeSeries: ITimeSeries, dG4Year: number, setFirstYear: React.Dispatch<React.SetStateAction<number | undefined>>, setLastYear: React.Dispatch<React.SetStateAction<number | undefined>>): [number, number] => {
+const SetYears = (
+    years: [number, number],
+    timeSeries: ITimeSeries,
+    dG4Year: number,
+    setFirstYear: React.Dispatch<React.SetStateAction<number | undefined>>,
+    setLastYear: React.Dispatch<React.SetStateAction<number | undefined>>,
+): [number, number] => {
     const newYears = years
-    if (timeSeries.startYear + dG4Year < years[0]) {
-        setFirstYear(timeSeries.startYear + dG4Year)
-        newYears[0] = timeSeries.startYear + dG4Year
-    }
+    if (timeSeries.startYear !== undefined && timeSeries.values !== undefined) {
+        if (timeSeries.startYear + dG4Year < years[0]) {
+            setFirstYear(timeSeries.startYear + dG4Year)
+            newYears[0] = timeSeries.startYear + dG4Year
+        }
 
-    if (timeSeries.startYear + dG4Year + timeSeries.values.length > years[1]) {
-        setLastYear(timeSeries.startYear + dG4Year + timeSeries.values.length)
-        newYears[1] = timeSeries.startYear + dG4Year + timeSeries.values.length
+        if (timeSeries.startYear + dG4Year + timeSeries.values.length > years[1]) {
+            setLastYear(timeSeries.startYear + dG4Year + timeSeries.values.length)
+            newYears[1] = timeSeries.startYear + dG4Year + timeSeries.values.length
+        }
     }
 
     return newYears
 }
 
-export const TimeSeriesYears = (asset: IAsset, dG4Year: number, setFirstYear: React.Dispatch<React.SetStateAction<number | undefined>>, setLastYear: React.Dispatch<React.SetStateAction<number | undefined>>) => {
+export const TimeSeriesYears = (
+    asset: IAsset,
+    dG4Year: number,
+    setFirstYear: React.Dispatch<React.SetStateAction<number | undefined>>,
+    setLastYear: React.Dispatch<React.SetStateAction<number | undefined>>,
+) => {
     let years: [number, number]
     years = [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER]
     if (asset.co2Emissions !== undefined) {
