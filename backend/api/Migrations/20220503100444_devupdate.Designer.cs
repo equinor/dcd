1b53722c5bf5df43f5a360b2886de8ae5ef52170
Fredrik Leknes <flek@equinor.com>
2022-05-03 12:10:16 +0200

    Updated database

--- backend/api/Migrations/20220503100444_devupdate.Designer.cs
+++ backend/api/Migrations/20220503100444_devupdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using api.Context;
 
@@ -11,9 +12,10 @@ using api.Context;
 namespace api.Migrations
 {
     [DbContext(typeof(DcdDbContext))]
-    partial class DcdDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220503100444_devupdate")]
+    partial class devupdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/api/Migrations/20220503100444_devupdate.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    public partial class devupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DG0Date",
+                table: "Cases",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<double>(
+                name: "FacilitiesAvailability",
+                table: "Cases",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GasInjectorCount",
+                table: "Cases",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProducerCount",
+                table: "Cases",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "RigMobDemob",
+                table: "Cases",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RiserCount",
+                table: "Cases",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TemplateCount",
+                table: "Cases",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WaterInjectorCount",
+                table: "Cases",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DG0Date",
+                table: "Cases");
+
+            migrationBuilder.DropColumn(
+                name: "FacilitiesAvailability",
+                table: "Cases");
+
+            migrationBuilder.DropColumn(
+                name: "GasInjectorCount",
+                table: "Cases");
+
+            migrationBuilder.DropColumn(
+                name: "ProducerCount",
+                table: "Cases");
+
+            migrationBuilder.DropColumn(
+                name: "RigMobDemob",
+                table: "Cases");
+
+            migrationBuilder.DropColumn(
+                name: "RiserCount",
+                table: "Cases");
+
+            migrationBuilder.DropColumn(
+                name: "TemplateCount",
+                table: "Cases");
+
+            migrationBuilder.DropColumn(
+                name: "WaterInjectorCount",
+                table: "Cases");
+        }
+    }
+}
