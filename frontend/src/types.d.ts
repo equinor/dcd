739499b11c11e42793571698ec8971f74a1e06ff
petterwildhagen <pewi@equinor.com>
2022-05-04 10:56:16 +0200

    Remove filtering on assets

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -176,8 +176,7 @@ declare namespace Components {
             values?: number /* double */[] | null;
             sum?: number; // double
         }
-        export type ProjectCategory =
-            0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21; // int32
+        export type ProjectCategory = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21; // int32
         export interface ProjectDto {
             projectId?: string; // uuid
             name?: string | null;
