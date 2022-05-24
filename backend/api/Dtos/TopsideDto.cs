17a8a07f2d180dac9e46902c688c584b8c5e21f6
Daniel Böhme <dboh@equinor.com>
2022-05-24 10:29:39 +0200

    Initial metadata for topside

--- backend/api/Dtos/TopsideDto.cs
+++ backend/api/Dtos/TopsideDto.cs
@@ -9,14 +9,24 @@ namespace api.Dtos
         public string Name { get; set; } = string.Empty!;
         public Guid ProjectId { get; set; }
         public TopsideCostProfileDto? CostProfile { get; set; }
-        public TopsideCessationCostProfileDto? CessationCostProfile { get; set; }
+        public TopsideCessationCostProfileDto? TopsideCessationCostProfileDto { get; set; }
         public double DryWeight { get; set; }
         public double OilCapacity { get; set; }
         public double GasCapacity { get; set; }
         public double FacilitiesAvailability { get; set; }
         public ArtificialLift ArtificialLift { get; set; }
         public Maturity Maturity { get; set; }
-        public Currency Currency { get; set; }
+        public double FuelConsumption { get; set; }
+        public double FlaredGas { get; set; }
+        public double CO2ShareOilProfile { get; set; }
+        public double CO2ShareGasProfile { get; set; }
+        public double CO2ShareWaterInjectionProfile { get; set; }
+        public double CO2OnMaxOilProfile { get; set; }
+        public double CO2OnMaxGasProfile { get; set; }
+        public double CO2OnMaxWaterInjectionProfile { get; set; }
+        public DateTimeOffset CostYear { get; set; }
+        public DateTimeOffset ProspVersion { get; set; }
+        public DateTimeOffset LastChanged { get; set; }
     }
 
     public class TopsideCostProfileDto : TimeSeriesCostDto
