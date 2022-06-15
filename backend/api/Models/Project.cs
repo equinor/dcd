9d9382cad4c6517f230b0430ad658b09da51ad0d
Eirik Sander-Fjeld <eisande@equinor.com>
2022-06-16 00:19:30 +0200

    Adding FusionProjectId to backend db Model

--- backend/api/Models/Project.cs
+++ backend/api/Models/Project.cs
@@ -5,6 +5,7 @@ namespace api.Models
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public Guid CommonLibraryId { get; set; }
+        public Guid FusionProjectId { get; set; }
         public string CommonLibraryName { get; set; } = null!;
         public string Description { get; set; } = string.Empty;
         public string Country { get; set; } = string.Empty;
