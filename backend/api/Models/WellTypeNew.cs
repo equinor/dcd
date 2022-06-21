299f7f013da5d528d57137ce8dda37becdfdcc76
Daniel Böhme <dboh@equinor.com>
2022-06-21 13:08:25 +0200

    Updated WellTypeNew file with description. Added extra line end on files

--- backend/api/Models/WellTypeNew.cs
+++ backend/api/Models/WellTypeNew.cs
@@ -2,17 +2,21 @@ namespace api.Models
 {
     public class WellTypeNew
     {
-        public WellTypeName Name { get; set; }
+        public string? Name { get; set; }
+        public WellTypeCategory Category { get; set; }
         public double WellCost { get; set; }
         public double DrillingDays { get; set; }
+        public string? Description { get; set; }
     }
 
-    public enum WellTypeName
+    public enum WellTypeCategory
     {
         Oil_Producer,
         Gas_Producer,
         Water_Injector,
         Gas_Injector,
         Exploration_Well,
+        Appraisal_Well,
+        Sidetrack
     }
 }
