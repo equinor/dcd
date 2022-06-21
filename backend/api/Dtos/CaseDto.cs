bfd7d7bd5ac0b0bddb71bca92264df39f2ae6095
Manveer Singh Pandher <Manveer.Pandher@Soprasteria.com>
2022-06-21 15:17:05 +0200

    Dotnet format

--- backend/api/Dtos/CaseDto.cs
+++ backend/api/Dtos/CaseDto.cs
@@ -34,8 +34,9 @@ namespace api.Dtos
         public CapexYear? CapexYear { get; set; }
     }
 
-    public class CapexYear {
-        public double[] values { get; set; }
+    public class CapexYear
+    {
+        public double[]? values { get; set; }
         public int? startYear { get; set; }
     }
 }
