8a95ee54488c86d37fc20393d423b3aa524946ad
Tord Austad <toaus@equinor.com>
2022-07-07 11:12:33 +0200

    Start implementation of generating costprofile from drilling schedule

--- backend/api/Services/WellProjectWellService.cs
+++ backend/api/Services/WellProjectWellService.cs
@@ -13,13 +13,15 @@ namespace api.Services
     {
         private readonly DcdDbContext _context;
         private readonly ProjectService _projectService;
+        private readonly WellProjectService _wellProjectService;
         private readonly ILogger<CaseService> _logger;
 
-        public WellProjectWellService(DcdDbContext context, ProjectService projectService, ILoggerFactory loggerFactory)
+        public WellProjectWellService(DcdDbContext context, ProjectService projectService, WellProjectService wellProjectService, ILoggerFactory loggerFactory)
         {
             _context = context;
             _projectService = projectService;
             _logger = loggerFactory.CreateLogger<CaseService>();
+            _wellProjectService = wellProjectService;
         }
 
         public ProjectDto CreateWellProjectWell(WellProjectWellDto wellProjectWellDto)
@@ -35,6 +37,24 @@ namespace api.Services
             throw new NotFoundInDBException();
         }
 
+        // public ProjectDto UpdateWellProjectWell(WellProjectWellDto updatedWellProjectWellDto)
+        // {
+        //     var existing = GetWellProjectWell(updatedWellProjectWellDto.WellId, updatedWellProjectWellDto.WellProjectId);
+        //     WellProjectWellAdapter.ConvertExisting(existing, updatedWellProjectWellDto);
+        //     if (updatedWellProjectWellDto.DrillingSchedule == null && existing.DrillingSchedule != null)
+        //     {
+        //         _context.DrillingSchedule!.Remove(existing.DrillingSchedule);
+        //     }
+        //     _context.WellProjectWell!.Update(existing);
+        //     _context.SaveChanges();
+        //     var projectId = _context.WellProjects!.FirstOrDefault(c => c.Id == updatedWellProjectWellDto.WellProjectId)?.ProjectId;
+        //     if (projectId != null)
+        //     {
+        //         return _projectService.GetProjectDto((Guid)projectId);
+        //     }
+        //     throw new NotFoundInDBException();
+        // }
+
         public ProjectDto UpdateWellProjectWell(WellProjectWellDto updatedWellProjectWellDto)
         {
             var existing = GetWellProjectWell(updatedWellProjectWellDto.WellId, updatedWellProjectWellDto.WellProjectId);
@@ -43,6 +63,23 @@ namespace api.Services
             {
                 _context.DrillingSchedule!.Remove(existing.DrillingSchedule);
             }
+
+            // Generate wellproject costprofile
+            var wellProject = _context.WellProjects!.Include(wp => wp.WellProjectWells).ThenInclude(wpw => wpw.DrillingSchedule).FirstOrDefault(wp => wp.Id == existing.WellProjectId);
+            if (wellProject?.CostProfile?.Override != true)
+            {
+                var project = _context.Projects!.Include(p => p.Wells).FirstOrDefault(p => p.Id == wellProject.ProjectId);
+                if (wellProject.WellProjectWells != null && project?.Wells != null
+                    && (wellProject.WellProjectWells.Any(wpw => wpw.DrillingSchedule != null
+                    && wpw.WellId != updatedWellProjectWellDto.WellId) || updatedWellProjectWellDto.DrillingSchedule != null))
+                {
+                    GenerateCostProfileFromDrillingSchedules(wellProject, wellProject.WellProjectWells.ToList(), project.Wells.ToList());
+                    var wellProjectDto = WellProjectDtoAdapter.Convert(wellProject);
+                    _wellProjectService.UpdateWellProject(wellProjectDto);
+                    // _context.WellProjects!.Update(wellProject);
+                }
+            }
+
             _context.WellProjectWell!.Update(existing);
             _context.SaveChanges();
             var projectId = _context.WellProjects!.FirstOrDefault(c => c.Id == updatedWellProjectWellDto.WellProjectId)?.ProjectId;
@@ -51,6 +88,105 @@ namespace api.Services
                 return _projectService.GetProjectDto((Guid)projectId);
             }
             throw new NotFoundInDBException();
+        }
+
+        private static WellProjectCostProfile? MergeCostProfiles(WellProjectCostProfile t1, WellProjectCostProfile t2)
+        {
+            var t1Year = t1.StartYear;
+            var t2Year = t2.StartYear;
+            var t1Values = t1.Values;
+            var t2Values = t2.Values;
+            if (t1Values.Length == 0)
+            {
+                if (t2.Values.Length == 0)
+                {
+                    return null;
+                }
+                return t2;
+            }
+            if (t2Values.Length == 0)
+            {
+                return t1;
+            }
+
+            var offset = t1Year < t2Year ? t2Year - t1Year : t1Year - t2Year;
+
+            var values = new List<double>();
+            if (t1Year < t2Year)
+            {
+                values = MergeTimeSeries(t1Values.ToList(), t2Values.ToList(), offset);
+                // values.AddRange(t1Values);
+                // values.InsertRange(offset, t2Values);
+            }
+            else
+            {
+                values = MergeTimeSeries(t2Values.ToList(), t1Values.ToList(), offset);
+                // values.AddRange(t2Values);
+                // values.InsertRange(offset, t1Values);
+            }
+
+            var timeSeries = new WellProjectCostProfile();
+            timeSeries.StartYear = Math.Min(t1Year, t2Year);
+            timeSeries.Values = values.ToArray();
+            return timeSeries;
+        }
+
+        private static List<double> MergeTimeSeries(List<double> t1, List<double> t2, int offset)
+        {
+            if (t1.Count - offset >= t2.Count)
+            {
+                for (var i = 0; i < t2.Count; i++)
+                {
+                    t1[i + offset] += t2[i];
+                }
+            }
+            else
+            {
+                for (var i = 0; i < t1.Count - offset; i++)
+                {
+                    t1[i + offset] += t2[i];
+                }
+                t1.AddRange(t2.TakeLast(t2.Count - t1.Count - offset));
+            }
+            return t1;
+        }
+
+
+
+        public void GenerateCostProfileFromDrillingSchedules(WellProject wellProject, List<WellProjectWell> wellProjectWells, List<Well> wells)
+        {
+            var costProfile = new WellProjectCostProfile();
+            // 1. Drilling schedules => Cost profiles
+            var costProfiles = new List<WellProjectCostProfile>();
+            foreach (var wellProjectWell in wellProjectWells)
+            {
+                if (wellProjectWell.DrillingSchedule == null) continue;
+
+                var well = wells.Find(w => w.Id == wellProjectWell.WellId);
+                if (well == null) continue;
+
+                var wellCostProfile = new WellProjectCostProfile();
+                wellCostProfile.StartYear = wellProjectWell.DrillingSchedule.StartYear;
+                var values = new List<double>();
+                foreach (var value in wellProjectWell.DrillingSchedule.Values)
+                {
+                    values.Add(value * well.WellCost);
+                }
+                wellCostProfile.Values = values.ToArray();
+                costProfiles.Add(wellCostProfile);
+            }
+            // 2. Merge cost profiles
+
+            var tempCostProfile = new WellProjectCostProfile();
+            foreach (var profile in costProfiles)
+            {
+                tempCostProfile = MergeCostProfiles(tempCostProfile, profile);
+            }
+
+            costProfile = tempCostProfile;
+
+            // 3.
+            wellProject.CostProfile = costProfile;
         }
 
         public WellProjectWell GetWellProjectWell(Guid wellId, Guid caseId)
