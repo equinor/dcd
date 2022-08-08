b0fd8442c4575d7ebc4881963f564019a702171a
Tord Austad <toaus@equinor.com>
2022-08-08 15:00:23 +0200

    Recalculate exploration costprofile when wellcost changes

--- backend/api/Services/ExplorationWellService.cs
+++ backend/api/Services/ExplorationWellService.cs
@@ -46,26 +46,8 @@ namespace api.Services
                 _context.DrillingSchedule!.Remove(existing.DrillingSchedule);
             }
 
-            // Generate exploration costprofile
             var exploration = _context.Explorations!.Include(wp => wp.CostProfile).Include(wp => wp.ExplorationWells).ThenInclude(wpw => wpw.DrillingSchedule).FirstOrDefault(wp => wp.Id == existing.ExplorationId);
-            if (exploration?.CostProfile?.Override != true)
-            {
-                var project = _context.Projects!.Include(p => p.Wells).FirstOrDefault(p => p.Id == exploration.ProjectId);
-                if (exploration.ExplorationWells != null && project?.Wells != null
-                    && (exploration.ExplorationWells.Any(wpw => wpw.DrillingSchedule != null
-                    && wpw.WellId != updatedExplorationWellDto.WellId) || updatedExplorationWellDto.DrillingSchedule != null))
-                {
-                    GenerateCostProfileFromDrillingSchedules(exploration, exploration.ExplorationWells.ToList(), project.Wells.ToList());
-                    var explorationDto = ExplorationDtoAdapter.Convert(exploration);
-                    _explorationService.UpdateExploration(explorationDto);
-                }
-                else if (exploration.ExplorationWells != null && project?.Wells != null)
-                {
-                    exploration.CostProfile = null;
-                    var explorationDto = ExplorationDtoAdapter.Convert(exploration);
-                    _explorationService.UpdateExploration(explorationDto);
-                }
-            }
+            _explorationService.CalculateCostProfile(exploration, existing, null);
 
             _context.ExplorationWell!.Update(existing);
             _context.SaveChanges();
@@ -75,115 +57,6 @@ namespace api.Services
                 return _projectService.GetProjectDto((Guid)projectId);
             }
             throw new NotFoundInDBException();
-        }
-
-        private static ExplorationCostProfile? MergeCostProfiles(ExplorationCostProfile t1, ExplorationCostProfile t2)
-        {
-            var t1Year = t1.StartYear;
-            var t2Year = t2.StartYear;
-            var t1Values = t1.Values;
-            var t2Values = t2.Values;
-            if (t1Values.Length == 0)
-            {
-                if (t2.Values.Length == 0)
-                {
-                    return null;
-                }
-                return t2;
-            }
-            if (t2Values.Length == 0)
-            {
-                return t1;
-            }
-
-            var offset = t1Year < t2Year ? t2Year - t1Year : t1Year - t2Year;
-
-            List<double> values;
-            if (t1Year < t2Year)
-            {
-                values = MergeTimeSeries(t1Values.ToList(), t2Values.ToList(), offset);
-            }
-            else
-            {
-                values = MergeTimeSeries(t2Values.ToList(), t1Values.ToList(), offset);
-            }
-
-            var timeSeries = new ExplorationCostProfile();
-            timeSeries.StartYear = Math.Min(t1Year, t2Year);
-            timeSeries.Values = values.ToArray();
-            return timeSeries;
-        }
-
-        private static List<double> MergeTimeSeries(List<double> t1, List<double> t2, int offset)
-        {
-            var doubleList = new List<double>();
-            if (offset > t1.Count)
-            {
-                doubleList.AddRange(t1);
-                var zeros = offset - t1.Count;
-                var zeroList = Enumerable.Repeat(0.0, zeros);
-                doubleList.AddRange(zeroList);
-                doubleList.AddRange(t2);
-                return doubleList;
-            }
-            doubleList.AddRange(t1.Take(offset));
-            if (t1.Count - offset == t2.Count)
-            {
-                doubleList.AddRange(t1.TakeLast(t1.Count - offset).Zip(t2, (x, y) => x + y));
-            }
-            else if (t1.Count - offset > t2.Count)
-            {
-                doubleList.AddRange(t1.TakeLast(t1.Count - offset).Zip(t2, (x, y) => x + y));
-                var remaining = t1.Count - offset - t2.Count;
-                doubleList.AddRange(t1.TakeLast(remaining));
-            }
-            else
-            {
-                doubleList.AddRange(t1.TakeLast(t1.Count - offset).Zip(t2, (x, y) => x + y));
-                var remaining = t2.Count - (t1.Count - offset);
-                doubleList.AddRange(t2.TakeLast(remaining));
-            }
-            return doubleList;
-        }
-
-        public void GenerateCostProfileFromDrillingSchedules(Exploration exploration, List<ExplorationWell> explorationWells, List<Well> wells)
-        {
-            ExplorationCostProfile costProfile;
-            var costProfiles = new List<ExplorationCostProfile>();
-            foreach (var explorationWell in explorationWells)
-            {
-                if (explorationWell.DrillingSchedule == null) { continue; }
-
-                var well = wells.Find(w => w.Id == explorationWell.WellId);
-                if (well == null) { continue; }
-
-                var wellCostProfile = new ExplorationCostProfile();
-                wellCostProfile.StartYear = explorationWell.DrillingSchedule.StartYear;
-                var values = new List<double>();
-                foreach (var value in explorationWell.DrillingSchedule.Values)
-                {
-                    values.Add(value * well.WellCost);
-                }
-                wellCostProfile.Values = values.ToArray();
-                costProfiles.Add(wellCostProfile);
-            }
-
-            var tempCostProfile = new ExplorationCostProfile();
-            foreach (var profile in costProfiles)
-            {
-                tempCostProfile = MergeCostProfiles(tempCostProfile, profile);
-            }
-
-            costProfile = tempCostProfile;
-            if (exploration.CostProfile != null)
-            {
-                exploration.CostProfile.StartYear = costProfile.StartYear;
-                exploration.CostProfile.Values = costProfile.Values;
-            }
-            else
-            {
-                exploration.CostProfile = costProfile;
-            }
         }
 
         public ExplorationWell GetExplorationWell(Guid wellId, Guid caseId)
--- backend/api/Services/WellService.cs
+++ backend/api/Services/WellService.cs
@@ -12,14 +12,16 @@ namespace api.Services
         private readonly DcdDbContext _context;
         private readonly ProjectService _projectService;
         private readonly WellProjectService _wellProjectService;
+        private readonly ExplorationService _explorationService;
         private readonly ILogger<CaseService> _logger;
 
-        public WellService(DcdDbContext context, ProjectService projectService, WellProjectService wellProjectService, ILoggerFactory loggerFactory)
+        public WellService(DcdDbContext context, ProjectService projectService, WellProjectService wellProjectService, ExplorationService explorationService, ILoggerFactory loggerFactory)
         {
             _context = context;
             _projectService = projectService;
             _logger = loggerFactory.CreateLogger<CaseService>();
             _wellProjectService = wellProjectService;
+            _explorationService = explorationService;
         }
 
         public ProjectDto CreateWell(WellDto wellDto)
@@ -38,10 +40,21 @@ namespace api.Services
 
             if (updateCostProfiles)
             {
-                foreach (var wpw in existing.WellProjectWells)
+                if (existing.WellProjectWells?.Count > 0)
                 {
-                    var wellProject = _wellProjectService.GetWellProject(wpw.WellProjectId);
-                    _wellProjectService.CalculateCostProfile(wellProject, wpw, existing);
+                    foreach (var wpw in existing.WellProjectWells)
+                    {
+                        var wellProject = _wellProjectService.GetWellProject(wpw.WellProjectId);
+                        _wellProjectService.CalculateCostProfile(wellProject, wpw, existing);
+                    }
+                }
+                else if (existing.ExplorationWells?.Count > 0)
+                {
+                    foreach (var ew in existing.ExplorationWells)
+                    {
+                        var exploration = _explorationService.GetExploration(ew.ExplorationId);
+                        _explorationService.CalculateCostProfile(exploration, ew, existing);
+                    }
                 }
             }
 
