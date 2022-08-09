3fae3ef655fca343ae1a297f969c24838dddd690
Tord Austad <toaus@equinor.com>
2022-08-09 16:12:51 +0200

    Retrieve appId from Fusion, remove unused code

--- frontend/src/auth/tokenProvider.tsx
+++
@@ -1,5 +0,0 @@
-export const obtainAccessToken = async (scopes: string[]): Promise<string | undefined> => {
-    console.log("Acquire access token: ")
-    // eslint-disable-next-line no-return-await
-    return await window.Fusion.modules.auth.acquireAccessToken({ scopes })
-}
