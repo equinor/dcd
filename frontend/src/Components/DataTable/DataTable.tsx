d028d2151865a7a3ec828539d9e703091ba7673a
Daniel Böhme <dboh@equinor.com>
2022-08-29 10:30:04 +0200

    added profile column, cleaned up code

--- frontend/src/Components/DataTable/DataTable.tsx
+++ frontend/src/Components/DataTable/DataTable.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react"
-import ReactDataSheet from "react-datasheet"
+import { useMemo } from "react"
 import "react-datasheet/lib/react-datasheet.css"
 import "./style.css"
 import { AgGridReact } from "ag-grid-react"
@@ -9,9 +8,6 @@ export interface CellValue {
     value: number | string
     readOnly?: boolean
 }
-
-class Table extends ReactDataSheet<CellValue> {}
-
 interface Props {
     columns: string[]
     gridData: any
@@ -19,19 +15,13 @@ interface Props {
     dG4Year: string
 }
 
-/* eslint-disable react/no-unstable-nested-components */
 function DataTable({
     columns, gridData, onCellsChanged, dG4Year,
 }: Props) {
-    // const [rowData, setRowData] = useState([
-    //     { rowDataToColumns() },
-    // ])
+    // const [rowData, setRowData] = useState<any[]>(rowDataToColumns())
+    // const [columnDefs, setColumnDefs] = useState<ColDef[]>()
 
     useAgGridStyles()
-
-    // const rowData = [
-    //     { 2025: 1, 2026: 1, 2027: 1 },
-    // ]
 
     const rowDataToColumns = () => {
         const col = columns
@@ -43,25 +33,26 @@ function DataTable({
                 objVal.push(`${gridData[0].map((v:any) => v.value)[i]}`)
             }
         }
-        const obj1 = objKey.reduce((obj:any, element:any, index:any) => ({ ...obj, [element]: objVal[index] }), {})
-        console.log(obj1)
-        return [obj1]
+        const rowObj = objKey.reduce((obj:any, element:any, index:any) => ({ ...obj, [element]: objVal[index] }), {})
+        return [rowObj]
+    }
+
+    const columnsArrayToColDef = () => {
+        const col = columns
+        const columnToColDef = []
+        const columnPinned = { field: "Profile", initialPinned: "left" }
+        for (let i = 0; i < col.length; i += 1) {
+            columnToColDef.push({ field: col[i] })
+        }
+        const columnWithProfile = columnToColDef.concat([...[columnPinned]])
+        return columnWithProfile
     }
 
     const defaultColDef = useMemo(() => ({
         resizable: true,
         sortable: true,
+        initialWidth: 120,
     }), [])
-
-    const columnsArrayToColDef = () => {
-        const col = columns
-        const columnToColDef = []
-        for (let i = 0; i < col.length; i += 1) {
-            columnToColDef.push({ field: col[i] })
-        }
-        console.log(columnToColDef)
-        return columnToColDef
-    }
 
     return (
         <div className="ag-theme-alpine" style={{ height: 200 }}>
