b4a4dc9743304026a1fb38076bc506be6ee4690a
Ahmed Abdi <ahmab@equinor.com>
2022-08-31 15:22:34 +0200

    Code cleanup

--- backend/api/Controllers/UploadController.cs
+++ backend/api/Controllers/UploadController.cs
@@ -1,55 +1,53 @@
-using System.Net.Http.Headers;
-
 using api.Dtos;
 using api.Services;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Graph;
 
-namespace api.Controllers
+namespace api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+[Authorize]
+public class UploadController : ControllerBase
 {
-    [Route("[controller]")]
-    [ApiController]
-    [Authorize]
-    public class UploadController : ControllerBase
+    private const string testDriveItemId = "01LF7VUDUW3IAIVUAVBNAJALIVG7JK62EZ";
+    private readonly GraphRestService _graphRestService;
+    private readonly ImportProspService _prospService;
+
+    public UploadController(ImportProspService prospService, GraphRestService graphRestService)
     {
-        private readonly ImportProspService _prospService;
-        private readonly GraphRestService _graphRestService;
-        private const string testDriveItemId = "01LF7VUDUW3IAIVUAVBNAJALIVG7JK62EZ";
+        _prospService = prospService;
+        _graphRestService = graphRestService;
+    }
 
-        public UploadController(ImportProspService prospService, GraphRestService graphRestService)
-        {
-            _prospService = prospService;
-            _graphRestService = graphRestService;
-        }
+    [HttpGet(Name = nameof(GetFilesFromSharePoint))]
+    public List<DriveItemDto> GetFilesFromSharePoint()
+    {
+        var graph = _graphRestService.GetFilesFromSite();
+        return graph;
+    }
 
-        [HttpGet(Name = nameof(GetFilesFromSharePoint))]
-        public List<DriveItemDto> GetFilesFromSharePoint()
+    [HttpPost(Name = nameof(Upload))]
+    [DisableRequestSizeLimit]
+    public async Task<ProjectDto?> Upload([FromQuery] Guid projectId, [FromQuery] Guid sourceCaseId)
+    {
+        var graph = _graphRestService.GetFilesFromSite();
+        try
         {
-            var graph = _graphRestService.GetFilesFromSite();
-            return graph;
-        }
+            var stream = _graphRestService.GetSharepointFileStream(testDriveItemId);
 
-        [HttpPost(Name = nameof(Upload)), DisableRequestSizeLimit]
-        public async Task<ProjectDto?> Upload([FromQuery] Guid projectId, [FromQuery] Guid sourceCaseId)
-        {
-            var graph = _graphRestService.GetFilesFromSite();
-            try
+            if (stream.Length > 0)
             {
-                var stream = _graphRestService.GetSharepointFileStream(testDriveItemId);
+                var dto = _prospService.ImportProsp(stream, sourceCaseId, projectId);
+                return dto;
+            }
 
-                if (stream.Length > 0)
-                {
-                    var dto = _prospService.ImportProsp(stream, sourceCaseId, projectId);
-                    return dto;
-                }
-                return null;
-            }
-            catch (Exception)
-            {
-                return null;
-            }
+            return null;
+        }
+        catch (Exception)
+        {
+            return null;
         }
     }
 }
--- backend/api/Services/GraphRestService.cs
+++ backend/api/Services/GraphRestService.cs
@@ -1,131 +1,124 @@
-using Azure.Identity;
-
-using Microsoft.Graph;
-using Microsoft.Identity.Client;
-using System.Linq;
+using System.Net.Http.Headers;
 
 using api.Dtos;
 
-using DocumentFormat.OpenXml.Office.CustomUI;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
 
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.Extensions.Azure;
-using Microsoft.IdentityModel.Tokens;
+namespace api.Services;
 
-namespace api.Services
+public class GraphRestService
 {
-    public class GraphRestService
+    private readonly IConfiguration _config;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public GraphRestService(IHttpContextAccessor httpContextAccessor, IConfiguration config)
     {
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly IConfiguration _config;
-
-        public GraphRestService(IHttpContextAccessor httpContextAccessor, IConfiguration config)
-        {
-            _httpContextAccessor = httpContextAccessor;
-            _config = config;
-        }
-
-        private string GetSiteId() => _config["SharePoint:Prosp:SiteId"];
-
-        private GraphServiceClient GetGraphClient()
-        {
-            var tenantId = _config["AzureAd:TenantId"];
-            var clientId = _config["AzureAd:ClientId"];
-            var clientSecret = _config["AzureAd:ClientSecret"];
-            var scopes = new[] { "Sites.Read.All" };
-
-            var token = _httpContextAccessor?.HttpContext?.Request?.Headers["Authorization"]
-                .ToString()
-                ?.Split(" ")
-                ?.LastOrDefault();
-
-            var cca = ConfidentialClientApplicationBuilder
-                .Create(clientId)
-                .WithTenantId(tenantId)
-                .WithClientSecret(clientSecret)
-                .Build();
-
-            var authProvider = new DelegateAuthenticationProvider(async (request) => {
-                // Use Microsoft.Identity.Client to retrieve token
-                var assertion = new UserAssertion(token);
-                var result = await cca.AcquireTokenOnBehalfOf(scopes, assertion).ExecuteAsync();
-
-                request.Headers.Authorization =
-                    new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", result.AccessToken);
-            });
-
-            var graphClient = new GraphServiceClient(authProvider);
-            return graphClient;
-        }
-
-        public List<DriveItemDto> GetFilesFromSite()
-        {
-            var graphClient = GetGraphClient();
-            var siteId = GetSiteId();
-            var dto = new List<DriveItemDto>();
-            var query = _config["SharePoint:Prosp:FileQuery"];
-            var validMimeTypes = new List<string>
-            {
-                ExcelMimeTypes.XLS,
-                ExcelMimeTypes.XLSB,
-                ExcelMimeTypes.XLSM,
-                ExcelMimeTypes.XLSX
-            };
-
-            var driveItemSearchCollectionPage = graphClient.Sites[siteId]
-                .Drive.Root.Search(query)
-                .Request()
-                .GetAsync()
-                .GetAwaiter()
-                .GetResult();
-
-            foreach (var driveItem in driveItemSearchCollectionPage.Where(item =>
-                         item.File != null && validMimeTypes.Contains(item.File.MimeType)))
-            {
-                ConvertToDto(driveItem, dto);
-            }
-
-            return dto;
-        }
-
-        private static void ConvertToDto(DriveItem driveItem, List<DriveItemDto> dto)
-        {
-            var item = new DriveItemDto()
-            {
-                Name = driveItem.Name,
-                Id = driveItem.Id,
-                CreatedBy = driveItem.CreatedBy,
-                Content = driveItem.Content,
-                CreatedDateTime = driveItem.CreatedDateTime,
-                Size = driveItem.Size,
-                SharepointIds = driveItem.SharepointIds,
-                LastModifiedBy = driveItem.LastModifiedBy,
-                LastModifiedDateTime = driveItem.LastModifiedDateTime,
-            };
-            dto.Add(item);
-        }
-
-        public Stream GetSharepointFileStream(string id)
-        {
-            var graphClient = GetGraphClient();
-            var siteId = GetSiteId();
-            var driveItemStream = graphClient.Sites[siteId]
-                .Drive.Items[id]
-                .Content.Request()
-                .GetAsync()
-                .GetAwaiter()
-                .GetResult();
-
-            return driveItemStream;
-        }
-
+        _httpContextAccessor = httpContextAccessor;
+        _config = config;
     }
 
-    public class ExcelMimeTypes
+    private string GetSiteId()
     {
-        public static string XLSX => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-        public static string XLSB => "application/vnd.ms-excel.sheet.binary.macroEnabled.12";
-        public static string XLS => "application/vnd.ms-excel";
-        public static string XLSM => "application/vnd.ms-excel.sheet.macroEnabled.12";
+        return _config["SharePoint:Prosp:SiteId"];
     }
+
+    private GraphServiceClient GetGraphClient()
+    {
+        var tenantId = _config["AzureAd:TenantId"];
+        var clientId = _config["AzureAd:ClientId"];
+        var clientSecret = _config["AzureAd:ClientSecret"];
+        var scopes = new[] { "Sites.Read.All" };
+
+        var token = _httpContextAccessor?.HttpContext?.Request?.Headers["Authorization"]
+            .ToString()
+            ?.Split(" ")
+            ?.LastOrDefault();
+
+        var cca = ConfidentialClientApplicationBuilder
+            .Create(clientId)
+            .WithTenantId(tenantId)
+            .WithClientSecret(clientSecret)
+            .Build();
+
+        var authProvider = new DelegateAuthenticationProvider(async request =>
+        {
+            // Use Microsoft.Identity.Client to retrieve token
+            var assertion = new UserAssertion(token);
+            var result = await cca.AcquireTokenOnBehalfOf(scopes, assertion).ExecuteAsync();
+
+            request.Headers.Authorization =
+                new AuthenticationHeaderValue("Bearer", result.AccessToken);
+        });
+
+        var graphClient = new GraphServiceClient(authProvider);
+        return graphClient;
+    }
+
+    public List<DriveItemDto> GetFilesFromSite()
+    {
+        var graphClient = GetGraphClient();
+        var siteId = GetSiteId();
+        var dto = new List<DriveItemDto>();
+        var query = _config["SharePoint:Prosp:FileQuery"];
+        var validMimeTypes = new List<string>
+        {
+            ExcelMimeTypes.XLS,
+            ExcelMimeTypes.XLSB,
+            ExcelMimeTypes.XLSM,
+            ExcelMimeTypes.XLSX
+        };
+
+        var driveItemSearchCollectionPage = graphClient.Sites[siteId]
+            .Drive.Root.Search(query)
+            .Request()
+            .GetAsync()
+            .GetAwaiter()
+            .GetResult();
+
+        foreach (var driveItem in driveItemSearchCollectionPage.Where(item =>
+                     item.File != null && validMimeTypes.Contains(item.File.MimeType)))
+            ConvertToDto(driveItem, dto);
+
+        return dto;
+    }
+
+    private static void ConvertToDto(DriveItem driveItem, List<DriveItemDto> dto)
+    {
+        var item = new DriveItemDto
+        {
+            Name = driveItem.Name,
+            Id = driveItem.Id,
+            CreatedBy = driveItem.CreatedBy,
+            Content = driveItem.Content,
+            CreatedDateTime = driveItem.CreatedDateTime,
+            Size = driveItem.Size,
+            SharepointIds = driveItem.SharepointIds,
+            LastModifiedBy = driveItem.LastModifiedBy,
+            LastModifiedDateTime = driveItem.LastModifiedDateTime
+        };
+        dto.Add(item);
+    }
+
+    public Stream GetSharepointFileStream(string id)
+    {
+        var graphClient = GetGraphClient();
+        var siteId = GetSiteId();
+        var driveItemStream = graphClient.Sites[siteId]
+            .Drive.Items[id]
+            .Content.Request()
+            .GetAsync()
+            .GetAwaiter()
+            .GetResult();
+
+        return driveItemStream;
+    }
+}
+
+public class ExcelMimeTypes
+{
+    public static string XLSX => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    public static string XLSB => "application/vnd.ms-excel.sheet.binary.macroEnabled.12";
+    public static string XLS => "application/vnd.ms-excel";
+    public static string XLSM => "application/vnd.ms-excel.sheet.macroEnabled.12";
 }
