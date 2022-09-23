f6de0b5b20017d52cd9805e1cacf76fecc7e8c10
Daniel Böhme <dboh@equinor.com>
2022-09-23 12:54:09 +0200

    Ag grid total footer (#765)
    * Added initial footer and bold font on totals
    
    * added total footer with aligned grid and manual calculations
    
    * Added conditional footer
    
    * Clean up
    
    * Changed totalTotalCostArray to use realtime values
    
    * Fixed currency bug where assets used units instead of currency
    
    * More clean up
    
    * Remove eslint
    
    * removed unecessary !
    
    * Added enableCharts, removed !s
    
    * defined tyes instead of using any, format datatable
    
    Co-authored-by: Tord Austad <toaus@equinor.com>

--- frontend/src/Components/DataTable/DataTable.tsx
+++ frontend/src/Components/DataTable/DataTable.tsx
@@ -7,7 +7,7 @@ import "react-datasheet/lib/react-datasheet.css"
 import "./style.css"
 import { AgGridReact } from "ag-grid-react"
 import { useAgGridStyles } from "@equinor/fusion-react-ag-grid-addons"
-import { CellValueChangedEvent } from "ag-grid-community"
+import { CellValueChangedEvent, ColDef } from "ag-grid-community"
 import { ITimeSeries } from "../../models/ITimeSeries"
 import { buildGridData } from "./helpers"
 import "ag-grid-enterprise"
@@ -25,20 +25,22 @@ interface Props {
     setHasChanges: Dispatch<SetStateAction<boolean>>,
     setTimeSeries: Dispatch<SetStateAction<ITimeSeries | undefined>>,
     timeSeries: (ITimeSeries | undefined)[]
+    profileType: string
 }
 
 function DataTable({
-    columns, gridData, dG4Year, profileName, profileEnum, setHasChanges, setTimeSeries, timeSeries,
+    columns, gridData, dG4Year, profileName, profileEnum, setHasChanges, setTimeSeries, timeSeries, profileType,
 }: Props) {
-    const gridRef = useRef<AgGridReact | null>(null)
+    const topGrid = useRef<AgGridReact>(null)
+    const bottomGrid = useRef<AgGridReact>(null)
 
     const combinedTimeseries: any = []
 
     useAgGridStyles()
 
     enum CurrencyEnum {
-        "MUSD" = 0,
-        "MNOK" = 1
+        "MNOK" = 1,
+        "MUSD" = 2
     }
 
     enum GSM3Enum {
@@ -67,18 +69,18 @@ function DataTable({
 
     const rowDataToColumns = () => {
         const col = columns
-        const objKey: any = []
-        const objVal: any = []
-        const objValSum: any = []
-        const combinedObjArr: any = []
+        const objKey: string[] = []
+        const objVal: string[] = []
+        const objValSum: number[] = []
+        const combinedObjArr: object[] = []
 
-        const value: any = []
+        const value: object[] = []
 
         if (gridData.length === 0) {
             for (let j = 0; j < profileName.length; j += 1) {
                 const rowPinned = { Profile: profileName[j], Unit: setUnit(j) }
                 const rowObj = objKey
-                    .reduce((obj: any, element: any, index: any) => ({ ...obj, [element]: objVal[index] }), {})
+                    .reduce((obj: object, element: string, index: number) => ({ ...obj, [element]: objVal[index] }), {})
                 combinedObjArr.push(rowObj)
 
                 const totalValueObj = { Total: 0 }
@@ -89,7 +91,7 @@ function DataTable({
         if (gridData.length >= 1 && col.length !== 0) {
             for (let j = 0; j < gridData.length; j += 1) {
                 const rowPinned = { Profile: profileName[j], Unit: setUnit(j) }
-                const totalValue: any = []
+                const totalValue: number[] = []
                 if (gridData[j] !== undefined) {
                     for (let i = 0; i < col.length; i += 1) {
                         if (gridData[j][0]) {
@@ -97,14 +99,16 @@ function DataTable({
                             objVal.push(`${gridData[j][0].map((v: any) => v.value)[i]}`)
                         }
                     }
-                    objValSum.push(gridData[j][0]?.map((v: any) => v.value).reduce((x: any, y: any) => x + y))
+                    objValSum.push(gridData[j][0]?.map((v: any) => v.value).reduce((x: number, y: number) => x + y))
                     totalValue.push(objValSum[j])
                 }
+                const objValToNumbers = objVal.map((x: string) => parseFloat(x))
                 const rowObj = objKey
-                    .reduce((obj: any, element: any, index: any) => ({ ...obj, [element]: objVal[index] }), {})
+                    .reduce((obj: object, element: string, index: number) => (
+                        { ...obj, [element]: objValToNumbers[index] }), {})
                 combinedObjArr.push(rowObj)
 
-                const totalValueObj = { Total: totalValue }
+                const totalValueObj = { Total: Number(totalValue) }
                 value.push({ ...combinedObjArr[j], ...totalValueObj, ...rowPinned })
             }
         }
@@ -116,11 +120,17 @@ function DataTable({
             const col = columns
             const columnToColDef = []
             const columnPinned = [
-                { field: "Profile", pinned: "left", width: "autoWidth" },
-                { field: "Unit", pinned: "left", width: "autoWidth" },
-                { field: "Total", pinned: "right" }]
+                {
+                    field: "Profile", pinned: "left", width: "autoWidth", aggFunc: "",
+                },
+                {
+                    field: "Unit", pinned: "left", width: "autoWidth", aggFunc: "",
+                },
+                {
+                    field: "Total", pinned: "right", aggFunc: "sum", cellStyle: { fontWeight: "bold" },
+                }]
             for (let i = 0; i < col.length; i += 1) {
-                columnToColDef.push({ field: col[i] })
+                columnToColDef.push({ field: col[i], aggFunc: "sum" })
             }
             const columnWithProfile = columnToColDef.concat([...columnPinned])
             return columnWithProfile
@@ -128,11 +138,17 @@ function DataTable({
         return undefined
     }
 
-    const defaultColDef = useMemo(() => ({
+    const defaultColDef = useMemo<ColDef>(() => ({
         resizable: true,
         sortable: true,
-        // initialWidth: 120,
         editable: true,
+        flex: 1,
+    }), [])
+
+    const footerColDef = useMemo<ColDef>(() => ({
+        resizable: true,
+        sortable: true,
+        editable: false,
         flex: 1,
     }), [])
 
@@ -143,41 +159,96 @@ function DataTable({
         const rowEventData = event.data
         const index = event.node.rowIndex
 
-        if (timeSeries! !== undefined) {
-            const convertObj = {
-                convertObj:
-                    (delete rowEventData.Unit, delete rowEventData.Profile,
-                    delete rowEventData.Total),
-                rowEventData,
-            }
-            const changeKeysToValue = Object.keys(rowEventData)
-                .reduce((prev: any, curr: any, ind: any) => ({ ...prev, [(ind)]: Number(rowEventData[curr]) }), {})
-            const newTimeSeries: ITimeSeries = { ...timeSeries![index!] }
-            newTimeSeries.startYear = (Number(Object.keys(rowEventData)[0]) - Number(dG4Year!))
-            newTimeSeries.name = profileName![index!]
-            newTimeSeries.values = Object.values(changeKeysToValue)
-            setTimeSeries(newTimeSeries)
-            const newGridData = buildGridData(newTimeSeries)
-            combinedTimeseries.push(newGridData)
+        const convertObj = {
+            convertObj:
+                (delete rowEventData.Unit, delete rowEventData.Profile,
+                delete rowEventData.Total),
+            rowEventData,
         }
+        const changeKeysToValue = Object.keys(rowEventData)
+            .reduce((prev: object, curr: string, ind: number) => (
+                { ...prev, [(ind)]: Number(rowEventData[curr]) }), {})
+        const newTimeSeries: ITimeSeries = { ...timeSeries[index!] }
+        newTimeSeries.startYear = (Number(Object.keys(rowEventData)[0]) - Number(dG4Year))
+        newTimeSeries.name = profileName[index!]
+        newTimeSeries.values = Object.values(changeKeysToValue)
+        setTimeSeries(newTimeSeries)
+        const newGridData = buildGridData(newTimeSeries)
+        combinedTimeseries.push(newGridData)
         setHasChanges(true)
     }, [dG4Year])
 
+    const columnTotalsData = () => {
+        const footerGridData = {
+            Profile: "Total cost",
+            Unit: setUnit(0),
+        }
+        const totalValueArray: number[] = []
+        const valueArray: number[][] = []
+        if (timeSeries.length >= 1 && columns.length !== 0) {
+            for (let i = 0; i < columns.length; i += 1) {
+                if (timeSeries[i] !== undefined) {
+                    valueArray.push(timeSeries[i]?.values ?? [])
+                }
+            }
+            for (let k = 0; k < columns.length; k += 1) {
+                totalValueArray.push(valueArray.reduce((prev, curr) => prev + curr[k], 0))
+            }
+        }
+        const value = columns
+            .reduce((obj: object, element: string, index: number) => (
+                { ...obj, [element]: totalValueArray[index] }), {})
+        const totalTotalCostArray = []
+        if (timeSeries.length >= 1 && columns.length !== 0) {
+            for (let j = 0; j < timeSeries.length; j += 1) {
+                if (timeSeries[j] !== undefined && gridData[j] !== undefined) {
+                    totalTotalCostArray.push(gridData[j][0]?.map((v: any) => v.value).reduce((x: any, y: any) => x + y))
+                }
+            }
+        }
+        const sum = totalTotalCostArray.reduce((prev, curr) => prev + curr, 0)
+        const totalTotalObj = { Total: Number(sum) }
+        const combinedFooterRow = [{ ...value, ...footerGridData, ...totalTotalObj }]
+        return combinedFooterRow
+    }
+
     return (
-        <div className="ag-theme-alpine">
-            <AgGridReact
-                ref={gridRef}
-                rowData={rowDataToColumns()}
-                columnDefs={columnsArrayToColDef()}
-                defaultColDef={defaultColDef}
-                animateRows
-                domLayout="autoHeight"
-                enableCellChangeFlash
-                onCellValueChanged={onCellValueChanged}
-                rowSelection="multiple"
-                enableRangeSelection
-                suppressCopySingleCellRanges
-            />
+        <div
+            style={{ display: "flex", flexDirection: "column", height: 150 }}
+            className="ag-theme-alpine"
+        >
+            <div style={{ flex: "1 1 auto" }}>
+                <AgGridReact
+                    ref={topGrid}
+                    alignedGrids={bottomGrid.current ? [bottomGrid.current] : undefined}
+                    rowData={rowDataToColumns()}
+                    columnDefs={columnsArrayToColDef()}
+                    defaultColDef={defaultColDef}
+                    animateRows
+                    domLayout="autoHeight"
+                    enableCellChangeFlash
+                    onCellValueChanged={onCellValueChanged}
+                    rowSelection="multiple"
+                    enableRangeSelection
+                    suppressCopySingleCellRanges
+                    suppressMovableColumns
+                    enableCharts
+                />
+            </div>
+            {profileType === "Cost"
+                && (
+                    <div style={{ flex: "none", height: "60px" }}>
+                        <AgGridReact
+                            ref={bottomGrid}
+                            alignedGrids={topGrid.current ? [topGrid.current] : undefined}
+                            rowData={columnTotalsData()}
+                            defaultColDef={footerColDef}
+                            columnDefs={columnsArrayToColDef()}
+                            headerHeight={0}
+                            rowStyle={{ fontWeight: "bold" }}
+                        />
+                    </div>
+                )}
         </div>
     )
 }
