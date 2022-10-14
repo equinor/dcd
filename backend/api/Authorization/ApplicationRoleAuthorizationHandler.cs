55534695a992bc1d2f9bc2f8ac7161559951de5c
Eirik Sander-Fjeld <eisande@equinor.com>
2022-10-14 16:05:58 +0200

    Adding Roles Middleware (#779)
    Adding Roles middleware to backend. 
    
    Co-authored-by: Tord Austad <toaus@equinor.com>

---
+++ backend/api/Authorization/ApplicationRoleAuthorizationHandler.cs
@@ -0,0 +1,59 @@
+namespace Api.Authorization;
+
+using System.Threading.Tasks;
+
+using Api.Authorization.Extensions;
+
+using Microsoft.AspNetCore.Authorization;
+
+public class ApplicationRoleAuthorizationHandler : AuthorizationHandler<ApplicationRoleRequirement>
+{
+    private readonly IHttpContextAccessor httpContextAccessor;
+
+
+    public ApplicationRoleAuthorizationHandler(IHttpContextAccessor httpContextAccessor)
+    {
+        this.httpContextAccessor = httpContextAccessor;
+    }
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ApplicationRoleRequirement requirement)
+    {
+        var requestPath = httpContextAccessor.HttpContext?.Request.Path;
+
+        if (context.User.Identity?.IsAuthenticated != true)
+        {
+            HandleUnauthenticatedRequest(context, requestPath);
+            return Task.CompletedTask;
+        }
+
+        var userRoles = context.User.AssignedApplicationRoles();
+        if (!IsAuthorized(requirement, userRoles))
+        {
+            Console.WriteLine(context.User.AssignedApplicationRoles());
+            HandleUnauthorizedRequest(context, requestPath, requirement.Roles, userRoles);
+            return Task.CompletedTask;
+        }
+
+        context.Succeed(requirement);
+        return Task.CompletedTask;
+    }
+
+    private static bool IsAuthorized(ApplicationRoleRequirement roleRequirement, List<ApplicationRole> userRoles)
+    {
+        return userRoles.Any(role => roleRequirement.Roles.Contains(role));
+    }
+
+    private void HandleUnauthorizedRequest(
+    AuthorizationHandlerContext context,
+    PathString? requestPath,
+    List<ApplicationRole> requiredAnyOfTheseRoles,
+    List<ApplicationRole> userRoles
+)
+    {
+        context.Fail();
+    }
+
+    private void HandleUnauthenticatedRequest(AuthorizationHandlerContext context, PathString? requestPath)
+    {
+        context.Fail();
+    }
+}
