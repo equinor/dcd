bdfa7a29f650ae71be89525b19160fbccbb9049f
Ahmed Abdi <ahmab@equinor.com>
2022-10-24 15:21:35 +0200

    ef migrations

---
+++ backend/api/Migrations/20221024132112_ProspCaseList.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    public partial class ProspCaseList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SharepointFileUrl",
+                table: "Cases",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SharepointFileUrl",
+                table: "Cases");
+        }
+    }
+}
