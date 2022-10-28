0850cc644cb097787646707b099481fb529bd3b2
Ahmed Abdi <ahmab@equinor.com>
2022-10-28 11:02:22 +0200

    New Topside variable 'WaterInjectionCapacity' (#817)
    Added new property in Topside model and mapped it for import in prospservice

--- backend/api/Adapters/TopsideDtoAdapter.cs
+++ backend/api/Adapters/TopsideDtoAdapter.cs
@@ -15,6 +15,7 @@ public static class TopsideDtoAdapter
             DryWeight = topside.DryWeight,
             OilCapacity = topside.OilCapacity,
             GasCapacity = topside.GasCapacity,
+            WaterInjectionCapacity = topside.WaterInjectionCapacity,
             ArtificialLift = topside.ArtificialLift,
             Maturity = topside.Maturity,
             Currency = topside.Currency,
@@ -38,7 +39,7 @@ public static class TopsideDtoAdapter
             ApprovedBy = topside.ApprovedBy,
             DG3Date = topside.DG3Date,
             DG4Date = topside.DG4Date,
-            FacilityOpex = topside.FacilityOpex
+            FacilityOpex = topside.FacilityOpex,
         };
         return topsideDto;
     }
@@ -56,7 +57,7 @@ public static class TopsideDtoAdapter
             Currency = costProfile.Currency,
             EPAVersion = costProfile.EPAVersion,
             Values = costProfile.Values,
-            StartYear = costProfile.StartYear
+            StartYear = costProfile.StartYear,
         };
         return topsideCostProfile;
     }
@@ -68,13 +69,13 @@ public static class TopsideDtoAdapter
             return null;
         }
 
-        TopsideCessationCostProfileDto topsideCostProfile = new TopsideCessationCostProfileDto
+        var topsideCostProfile = new TopsideCessationCostProfileDto
         {
             Id = topsideCessationCostProfile.Id,
             Currency = topsideCessationCostProfile.Currency,
             EPAVersion = topsideCessationCostProfile.EPAVersion,
             Values = topsideCessationCostProfile.Values,
-            StartYear = topsideCessationCostProfile.StartYear
+            StartYear = topsideCessationCostProfile.StartYear,
         };
         return topsideCostProfile;
     }
