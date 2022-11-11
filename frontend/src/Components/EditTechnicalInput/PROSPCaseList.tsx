c5b142dd6e3acd02e50570362bc93e3c1223ed64
Daniel Böhme <dboh@equinor.com>
2022-11-11 13:28:46 +0100

    Prosp import case auto select (#870)
    Added case autoselect when change on prosp import

--- frontend/src/Components/EditTechnicalInput/PROSPCaseList.tsx
+++ frontend/src/Components/EditTechnicalInput/PROSPCaseList.tsx
@@ -6,7 +6,7 @@ import {
     ChangeEvent, Dispatch, SetStateAction, useCallback, useEffect, useMemo, useRef, useState,
 } from "react"
 import { AgGridReact } from "ag-grid-react"
-import { RowNode } from "ag-grid-enterprise"
+import { GetRowIdFunc, GetRowIdParams, RowNode } from "ag-grid-enterprise"
 import styled from "styled-components"
 import { external_link } from "@equinor/eds-icons"
 import { Project } from "../../models/Project"
@@ -86,7 +86,13 @@ function PROSPCaseList({
         resizable: true,
     }), [])
 
+    const caseAutoSelect = (nodeId: string) => {
+        const rowNode = gridRef.current?.getRowNode(nodeId)
+        rowNode.setDataValue("caseSelected", true)
+    }
+
     const changeStatus = (p: any, value: ImportStatusEnum) => {
+        caseAutoSelect(p.node?.data.id)
         p.setValue(value)
     }
 
@@ -124,9 +130,12 @@ function PROSPCaseList({
         return options
     }
 
+    const getRowId = useMemo<GetRowIdFunc>(() => (params: GetRowIdParams) => params.data.id, [])
+
     const handleFileChange = (event: ChangeEvent<HTMLSelectElement>, p: any) => {
         const value = { ...p.value }
         value[1] = event.currentTarget.selectedOptions[0].value
+        caseAutoSelect(p.node?.data.id)
         p.setValue(value)
     }
 
@@ -275,6 +284,7 @@ function PROSPCaseList({
                     animateRows
                     domLayout="autoHeight"
                     onGridReady={onGridReady}
+                    getRowId={getRowId}
                 />
             </div>
             <ApplyButtonWrapper>
