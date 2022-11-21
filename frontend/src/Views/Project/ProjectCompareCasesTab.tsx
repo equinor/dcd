b37e81ac706e15ddb769d286e4b74a1beed9dd07
Daniel Böhme <dboh@equinor.com>
2022-11-21 10:26:38 +0100

    Added case data to rows

--- frontend/src/Views/Project/ProjectCompareCasesTab.tsx
+++ frontend/src/Views/Project/ProjectCompareCasesTab.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components"
-import React, { useMemo, useRef, useState } from "react"
+import React, {
+    useEffect, useMemo, useRef, useState,
+} from "react"
 import {
     Typography,
 } from "@equinor/eds-core-react"
-import LinearDataTable from "../../Components/LinearDataTable"
 import { AgGridReact } from "ag-grid-react"
+import LinearDataTable from "../../Components/LinearDataTable"
 import { customUnitHeaderTemplate } from "../../AgGridUnitInHeader"
 import { Project } from "../../models/Project"
 
@@ -13,6 +15,26 @@ interface Props {
     capexYearX: number[]
     capexYearY: number[][]
     caseTitles: string[]
+}
+
+interface TableCompareCase {
+    id: string,
+    cases: string,
+    description: string,
+    npv: number,
+    breakEven: number,
+    irr: number,
+    oilProduction: number,
+    gasProduction: number,
+    nglProduction: number,
+    totalExportedVolumes: number,
+    studyCostsPlusOpex: number,
+    cessationCosts: number,
+    offshorePlusOnshoreFacilityCosts: number,
+    developmentCosts: number,
+    explorationWellCosts: number,
+    totalCO2Emissions: number,
+    cO2Intensity: number,
 }
 
 // const Wrapper = styled.div`
@@ -57,6 +79,41 @@ function ProjectCompareCasesTab({
         editable: true,
     }), [])
 
+    const [rowData, setRowData] = useState<TableCompareCase[]>()
+
+    const casesToRowData = () => {
+        if (project) {
+            const tableCompareCases: TableCompareCase[] = []
+            project.cases.forEach((c) => {
+                const tableCase: TableCompareCase = {
+                    id: c.id!,
+                    cases: c.name ?? "",
+                    description: c.description ?? "",
+                    npv: c.npv ?? 0,
+                    breakEven: c.breakEven ?? 0,
+                    irr: 0,
+                    oilProduction: 0,
+                    gasProduction: 0,
+                    nglProduction: 0,
+                    totalExportedVolumes: 0,
+                    studyCostsPlusOpex: 0,
+                    cessationCosts: 0,
+                    offshorePlusOnshoreFacilityCosts: 0,
+                    developmentCosts: 0,
+                    explorationWellCosts: 0,
+                    totalCO2Emissions: 0,
+                    cO2Intensity: 0,
+                }
+                tableCompareCases.push(tableCase)
+            })
+            setRowData(tableCompareCases)
+        }
+    }
+
+    useEffect(() => {
+        casesToRowData()
+    }, [project.cases])
+
     const [columnDefs] = useState([
         {
             field: "cases", width: 400,
@@ -65,6 +122,8 @@ function ProjectCompareCasesTab({
             headerName: "Economic KPIs (pre-tax)",
             children: [
                 {
+                    field: "npv",
+                    headerName: "",
                     width: 175,
                     editable: false,
                     headerComponentParams: {
@@ -72,6 +131,8 @@ function ProjectCompareCasesTab({
                     },
                 },
                 {
+                    field: "breakEven",
+                    headerName: "",
                     width: 175,
                     headerComponentParams: {
                         template:
@@ -79,6 +140,8 @@ function ProjectCompareCasesTab({
                     },
                 },
                 {
+                    field: "irr",
+                    headerName: "",
                     width: 175,
                     headerComponentParams: {
                         template: customUnitHeaderTemplate("IRR", "%"),
@@ -90,24 +153,32 @@ function ProjectCompareCasesTab({
             headerName: "Production profiles",
             children: [
                 {
+                    field: "oilProduction",
+                    headerName: "",
                     width: 175,
                     headerComponentParams: {
                         template: customUnitHeaderTemplate("Oil production", "MSm3"),
                     },
                 },
                 {
+                    field: "gasProduction",
+                    headerName: "",
                     width: 175,
                     headerComponentParams: {
                         template: customUnitHeaderTemplate("Gas production", "GSm3"),
                     },
                 },
                 {
+                    field: "nglProduction",
+                    headerName: "",
                     width: 175,
                     headerComponentParams: {
                         template: customUnitHeaderTemplate("NGL production", "mill tonnes"),
                     },
                 },
                 {
+                    field: "totalExportedVolumes",
+                    headerName: "",
                     width: 175,
                     headerComponentParams: {
                         template: customUnitHeaderTemplate("Total exported volumes", "mill boe"),
@@ -119,12 +190,16 @@ function ProjectCompareCasesTab({
             headerName: "Cost profiles",
             children: [
                 {
+                    field: "studyCostsPlusOpex",
+                    headerName: "",
                     width: 175,
                     headerComponentParams: {
                         template: customUnitHeaderTemplate("Study costs + OPEX", "mill USD per 2020"),
                     },
                 },
                 {
+                    field: "cessationCosts",
+                    headerName: "",
                     width: 175,
                     headerComponentParams: {
                         template: customUnitHeaderTemplate("Cessation costs", "USD/bbl"),
@@ -136,18 +211,24 @@ function ProjectCompareCasesTab({
             headerName: "Investment profiles",
             children: [
                 {
+                    field: "offshorePlusOnshoreFacilityCosts",
+                    headerName: "",
                     width: 225,
                     headerComponentParams: {
                         template: customUnitHeaderTemplate("Offshore + Onshore facility costs", "mill USD per 2020"),
                     },
                 },
                 {
+                    field: "developmentCosts",
+                    headerName: "",
                     width: 175,
                     headerComponentParams: {
                         template: customUnitHeaderTemplate("Development well costs", "mill USD per 2020"),
                     },
                 },
                 {
+                    field: "explorationWellCosts",
+                    headerName: "",
                     width: 175,
                     headerComponentParams: {
                         template: customUnitHeaderTemplate("Exploration well costs", "mill USD per 2020"),
@@ -159,12 +240,16 @@ function ProjectCompareCasesTab({
             headerName: "CO2 emissions",
             children: [
                 {
+                    field: "totalCO2Emissions",
+                    headerName: "",
                     width: 175,
                     headerComponentParams: {
                         template: customUnitHeaderTemplate("Total CO2 emissions", "mill tonnes"),
                     },
                 },
                 {
+                    field: "cO2Intensity",
+                    headerName: "",
                     width: 175,
                     headerComponentParams: {
                         template: customUnitHeaderTemplate("CO2 intensity", "kg CO2/boe"),
@@ -183,7 +268,7 @@ function ProjectCompareCasesTab({
         >
             <AgGridReact
                 ref={gridRef}
-                rowData={[]}
+                rowData={rowData}
                 columnDefs={columnDefs}
                 defaultColDef={defaultColDef}
                 animateRows
