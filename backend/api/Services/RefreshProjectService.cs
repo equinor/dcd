be79beca59f4b2033bcd3cc9c011701108e71d79
Eirik Sander-Fjeld <eisande@equinor.com>
2022-11-28 11:40:38 +0100

    641_Refreshprojectproperties (#893)
    * Adding hostedservice for running backgroundtasks
    
    * Updated delay from 10 to 60 seconds
    
    * Moved config to appsettings
    
    * Adding logging statements
    
    * Formating issues
    
    * Making properties readonly after codacy warnings
    
    * Making job run each hour

---
+++ backend/api/Services/RefreshProjectService.cs
@@ -0,0 +1,52 @@
+namespace api.Services;
+
+public class RefreshProjectService : BackgroundService
+{
+    private const int generalDelay = 1 * 10 * 1000 * 3600; // Each hour
+    private readonly ILogger<RefreshProjectService> _logger;
+    private readonly IConfiguration _configuration;
+    public RefreshProjectService(ILogger<RefreshProjectService> logger,
+        IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(generalDelay, stoppingToken);
+            await UpdateProjects();
+        }
+    }
+
+    private Task UpdateProjects()
+    {
+        if (Showtime())
+        {
+            // here i can write logic for taking backup at midnight
+            Console.WriteLine("Executing background task");
+        }
+        return Task.FromResult("Done");
+    }
+
+    private bool Showtime()
+    {
+        var runtime = _configuration.GetSection("HostedService").GetValue<string>("RunTime");
+        var hour = Int32.Parse(runtime.Split(':')[0]);
+        var minute = Int32.Parse(runtime.Split(':')[1]);
+        var second = Int32.Parse(runtime.Split(':')[2]);
+        TimeSpan start = new TimeSpan(hour, minute, second);
+        TimeSpan end = new TimeSpan(hour + 1, minute, second);
+        TimeSpan now = DateTime.Now.TimeOfDay;
+
+        if ((now > start) && (now < end))
+        {
+            _logger.LogInformation("HostingService: Showtime");
+            return true;
+        }
+
+        return false;
+    }
+}
