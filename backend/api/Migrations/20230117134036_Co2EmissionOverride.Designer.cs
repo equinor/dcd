84e5917cf3bdee2fb674173b6c229df2cfc23050
Tord Austad <toaus@equinor.com>
2023-01-17 14:42:20 +0100

    Add migration files

--- backend/api/Migrations/20230117134036_Co2EmissionOverride.Designer.cs
+++ backend/api/Migrations/20230117134036_Co2EmissionOverride.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using api.Context;
 
@@ -11,9 +12,10 @@ using api.Context;
 namespace api.Migrations
 {
     [DbContext(typeof(DcdDbContext))]
-    partial class DcdDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230117134036_Co2EmissionOverride")]
+    partial class Co2EmissionOverride
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
