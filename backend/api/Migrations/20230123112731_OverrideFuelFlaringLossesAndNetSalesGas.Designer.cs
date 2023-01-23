4ac26de3cda2b0622abed1361ff6bc7f47de2db3
Tord Austad <toaus@equinor.com>
2023-01-23 12:28:44 +0100

    Add migration files

--- backend/api/Migrations/20230123112731_OverrideFuelFlaringLossesAndNetSalesGas.Designer.cs
+++ backend/api/Migrations/20230123112731_OverrideFuelFlaringLossesAndNetSalesGas.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using api.Context;
 
@@ -11,9 +12,10 @@ using api.Context;
 namespace api.Migrations
 {
     [DbContext(typeof(DcdDbContext))]
-    partial class DcdDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230123112731_OverrideFuelFlaringLossesAndNetSalesGas")]
+    partial class OverrideFuelFlaringLossesAndNetSalesGas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
