be878e44d6809f7e135f027ba2c7796db0d7ea79
Tord Austad <toaus@equinor.com>
2023-03-22 16:59:34 +0100

    Add PR environment to CORS rules

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -95,7 +95,8 @@ builder.Services.AddCors(options =>
                 "https://pro-s-portal-fprd.azurewebsites.net",
                 "https://fusion-s-portal-ci.azurewebsites.net",
                 "https://fusion-s-portal-fqa.azurewebsites.net",
-                "https://fusion-s-portal-fprd.azurewebsites.net"
+                "https://fusion-s-portal-fprd.azurewebsites.net",
+                "https://pr-3422.fusion-dev.net"
             ).SetIsOriginAllowedToAllowWildcardSubdomains();
         });
 });
