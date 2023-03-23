113087d4609fefeb72a36b38ed1d87f7daa6ca36
Tord Austad <toaus@equinor.com>
2023-03-23 09:48:31 +0100

    Add new fusion urls to CORS

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -95,7 +95,9 @@ builder.Services.AddCors(options =>
                 "https://pro-s-portal-fprd.azurewebsites.net",
                 "https://fusion-s-portal-ci.azurewebsites.net",
                 "https://fusion-s-portal-fqa.azurewebsites.net",
-                "https://fusion-s-portal-fprd.azurewebsites.net"
+                "https://fusion-s-portal-fprd.azurewebsites.net",
+                "https://pr-3422.fusion-dev.net",
+                "https://pr-*.fusion-dev.net"
             ).SetIsOriginAllowedToAllowWildcardSubdomains();
         });
 });
