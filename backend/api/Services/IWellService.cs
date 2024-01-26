457331c922e8378e153c1c180a16cd826dec32e4
Jonas Bach <69851288+JonasABach@users.noreply.github.com>
2024-01-26 15:07:08 +0100

    feature: fixed Backend for delete well

--- backend/api/Services/IWellService.cs
+++ backend/api/Services/IWellService.cs
@@ -7,10 +7,13 @@ namespace api.Services
     {
         WellDto[]? CreateMultipleWells(WellDto[] wellDtos);
         ProjectDto CreateWell(WellDto wellDto);
+        ProjectDto DeleteWell(Guid wellId);
+
         IEnumerable<Well> GetAll();
         IEnumerable<WellDto> GetAllDtos();
         IEnumerable<WellDto> GetDtosForProject(Guid projectId);
         Well GetWell(Guid wellId);
+
         WellDto GetWellDto(Guid wellId);
         WellDto UpdateExistingWell(WellDto updatedWellDto);
         WellDto[] UpdateMultipleWells(WellDto[] updatedWellDtos);
