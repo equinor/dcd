926c2de33e549e1ef6122876a9afd05780a3b6ad
Tord Austad <toaus@equinor.com>
2024-02-15 15:57:26 +0100

    .

---
+++ frontend/vite.config.ts
@@ -0,0 +1,30 @@
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+import { defineConfig } from "vite"
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js"
+import checker from "vite-plugin-checker"
+
+export default defineConfig(({ command }) => {
+    if (command === "build") {
+    // https://github.com/equinor/fusion-framework/issues/997
+        const sourcemap = process.env.GENERATE_SOURCEMAP === "true" ? "inline" : false
+        return {
+            build: {
+                sourcemap,
+                rollupOptions: {
+                    output: {
+                        manualChunks: undefined,
+                    },
+                },
+            },
+            plugins: [viteCommonjs(), cssInjectedByJsPlugin()],
+        }
+    }
+    return {
+        plugins: [
+            checker({
+                typescript: true,
+            }),
+        ],
+        define: { global: "{}" },
+    }
+})
