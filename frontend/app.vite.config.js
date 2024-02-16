0d25550f0a14c51d7a5e058b72a67c8d3419fa81
Tord Austad <toaus@equinor.com>
2024-02-16 05:36:51 +0100

    Fusion deploy (#1018)
    * Zip files before publishing in pipeline
    
    * Add manifest file

---
+++ frontend/app.vite.config.js
@@ -0,0 +1,29 @@
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+import { defineConfig } from 'vite';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import checker from 'vite-plugin-checker';
+
+export default defineConfig(({ command }) => {
+  if (command === 'build') {
+    return {
+      build: {
+        rollupOptions: {
+          output: {
+            inlineDynamicImports: true,
+            manualChunks: undefined
+          }
+        }
+      },
+      plugins: [viteCommonjs(), cssInjectedByJsPlugin()]
+    };
+  }
+  return {
+    plugins: [
+      viteCommonjs(),
+      checker({
+        typescript: true
+      })
+    ],
+    define: { global: '{}' }
+  };
+});
