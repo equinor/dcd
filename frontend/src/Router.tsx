14d5b3e310e77558e2549ee17b4f41cc901c6b55
Arian <89781510+arian-garshi@users.noreply.github.com>
2024-02-17 02:39:33 +0100

    fix: refactored routing

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,9 +1,28 @@
-import { RouterProvider } from "react-router-dom"
+import { RouterProvider, RouteObject } from "react-router-dom"
 import { useRouter } from "@equinor/fusion-framework-react-app/navigation"
+import Overview from "./Components/Overview"
+import ProjectView from "./Views/ProjectView"
+import CaseView from "./Views/CaseView"
+import RouteCoordinator from "./Components/RouteCoordinator"
 
-import routes from "./routes"
+const routes: RouteObject[] = [
+    {
+        path: "/",
+        element: <RouteCoordinator />,
+        children: [
+            {
+                path: ":fusionContextId",
+                element: <Overview />,
+                children: [
+                    { index: true, element: <ProjectView /> },
+                    { path: "case/:caseId", element: <CaseView /> },
+                ],
+            },
+        ],
+    },
+]
 
-export default function () {
+export default function AppRouter() {
     const router = useRouter(routes)
-    return <RouterProvider router={router} fallbackElement={<p>:(</p>} />
+    return <RouterProvider router={router} fallbackElement={<div>Loading...</div>} />
 }
