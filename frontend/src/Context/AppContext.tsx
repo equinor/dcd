f6f6dfe7acb17f1b7bc006accae677327bf0d0a7
JonasABach <jobac@equinor.com>
2024-02-21 10:08:23 +0100

    Change capital letters

--- frontend/src/Context/AppContext.tsx
+++ frontend/src/Context/AppContext.tsx
@@ -5,7 +5,7 @@ import React, {
     useContext,
     useMemo,
 } from "react";
-import { ITimeSeries } from "../models/ITimeSeries";
+import { ITimeSeries } from "../Models/ITimeSeries";
 
 
 // Assuming Components.Schemas.* are defined elsewhere
