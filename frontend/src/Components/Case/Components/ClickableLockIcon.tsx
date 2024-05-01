ed568cf73fc6369f7d25a507b7a27151e44b6860
Arian <89781510+arian-garshi@users.noreply.github.com>
2024-05-01 23:05:40 +0200

    fix: caseTabTable refactor and cell text alignment bugfix

---
+++ frontend/src/Components/Case/Components/ClickableLockIcon.tsx
@@ -0,0 +1,55 @@
+import React, { Dispatch, SetStateAction } from "react"
+import { lock, lock_open } from "@equinor/eds-icons"
+import { Icon } from "@equinor/eds-core-react"
+
+interface LockIconProps {
+    clickedElement: any
+    setOverrideModalOpen: Dispatch<SetStateAction<boolean>>
+    setOverrideModalProfileName: Dispatch<SetStateAction<string>>
+    setOverrideModalProfileSet: Dispatch<SetStateAction<any | undefined>>
+    setOverrideProfile: Dispatch<SetStateAction<any | undefined>>
+}
+
+const LockIcon: React.FC<LockIconProps> = ({
+    clickedElement,
+    setOverrideModalOpen,
+    setOverrideModalProfileName,
+    setOverrideModalProfileSet,
+    setOverrideProfile,
+}) => {
+    const handleLockIconClick = (params: any) => {
+        if (params?.data?.override !== undefined) {
+            setOverrideModalOpen(true)
+            setOverrideModalProfileName(params.data.profileName)
+            setOverrideModalProfileSet(() => params.data.overrideProfileSet)
+            setOverrideProfile(params.data.overrideProfile)
+
+            params.api.redrawRows()
+            params.api.refreshCells()
+        }
+    }
+
+    if (clickedElement.data?.overrideProfileSet !== undefined) {
+        return (clickedElement.data.overrideProfile?.override) ? (
+            <Icon
+                data={lock_open}
+                opacity={0.5}
+                color="#007079"
+                onClick={() => handleLockIconClick(clickedElement)}
+            />
+        )
+            : (
+                <Icon
+                    data={lock}
+                    color="#007079"
+                    onClick={() => handleLockIconClick(clickedElement)}
+                />
+            )
+    }
+    if (!clickedElement?.data?.set) {
+        return <Icon data={lock} color="#007079" />
+    }
+    return null
+}
+
+export default LockIcon
