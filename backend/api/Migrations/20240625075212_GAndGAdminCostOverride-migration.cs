0ef5436225f86b4b00b46dec79a43799849683bd
JonasABach <jobac@equinor.com>
2024-06-25 09:53:44 +0200

    Migrations + dotnet format

---
+++ backend/api/Migrations/20240625075212_GAndGAdminCostOverride-migration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class GAndGAdminCostOverridemigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GAndGAdminCostOverride",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ExplorationId = table.Column<Guid>(name: "Exploration.Id", type: "uniqueidentifier", nullable: false),
+                    StartYear = table.Column<int>(type: "int", nullable: false),
+                    InternalData = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EPAVersion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Currency = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GAndGAdminCostOverride", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GAndGAdminCostOverride_Explorations_Exploration.Id",
+                        column: x => x.ExplorationId,
+                        principalTable: "Explorations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GAndGAdminCostOverride_Exploration.Id",
+                table: "GAndGAdminCostOverride",
+                column: "Exploration.Id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GAndGAdminCostOverride");
+        }
+    }
+}
