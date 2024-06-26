c0a471ca206455f0fa3e8b1d06ed131498dfe9b2
Eleni Martinez <elmar@equinor.com>
2024-06-26 12:54:53 +0200

    Switch styles from .css to module

--- frontend/src/AppComponent.tsx
+++ frontend/src/AppComponent.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react"
-import { createGlobalStyle } from "styled-components"
 import { QueryClient, QueryClientProvider } from "react-query"
 import { ReactQueryDevtools } from "react-query/devtools"
 import { APP_VERSION } from "./version"
@@ -12,16 +11,7 @@ import { ModalContextProvider } from "./Context/ModalContext"
 import { ProjectContextProvider } from "./Context/ProjectContext"
 import { CaseContextProvider } from "./Context/CaseContext"
 import { AppContextProvider } from "./Context/AppContext"
-
-const GlobalStyle = createGlobalStyle`
-    .ag-aria-description-container {
-    display: none !important;
-  }
-
-  .red-cell {
-    background-color: #FFC0C1 !important;
-}
-`
+import Styles from "./styles"
 
 const AppComponent: FC = () => {
     const queryClient = new QueryClient()
@@ -41,7 +31,7 @@ const AppComponent: FC = () => {
                 <ProjectContextProvider>
                     <CaseContextProvider>
                         <ModalContextProvider>
-                            <GlobalStyle />
+                            <Styles />
                             <AppRouter />
                         </ModalContextProvider>
                     </CaseContextProvider>
