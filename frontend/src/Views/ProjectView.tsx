76c044e4431ce6bba7bad9acc71c65285cd684da
JonasABach <jobac@equinor.com>
2024-06-28 12:12:53 +0200

    uncomment undo/redo feature

--- frontend/src/Views/ProjectView.tsx
+++ frontend/src/Views/ProjectView.tsx
@@ -28,7 +28,7 @@ const ProjectView = () => {
                     <List>
                         <Tab>Overview </Tab>
                         <Tab>Compare cases</Tab>
-                        {/* uncomment for next release <Tab>Case edit history</Tab> */}
+                        <Tab>Case edit history</Tab>
                         <Tab>Settings</Tab>
                     </List>
                     <Panels>
@@ -38,11 +38,11 @@ const ProjectView = () => {
                         <Panel>
                             <ProjectCompareCasesTab />
                         </Panel>
-                        {/* uncomment for next release
+
                         <Panel>
                             <EditHistoryOverviewTab />
                         </Panel>
-                        */}
+
                         <Panel>
                             <ProjectSettingsTab />
                         </Panel>
