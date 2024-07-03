1bb1ef9b200090b34202fa538289b8c02f38188b
Arian <89781510+arian-garshi@users.noreply.github.com>
2024-07-03 14:41:59 +0200

    fix: input in tables dont show up sometimes (#1205)

--- frontend/src/Components/Case/Tabs/CaseCost/Tables/ExplorationWellCosts.tsx
+++ frontend/src/Components/Case/Tabs/CaseCost/Tables/ExplorationWellCosts.tsx
@@ -1,111 +1,118 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useProjectContext } from "../../../../../Context/ProjectContext"
 import CaseTabTable from "../../../Components/CaseTabTable"
 import { ITimeSeriesData } from "../../../../../Models/Interfaces"
 
 interface ExplorationWellCostsProps {
-    tableYears: [number, number]
-    explorationWellsGridRef: React.MutableRefObject<any>
-    alignedGridsRef: any[]
-    caseData: Components.Schemas.CaseDto
-    apiData: Components.Schemas.CaseWithAssetsDto | undefined
+    tableYears: [number, number];
+    explorationWellsGridRef: React.MutableRefObject<any>;
+    alignedGridsRef: any[];
+    apiData: Components.Schemas.CaseWithAssetsDto;
 }
 
 const ExplorationWellCosts: React.FC<ExplorationWellCostsProps> = ({
     tableYears,
     explorationWellsGridRef,
     alignedGridsRef,
-    caseData,
     apiData,
 }) => {
     const { project } = useProjectContext()
 
-    const exploration = apiData?.exploration
-    const gAndGAdminCostData = apiData?.gAndGAdminCost
-    const explorationGAndGAdminCostOverrideData = apiData?.gAndGAdminCostOverride
+    const [explorationTimeSeriesData, setExplorationTimeSeriesData] = useState<ITimeSeriesData[]>([])
 
-    const seismicAcquisitionAndProcessingData = apiData?.seismicAcquisitionAndProcessing
-    const countryOfficeCostData = apiData?.countryOfficeCost
-    const explorationWellCostProfileData = apiData?.explorationWellCostProfile
-    const appraisalWellCostProfileData = apiData?.appraisalWellCostProfile
-    const sidetrackCostProfileData = apiData?.sidetrackCostProfile
+    useEffect(() => {
+        const exploration = apiData?.exploration
+        const gAndGAdminCostData = apiData.gAndGAdminCost
+        const explorationGAndGAdminCostOverrideData = apiData.gAndGAdminCostOverride
 
-    if (!exploration) { return null }
+        const seismicAcquisitionAndProcessingData = apiData.seismicAcquisitionAndProcessing
+        const countryOfficeCostData = apiData.countryOfficeCost
+        const explorationWellCostProfileData = apiData.explorationWellCostProfile
+        const appraisalWellCostProfileData = apiData.appraisalWellCostProfile
+        const sidetrackCostProfileData = apiData.sidetrackCostProfile
 
-    const explorationTimeSeriesData: ITimeSeriesData[] = [
-        {
-            profileName: "G&G and admin",
-            unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
-            profile: gAndGAdminCostData,
-            resourceName: "gAndGAdminCost",
-            resourceId: exploration.id,
-            resourceProfileId: explorationGAndGAdminCostOverrideData?.id,
-            resourcePropertyKey: "gAndGAdminCost",
-            editable: true,
-            overridable: true,
-            overrideProfile: explorationGAndGAdminCostOverrideData,
-        },
-        {
-            profileName: "Seismic acquisition and processing",
-            unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
-            profile: seismicAcquisitionAndProcessingData,
-            resourceName: "seismicAcquisitionAndProcessing",
-            resourceId: exploration.id,
-            resourceProfileId: seismicAcquisitionAndProcessingData?.id,
-            resourcePropertyKey: "seismicAcquisitionAndProcessing",
-            editable: true,
-            overridable: false,
-        },
-        {
-            profileName: "Country office",
-            unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
-            profile: countryOfficeCostData,
-            resourceName: "countryOfficeCost",
-            resourceId: exploration.id,
-            resourceProfileId: countryOfficeCostData?.id,
-            resourcePropertyKey: "countryOfficeCost",
-            editable: true,
-            overridable: false,
-        },
-        {
-            profileName: "Exploration well",
-            unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
-            profile: explorationWellCostProfileData,
-            resourceName: "explorationWellCostProfile",
-            resourceId: exploration.id,
-            resourceProfileId: explorationWellCostProfileData?.id,
-            resourcePropertyKey: "explorationWellCostProfile",
-            editable: false,
-            overridable: false,
-        },
-        {
-            profileName: "Appraisal well",
-            unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
-            profile: appraisalWellCostProfileData,
-            resourceName: "appraisalWellCostProfile",
-            resourceId: exploration.id,
-            resourceProfileId: appraisalWellCostProfileData?.id,
-            resourcePropertyKey: "appraisalWellCostProfile",
-            editable: false,
-            overridable: false,
-        },
-        {
-            profileName: "Sidetrack well",
-            unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
-            profile: sidetrackCostProfileData,
-            resourceName: "sidetrackCostProfile",
-            resourceId: exploration.id,
-            resourceProfileId: sidetrackCostProfileData?.id,
-            resourcePropertyKey: "sidetrackCostProfile",
-            editable: false,
-            overridable: false,
-        },
-    ]
+        if (!exploration) {
+            console.error("No exploration data")
+            return
+        }
+
+        const newExplorationTimeSeriesData: ITimeSeriesData[] = [
+            {
+                profileName: "G&G and admin",
+                unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
+                profile: gAndGAdminCostData,
+                resourceName: "gAndGAdminCost",
+                resourceId: exploration.id,
+                resourceProfileId: explorationGAndGAdminCostOverrideData?.id,
+                resourcePropertyKey: "gAndGAdminCost",
+                editable: true,
+                overridable: true,
+                overrideProfile: explorationGAndGAdminCostOverrideData,
+            },
+            {
+                profileName: "Seismic acquisition and processing",
+                unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
+                profile: seismicAcquisitionAndProcessingData,
+                resourceName: "seismicAcquisitionAndProcessing",
+                resourceId: exploration.id,
+                resourceProfileId: seismicAcquisitionAndProcessingData?.id,
+                resourcePropertyKey: "seismicAcquisitionAndProcessing",
+                editable: true,
+                overridable: false,
+            },
+            {
+                profileName: "Country office",
+                unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
+                profile: countryOfficeCostData,
+                resourceName: "countryOfficeCost",
+                resourceId: exploration.id,
+                resourceProfileId: countryOfficeCostData?.id,
+                resourcePropertyKey: "countryOfficeCost",
+                editable: true,
+                overridable: false,
+            },
+            {
+                profileName: "Exploration well",
+                unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
+                profile: explorationWellCostProfileData,
+                resourceName: "explorationWellCostProfile",
+                resourceId: exploration.id,
+                resourceProfileId: explorationWellCostProfileData?.id,
+                resourcePropertyKey: "explorationWellCostProfile",
+                editable: false,
+                overridable: false,
+            },
+            {
+                profileName: "Appraisal well",
+                unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
+                profile: appraisalWellCostProfileData,
+                resourceName: "appraisalWellCostProfile",
+                resourceId: exploration.id,
+                resourceProfileId: appraisalWellCostProfileData?.id,
+                resourcePropertyKey: "appraisalWellCostProfile",
+                editable: false,
+                overridable: false,
+            },
+            {
+                profileName: "Sidetrack well",
+                unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
+                profile: sidetrackCostProfileData,
+                resourceName: "sidetrackCostProfile",
+                resourceId: exploration.id,
+                resourceProfileId: sidetrackCostProfileData?.id,
+                resourcePropertyKey: "sidetrackCostProfile",
+                editable: false,
+                overridable: false,
+            },
+        ]
+
+        setExplorationTimeSeriesData(newExplorationTimeSeriesData)
+    }, [apiData, project])
 
     return (
         <CaseTabTable
             timeSeriesData={explorationTimeSeriesData}
-            dg4Year={caseData?.dG4Date ? new Date(caseData?.dG4Date).getFullYear() : 2030}
+            dg4Year={apiData.case.dG4Date ? new Date(apiData.case.dG4Date).getFullYear() : 2030}
             tableYears={tableYears}
             tableName="Exploration well cost"
             gridRef={explorationWellsGridRef}
--- frontend/src/Components/Case/Tabs/CaseCost/Tables/OpexCosts.tsx
+++ frontend/src/Components/Case/Tabs/CaseCost/Tables/OpexCosts.tsx
@@ -1,4 +1,4 @@
-import React, { } from "react"
+import React, { useEffect, useState } from "react"
 import { useProjectContext } from "../../../../../Context/ProjectContext"
 import CaseTabTable from "../../../Components/CaseTabTable"
 import { ITimeSeriesData } from "../../../../../Models/Interfaces"
@@ -7,87 +7,93 @@ interface OpexCostsProps {
     tableYears: [number, number]
     opexGridRef: React.MutableRefObject<any>
     alignedGridsRef: any[]
-    caseData: Components.Schemas.CaseDto
-    apiData: Components.Schemas.CaseWithAssetsDto | undefined
+    apiData: Components.Schemas.CaseWithAssetsDto
 }
 
 const OpexCosts: React.FC<OpexCostsProps> = ({
-    tableYears, opexGridRef, alignedGridsRef, caseData, apiData,
+    tableYears, opexGridRef, alignedGridsRef, apiData,
 }) => {
     const { project } = useProjectContext()
 
-    const historicCostCostProfileData = apiData?.historicCostCostProfile
-    const wellInterventionCostProfileData = apiData?.wellInterventionCostProfile
-    const wellInterventionCostProfileOverrideData = apiData?.wellInterventionCostProfileOverride
-    const offshoreFacilitiesOperationsCostProfileData = apiData?.offshoreFacilitiesOperationsCostProfile
-    const offshoreFacilitiesOperationsCostProfileOverrideData = apiData?.offshoreFacilitiesOperationsCostProfileOverride
-    const onshoreRelatedOPEXCostProfileData = apiData?.onshoreRelatedOPEXCostProfile
-    const additionalOPEXCostProfileData = apiData?.additionalOPEXCostProfile
+    const [opexTimeSeriesData, setOpexTimeSeriesData] = useState<ITimeSeriesData[]>([])
 
-    const opexTimeSeriesData: ITimeSeriesData[] = [
-        {
-            profileName: "Historic cost",
-            unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
-            profile: historicCostCostProfileData,
-            resourceName: "historicCostCostProfile",
-            resourceId: caseData.id,
-            resourceProfileId: historicCostCostProfileData?.id,
-            resourcePropertyKey: "historicCostCostProfile",
-            editable: true,
-            overridable: false,
-        },
-        {
-            profileName: "Well intervention",
-            unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
-            profile: wellInterventionCostProfileData,
-            resourceName: "wellInterventionCostProfileOverride",
-            resourceId: caseData.id,
-            resourceProfileId: wellInterventionCostProfileOverrideData?.id,
-            resourcePropertyKey: "wellInterventionCostProfileOverride",
-            overridable: true,
-            overrideProfile: wellInterventionCostProfileOverrideData,
-            editable: true,
-        },
-        {
-            profileName: "Offshore facilities operations",
-            unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
-            profile: offshoreFacilitiesOperationsCostProfileData,
-            resourceName: "offshoreFacilitiesOperationsCostProfileOverride",
-            resourceId: caseData.id,
-            resourceProfileId: offshoreFacilitiesOperationsCostProfileOverrideData?.id,
-            resourcePropertyKey: "offshoreFacilitiesOperationsCostProfileOverride",
-            overridable: true,
-            overrideProfile: offshoreFacilitiesOperationsCostProfileOverrideData,
-            editable: true,
-        },
-        {
-            profileName: "Onshore related OPEX (input req.)",
-            unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
-            profile: onshoreRelatedOPEXCostProfileData,
-            resourceName: "onshoreRelatedOPEXCostProfile",
-            resourceId: caseData.id,
-            resourceProfileId: onshoreRelatedOPEXCostProfileData?.id,
-            resourcePropertyKey: "onshoreRelatedOPEXCostProfile",
-            editable: true,
-            overridable: false,
-        },
-        {
-            profileName: "Additional OPEX (input req.)",
-            unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
-            profile: additionalOPEXCostProfileData,
-            resourceName: "additionalOPEXCostProfile",
-            resourceId: caseData.id,
-            resourceProfileId: additionalOPEXCostProfileData?.id,
-            resourcePropertyKey: "additionalOPEXCostProfile",
-            editable: true,
-            overridable: false,
-        },
-    ]
+    useEffect(() => {
+        const historicCostCostProfileData = apiData.historicCostCostProfile
+        const wellInterventionCostProfileData = apiData.wellInterventionCostProfile
+        const wellInterventionCostProfileOverrideData = apiData.wellInterventionCostProfileOverride
+        const offshoreFacilitiesOperationsCostProfileData = apiData.offshoreFacilitiesOperationsCostProfile
+        const offshoreFacilitiesOperationsCostProfileOverrideData = apiData.offshoreFacilitiesOperationsCostProfileOverride
+        const onshoreRelatedOPEXCostProfileData = apiData.onshoreRelatedOPEXCostProfile
+        const additionalOPEXCostProfileData = apiData.additionalOPEXCostProfile
+        const caseData = apiData.case
+
+        const newOpexTimeSeriesData: ITimeSeriesData[] = [
+            {
+                profileName: "Historic cost",
+                unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
+                profile: historicCostCostProfileData,
+                resourceName: "historicCostCostProfile",
+                resourceId: caseData.id,
+                resourceProfileId: historicCostCostProfileData?.id,
+                resourcePropertyKey: "historicCostCostProfile",
+                editable: true,
+                overridable: false,
+            },
+            {
+                profileName: "Well intervention",
+                unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
+                profile: wellInterventionCostProfileData,
+                resourceName: "wellInterventionCostProfileOverride",
+                resourceId: caseData.id,
+                resourceProfileId: wellInterventionCostProfileOverrideData?.id,
+                resourcePropertyKey: "wellInterventionCostProfileOverride",
+                overridable: true,
+                overrideProfile: wellInterventionCostProfileOverrideData,
+                editable: true,
+            },
+            {
+                profileName: "Offshore facilities operations",
+                unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
+                profile: offshoreFacilitiesOperationsCostProfileData,
+                resourceName: "offshoreFacilitiesOperationsCostProfileOverride",
+                resourceId: caseData.id,
+                resourceProfileId: offshoreFacilitiesOperationsCostProfileOverrideData?.id,
+                resourcePropertyKey: "offshoreFacilitiesOperationsCostProfileOverride",
+                overridable: true,
+                overrideProfile: offshoreFacilitiesOperationsCostProfileOverrideData,
+                editable: true,
+            },
+            {
+                profileName: "Onshore related OPEX (input req.)",
+                unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
+                profile: onshoreRelatedOPEXCostProfileData,
+                resourceName: "onshoreRelatedOPEXCostProfile",
+                resourceId: caseData.id,
+                resourceProfileId: onshoreRelatedOPEXCostProfileData?.id,
+                resourcePropertyKey: "onshoreRelatedOPEXCostProfile",
+                editable: true,
+                overridable: false,
+            },
+            {
+                profileName: "Additional OPEX (input req.)",
+                unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
+                profile: additionalOPEXCostProfileData,
+                resourceName: "additionalOPEXCostProfile",
+                resourceId: caseData.id,
+                resourceProfileId: additionalOPEXCostProfileData?.id,
+                resourcePropertyKey: "additionalOPEXCostProfile",
+                editable: true,
+                overridable: false,
+            },
+        ]
+
+        setOpexTimeSeriesData(newOpexTimeSeriesData)
+    }, [apiData, project])
 
     return (
         <CaseTabTable
             timeSeriesData={opexTimeSeriesData}
-            dg4Year={caseData?.dG4Date ? new Date(caseData?.dG4Date).getFullYear() : 2030}
+            dg4Year={apiData.case.dG4Date ? new Date(apiData.case.dG4Date).getFullYear() : 2030}
             tableYears={tableYears}
             tableName="OPEX cost"
             gridRef={opexGridRef}
