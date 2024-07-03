0fda60435bc2b23d4034bec2b51b80c7298fd118
Arian <89781510+arian-garshi@users.noreply.github.com>
2024-07-03 16:03:50 +0200

    fix: undo edits functionality dont have access to case id

--- frontend/src/Components/Controls/Sidebar/Sidebar.tsx
+++ frontend/src/Components/Controls/Sidebar/Sidebar.tsx
@@ -95,7 +95,8 @@ const Sidebar = () => {
                     <Grid item xs={12}>
                         <Divider />
                     </Grid>
-                    {/* uncomment for next release <CurrentCaseEditHistory /> */}
+                    {/* uncomment for next release */}
+                    <CurrentCaseEditHistory />
                 </Grid>
             </StyledSidebarContent>
             <SidebarFooter>
