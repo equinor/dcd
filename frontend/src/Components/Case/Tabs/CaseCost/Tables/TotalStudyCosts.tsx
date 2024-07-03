70a9bcb21caa28d780ffdfdc7c9a5b7fe71b8220
Arian <89781510+arian-garshi@users.noreply.github.com>
2024-07-03 13:03:08 +0200

    fix: add useeffect to keep totalstudycost updated when apidata changes

--- frontend/src/Components/Case/Tabs/CaseCost/Tables/TotalStudyCosts.tsx
+++ frontend/src/Components/Case/Tabs/CaseCost/Tables/TotalStudyCosts.tsx
@@ -1,63 +1,74 @@
-import React, { } from "react"
+import React, { useEffect, useState } from "react"
 import { useProjectContext } from "../../../../../Context/ProjectContext"
 import CaseTabTable from "../../../Components/CaseTabTable"
 import { ITimeSeriesData } from "../../../../../Models/Interfaces"
 
 interface CesationCostsProps {
-    tableYears: [number, number]
-    studyGridRef: React.MutableRefObject<any>
-    alignedGridsRef: any[]
-    caseData: Components.Schemas.CaseDto
-    apiData: Components.Schemas.CaseWithAssetsDto | undefined
+    tableYears: [number, number];
+    studyGridRef: React.MutableRefObject<any>;
+    alignedGridsRef: any[];
+    caseData: Components.Schemas.CaseDto;
+    apiData: Components.Schemas.CaseWithAssetsDto | undefined;
 }
 
 const TotalStudyCosts: React.FC<CesationCostsProps> = ({
-    tableYears, studyGridRef, alignedGridsRef, caseData, apiData,
+    tableYears,
+    studyGridRef,
+    alignedGridsRef,
+    caseData,
+    apiData,
 }) => {
     const { project } = useProjectContext()
-    const totalFeasibilityAndConceptStudiesData = apiData?.totalFeasibilityAndConceptStudies
-    const totalFeasibilityAndConceptStudiesOverrideData = apiData?.totalFeasibilityAndConceptStudiesOverride
-    const totalFEEDStudiesData = apiData?.totalFEEDStudies
-    const totalFEEDStudiesOverrideData = apiData?.totalFEEDStudiesOverride
-    const totalOtherStudiesCostProfileData = apiData?.totalOtherStudiesCostProfile
 
-    const studyTimeSeriesData: ITimeSeriesData[] = [
-        {
-            profileName: "Feasibility & conceptual stud.",
-            unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
-            profile: totalFeasibilityAndConceptStudiesData,
-            resourceName: "totalFeasibilityAndConceptStudiesOverride",
-            resourceId: caseData.id,
-            resourceProfileId: totalFeasibilityAndConceptStudiesOverrideData?.id,
-            resourcePropertyKey: "totalFeasibilityAndConceptStudiesOverride",
-            overridable: true,
-            overrideProfile: totalFeasibilityAndConceptStudiesOverrideData,
-            editable: true,
-        },
-        {
-            profileName: "FEED studies (DG2-DG3)",
-            unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
-            profile: totalFEEDStudiesData,
-            resourceName: "totalFEEDStudiesOverride",
-            resourceId: caseData.id,
-            resourceProfileId: totalFEEDStudiesOverrideData?.id,
-            resourcePropertyKey: "totalFEEDStudiesOverride",
-            overridable: true,
-            overrideProfile: totalFEEDStudiesOverrideData,
-            editable: true,
-        },
-        {
-            profileName: "Other studies",
-            unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
-            profile: totalOtherStudiesCostProfileData,
-            resourceName: "totalOtherStudiesCostProfile",
-            resourceId: caseData?.id,
-            resourceProfileId: totalOtherStudiesCostProfileData?.id,
-            resourcePropertyKey: "totalOtherStudiesCostProfile",
-            editable: true,
-            overridable: false,
-        },
-    ]
+    const [studyTimeSeriesData, setStudyTimeSeriesData] = useState<ITimeSeriesData[]>([])
+
+    useEffect(() => {
+        const totalFeasibilityAndConceptStudiesData = apiData?.totalFeasibilityAndConceptStudies
+        const totalFeasibilityAndConceptStudiesOverrideData = apiData?.totalFeasibilityAndConceptStudiesOverride
+        const totalFEEDStudiesData = apiData?.totalFEEDStudies
+        const totalFEEDStudiesOverrideData = apiData?.totalFEEDStudiesOverride
+        const totalOtherStudiesCostProfileData = apiData?.totalOtherStudiesCostProfile
+
+        const newStudyTimeSeriesData: ITimeSeriesData[] = [
+            {
+                profileName: "Feasibility & conceptual stud.",
+                unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
+                profile: totalFeasibilityAndConceptStudiesData,
+                resourceName: "totalFeasibilityAndConceptStudiesOverride",
+                resourceId: caseData.id,
+                resourceProfileId: totalFeasibilityAndConceptStudiesOverrideData?.id,
+                resourcePropertyKey: "totalFeasibilityAndConceptStudiesOverride",
+                overridable: true,
+                overrideProfile: totalFeasibilityAndConceptStudiesOverrideData,
+                editable: true,
+            },
+            {
+                profileName: "FEED studies (DG2-DG3)",
+                unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
+                profile: totalFEEDStudiesData,
+                resourceName: "totalFEEDStudiesOverride",
+                resourceId: caseData.id,
+                resourceProfileId: totalFEEDStudiesOverrideData?.id,
+                resourcePropertyKey: "totalFEEDStudiesOverride",
+                overridable: true,
+                overrideProfile: totalFEEDStudiesOverrideData,
+                editable: true,
+            },
+            {
+                profileName: "Other studies",
+                unit: `${project?.currency === 1 ? "MNOK" : "MUSD"}`,
+                profile: totalOtherStudiesCostProfileData,
+                resourceName: "totalOtherStudiesCostProfile",
+                resourceId: caseData?.id,
+                resourceProfileId: totalOtherStudiesCostProfileData?.id,
+                resourcePropertyKey: "totalOtherStudiesCostProfile",
+                editable: true,
+                overridable: false,
+            },
+        ]
+
+        setStudyTimeSeriesData(newStudyTimeSeriesData)
+    }, [apiData, project, caseData])
 
     return (
         <CaseTabTable
