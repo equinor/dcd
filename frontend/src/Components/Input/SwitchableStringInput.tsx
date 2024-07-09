d4c6b2ee239406c550f79baa45bd4395ff10c214
Arian <89781510+arian-garshi@users.noreply.github.com>
2024-07-09 10:27:02 +0200

    feat: added switchableStringInput and changed host input type to string

---
+++ frontend/src/Components/Input/SwitchableStringInput.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { Input } from "@equinor/eds-core-react"
+import useDataEdits from "../../Hooks/useDataEdits"
+import { useProjectContext } from "../../Context/ProjectContext"
+import { ResourceName, ResourcePropertyKey } from "../../Models/Interfaces"
+import InputSwitcher from "./Components/InputSwitcher"
+
+interface CaseEditInputProps {
+    label: string;
+    value: string | undefined;
+    resourceName: ResourceName
+    resourcePropertyKey: ResourcePropertyKey
+    resourceId?: string;
+}
+
+const SwitchableStringInput: React.FC<CaseEditInputProps> = ({
+    label,
+    value,
+    resourceName,
+    resourcePropertyKey,
+    resourceId,
+}: CaseEditInputProps) => {
+    const { addEdit } = useDataEdits()
+    const { project } = useProjectContext()
+    const { caseId } = useParams()
+
+    // Local state to manage input value
+    const [inputValue, setInputValue] = useState(value || "")
+
+    // Update local state when value prop changes
+    useEffect(() => {
+        setInputValue(value || "")
+    }, [value])
+
+    const addToEditsAndSubmit = (insertedValue: string) => {
+        if (!caseId || !project) { return }
+
+        addEdit({
+            newValue: insertedValue,
+            previousValue: value,
+            inputLabel: label,
+            projectId: project.id,
+            resourceName,
+            resourcePropertyKey,
+            resourceId,
+            caseId,
+        })
+    }
+
+    return (
+        <InputSwitcher
+            label={label}
+            value={`${inputValue}`}
+        >
+            <Input
+                label={label}
+                value={inputValue}
+                onChange={(e: any) => setInputValue(e.target.value)} // Update local state on change
+                onBlur={(e: any) => addToEditsAndSubmit(e.target.value)}
+            />
+        </InputSwitcher>
+    )
+}
+
+export default SwitchableStringInput
