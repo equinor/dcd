43edb16094b2d43070c319d1ca5abf754baba071
Arian <89781510+arian-garshi@users.noreply.github.com>
2024-09-03 13:43:20 +0200

    added header

--- frontend/src/Components/LoadingSkeletons/CaseCo2TabSkeleton.tsx
+++ frontend/src/Components/LoadingSkeletons/CaseCo2TabSkeleton.tsx
@@ -3,6 +3,17 @@ import { Grid, Skeleton } from "@mui/material"
 
 const CaseCo2TabSkeleton: React.FC = () => (
     <Grid container spacing={2} sx={{ padding: "16px" }}>
+        <Grid item xs={12} container spacing={1} justifyContent="space-between" alignItems="baseline">
+            <Grid item xs={12} md={6} lg={4}>
+                <Skeleton animation="wave" height={60} />
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+                <Skeleton animation="wave" height={60} />
+            </Grid>
+        </Grid>
+        <Grid item xs={12} sx={{ marginBottom: 5, marginTop: -3 }}>
+            <Skeleton animation="wave" height={60} />
+        </Grid>
         <Grid item xs={12}>
             <Skeleton variant="text" animation="wave" width="100%" height={60} />
             <Skeleton variant="text" animation="wave" width="40%" height={60} />
--- frontend/src/Components/LoadingSkeletons/CaseProductionProfilesTabSkeleton.tsx
+++ frontend/src/Components/LoadingSkeletons/CaseProductionProfilesTabSkeleton.tsx
@@ -3,6 +3,17 @@ import { Grid, Skeleton } from "@mui/material"
 
 const CaseProductionProfilesTabSkeleton: React.FC = () => (
     <Grid container spacing={2} sx={{ padding: "16px" }}>
+        <Grid item xs={12} container spacing={1} justifyContent="space-between" alignItems="baseline">
+            <Grid item xs={12} md={6} lg={4}>
+                <Skeleton animation="wave" height={60} />
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+                <Skeleton animation="wave" height={60} />
+            </Grid>
+        </Grid>
+        <Grid item xs={12} sx={{ marginBottom: 5, marginTop: -3 }}>
+            <Skeleton animation="wave" height={60} />
+        </Grid>
         {[...Array(7)].map((_, index) => (
             <Grid item xs={12} md={6} lg={3} key={`menu-item-${index + 1}`}>
                 <Skeleton variant="text" animation="wave" width="80%" height={20} />
