c7576a153b4ee55a8b243de5e56d202895e4fc7e
Daniel Böhme <dboh@equinor.com>
2024-10-08 22:15:59 +0200

    fixed revision chip

--- frontend/src/Components/Controls/RevisionChip.tsx
+++ frontend/src/Components/Controls/RevisionChip.tsx
@@ -1,4 +1,3 @@
-import styled from "styled-components"
 import { Chip, Tooltip, Icon } from "@equinor/eds-core-react"
 import { useState } from "react"
 import { Typography } from "@mui/material"
@@ -6,47 +5,39 @@ import { close } from "@equinor/eds-icons"
 import RevisionDetailsModal from "./RevisionDetailsModal"
 import { useProjectContext } from "../../Context/ProjectContext"
 
-const StyledChip = styled(Chip)`
-    border-width: 0;
-    font-size: 1rem;
-    line-height: 1.8rem;
-    height: auto;
-    padding: 0 0.7rem 0 0.5rem;
-    cursor: help;
-    color: #797979;
-    svg {
-        fill: #358132;
-    }
-`
-
-const SmallTooltip = styled(Tooltip)`
-    white-space: pre-wrap !important;
-    max-width: 300px !important;
-    font-size: 1rem !important;
-    text-align: center !important;
-    div[class*="Arrow"] {
-        top: -10px !important;
-    }
-`
-
 const RevisionChip = () => {
     const { setIsRevision } = useProjectContext()
     const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [showCloseIcon, setShowCloseIcon] = useState(false)
+
+    const handleMouseOver = () => {
+        setShowCloseIcon(true)
+    }
+
+    const handleMouseOut = () => {
+        setShowCloseIcon(false)
+    }
+
+    const revisionName = () => (
+        <Tooltip title="View details">
+            <Typography onClick={() => setIsMenuOpen(true)} variant="body2">
+                APx Rev 1
+            </Typography>
+        </Tooltip>
+    )
 
     return (
         <>
-            <SmallTooltip placement="bottom-start" title="View details">
-                <StyledChip
-                    variant="default"
-
-                >
-                    <Typography onClick={() => setIsMenuOpen(true)} variant="body2">
-                        APx Rev 1
-                    </Typography>
-                    <Icon data={close} onClick={() => setIsRevision(false)} />
-
-                </StyledChip>
-            </SmallTooltip>
+            <Chip onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+                {!showCloseIcon ? revisionName() : (
+                    <>
+                        {revisionName()}
+                        <Tooltip title="Exit revision">
+                            <Icon data={close} size={16} onClick={() => setIsRevision(false)} />
+                        </Tooltip>
+                    </>
+                )}
+            </Chip>
             <RevisionDetailsModal isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
         </>
     )
