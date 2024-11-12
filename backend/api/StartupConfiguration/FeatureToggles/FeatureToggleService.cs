bff522f87f2f2642aee554932582eaca57d5ad5e
Daniel Böhme <dboh@equinor.com>
2024-11-12 13:50:47 +0100

    Activate Revision for CI

--- backend/api/StartupConfiguration/FeatureToggles/FeatureToggleService.cs
+++ backend/api/StartupConfiguration/FeatureToggles/FeatureToggleService.cs
@@ -11,5 +11,5 @@ public static class FeatureToggleService
         };
     }
 
-    public static readonly bool RevisionEnabled = DcdEnvironments.CurrentEnvironment is DcdEnvironments.LocalDev;
+    public static readonly bool RevisionEnabled = DcdEnvironments.CurrentEnvironment is DcdEnvironments.LocalDev or DcdEnvironments.Ci;
 }
