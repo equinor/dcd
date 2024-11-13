5aa16dbf376733098c09d57fdd0b9d01596d057d
kvam <frank.kvam@gmail.com>
2024-11-13 09:50:53 +0100

    Cleanup environments

--- backend/api/StartupConfiguration/DcdEnvironments.cs
+++ backend/api/StartupConfiguration/DcdEnvironments.cs
@@ -5,11 +5,20 @@ public static class DcdEnvironments
     public static string CurrentEnvironment { get; set; } = null!;
 
     public const string LocalDev = "localdev";
+
     public const string Ci = "CI";
-    public const string RadixDev = "radix-dev";
+
     public const string Dev = "dev";
+    public const string RadixDev = "radix-dev";
+
     public const string Qa = "qa";
     public const string RadixQa = "radix-qa";
+
     public const string Prod = "prod";
     public const string RadixProd = "radix-prod";
+
+    public static bool IsLocal() => CurrentEnvironment is LocalDev;
+    public static bool IsCi() => CurrentEnvironment is Ci or Dev or RadixDev;
+    public static bool IsQa() => CurrentEnvironment is Qa or RadixQa;
+    public static bool IsProd() => CurrentEnvironment is Prod or RadixProd;
 }
