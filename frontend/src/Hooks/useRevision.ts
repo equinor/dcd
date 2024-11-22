3b30ed6c5e2e53fff9840499336d202bfa8f7f38
JonasABach <jobac@equinor.com>
2024-11-22 12:49:20 +0100

    Update useRevision.ts

--- frontend/src/Hooks/useRevision.ts
+++ frontend/src/Hooks/useRevision.ts
@@ -1,10 +1,11 @@
-import { useState } from "react"
-import { useQueryClient } from "@tanstack/react-query"
+import { useEffect, useState } from "react"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
 import { useNavigate, useParams } from "react-router-dom"
 import { useModuleCurrentContext } from "@equinor/fusion-framework-react-module-context"
 
 import { GetProjectService } from "@/Services/ProjectService"
 import { useProjectContext } from "@/Context/ProjectContext"
+import { projectQueryFn } from "@/Services/QueryFunctions"
 
 export const useRevisions = () => {
     const { isRevision, setIsRevision, projectId } = useProjectContext()
@@ -53,6 +54,29 @@ export const useRevisions = () => {
     const disableCurrentRevision = (
         currentRevisionId: string | undefined,
     ) => isRevision && currentRevisionId === revisionId
+
+    const externalId = currentContext?.externalId
+    const { data: projectApiData } = useQuery({
+        queryKey: ["projectApiData", externalId],
+        queryFn: () => projectQueryFn(externalId),
+        enabled: !!externalId,
+    })
+
+    useEffect(() => {
+        let timeoutId: NodeJS.Timeout | undefined
+
+        if (projectApiData && !projectApiData?.revisionsDetailsList.find((r: Components.Schemas.RevisionDetailsDto) => r.revisionId === revisionId)) {
+            timeoutId = setTimeout(() => {
+                exitRevisionView()
+            }, 150)
+        }
+
+        return () => {
+            if (timeoutId) {
+                clearTimeout(timeoutId)
+            }
+        }
+    }, [projectApiData])
 
     return {
         isRevisionsLoading,
