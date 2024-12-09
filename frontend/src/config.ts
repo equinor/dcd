017f34868e954c926b144cc716bad0b4f570a5f3
Trym Helle <tryhe@equinor.com>
2024-12-09 11:44:35 +0100

    remove local aggrid workaround

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -13,7 +13,7 @@ import { MultiFilterModule } from "@ag-grid-enterprise/multi-filter"
 import { RangeSelectionModule } from "@ag-grid-enterprise/range-selection"
 import { SetFilterModule } from "@ag-grid-enterprise/set-filter"
 import { ExcelExportModule } from "@ag-grid-enterprise/excel-export"
-import { agGridLicenseKey } from "./agGridLicense"
+// import { agGridLicenseKey } from "./agGridLicense"
 
 export const configure: AppModuleInitiator = (configurator, args) => {
     // const { agGridLicense } = (args.env.config?.environment as { agGridLicense?: string })
@@ -32,21 +32,15 @@ export const configure: AppModuleInitiator = (configurator, args) => {
         ExcelExportModule,
     ])
 
-    // if (agGridLicense && agGridLicense.length > 0) {
+    // if (agGridLicenseKey && agGridLicenseKey.length > 0) {
     //     enableAgGrid(configurator, {
-    //         licenseKey: agGridLicense || "",
+    //         licenseKey: agGridLicenseKey || "",
     //     })
     // } else {
     //     enableAgGrid(configurator)
     // }
 
-    if (agGridLicenseKey && agGridLicenseKey.length > 0) {
-        enableAgGrid(configurator, {
-            licenseKey: agGridLicenseKey || "",
-        })
-    } else {
-        enableAgGrid(configurator)
-    }
+    enableAgGrid(configurator)
 
     configurator.useFrameworkServiceClient("portal")
 
