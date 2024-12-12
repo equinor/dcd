dc3c8dd92dc1a67466f1336540f9fb0b03d81e97
kvam <frank.kvam@gmail.com>
2024-12-12 09:43:55 +0100

    Handle dbset defaults according to MS documentation best practices

--- backend/api/Context/DcdDbContext.cs
+++ backend/api/Context/DcdDbContext.cs
@@ -1155,103 +1155,81 @@ public class DcdDbContext(DbContextOptions<DcdDbContext> options, IServiceProvid
             || caseChanges;
     }
 
-    public DbSet<Project> Projects { get; set; } = null!;
-    public DbSet<ProjectMember> ProjectMembers { get; set; } = null!;
-    public DbSet<RevisionDetails> RevisionDetails { get; set; } = null!;
-    public DbSet<ExplorationOperationalWellCosts> ExplorationOperationalWellCosts { get; set; } = null!;
-    public DbSet<DevelopmentOperationalWellCosts> DevelopmentOperationalWellCosts { get; set; } = null!;
-
-    public DbSet<Case> Cases { get; set; } = null!;
-    public DbSet<CessationWellsCostOverride> CessationWellsCostOverride { get; set; } = null!;
-    public DbSet<CessationOffshoreFacilitiesCostOverride> CessationOffshoreFacilitiesCostOverride { get; set; } = null!;
-    public DbSet<CessationOnshoreFacilitiesCostProfile> CessationOnshoreFacilitiesCostProfile { get; set; } = null!;
-    public DbSet<TotalFeasibilityAndConceptStudiesOverride> TotalFeasibilityAndConceptStudiesOverride { get; set; } = null!;
-    public DbSet<TotalFEEDStudiesOverride> TotalFEEDStudiesOverride { get; set; } = null!;
-    public DbSet<TotalOtherStudiesCostProfile> TotalOtherStudiesCostProfile { get; set; } = null!;
-    public DbSet<HistoricCostCostProfile> HistoricCostCostProfile { get; set; } = null!;
-    public DbSet<WellInterventionCostProfileOverride> WellInterventionCostProfileOverride { get; set; } = null!;
-    public DbSet<OffshoreFacilitiesOperationsCostProfileOverride> OffshoreFacilitiesOperationsCostProfileOverride { get; set; } = null!;
-    public DbSet<OnshoreRelatedOPEXCostProfile> OnshoreRelatedOPEXCostProfile { get; set; } = null!;
-    public DbSet<AdditionalOPEXCostProfile> AdditionalOPEXCostProfile { get; set; } = null!;
-    public DbSet<Image> Images { get; set; } = null!;
-
-    public DbSet<Well> Wells { get; set; } = null!;
-    public DbSet<WellProjectWell> WellProjectWell { get; set; } = null!;
-    public DbSet<ExplorationWell> ExplorationWell { get; set; } = null!;
-
-    public DbSet<Surf> Surfs { get; set; } = null!;
-    public DbSet<SurfCostProfile> SurfCostProfile { get; set; } = null!;
-    public DbSet<SurfCostProfileOverride> SurfCostProfileOverride { get; set; } = null!;
-    public DbSet<SurfCessationCostProfile> SurfCessationCostProfiles { get; set; } = null!;
-
-    public DbSet<Substructure> Substructures { get; set; } = null!;
-    public DbSet<SubstructureCostProfile> SubstructureCostProfiles { get; set; } = null!;
-    public DbSet<SubstructureCostProfileOverride> SubstructureCostProfileOverride { get; set; } = null!;
-    public DbSet<SubstructureCessationCostProfile> SubstructureCessationCostProfiles { get; set; } = null!;
-
-    public DbSet<Topside> Topsides { get; set; } = null!;
-    public DbSet<TopsideCostProfile> TopsideCostProfiles { get; set; } = null!;
-    public DbSet<TopsideCostProfileOverride> TopsideCostProfileOverride { get; set; } = null!;
-    public DbSet<TopsideCessationCostProfile> TopsideCessationCostProfiles { get; set; } = null!;
-
-    public DbSet<Transport> Transports { get; set; } = null!;
-    public DbSet<TransportCostProfile> TransportCostProfile { get; set; } = null!;
-    public DbSet<TransportCostProfileOverride> TransportCostProfileOverride { get; set; } = null!;
-    public DbSet<TransportCessationCostProfile> TransportCessationCostProfiles { get; set; } = null!;
-
-    public DbSet<OnshorePowerSupply> OnshorePowerSupplies { get; set; } = null!;
-    public DbSet<OnshorePowerSupplyCostProfile> OnshorePowerSupplyCostProfile { get; set; } = null!;
-    public DbSet<OnshorePowerSupplyCostProfileOverride> OnshorePowerSupplyCostProfileOverride { get; set; } = null!;
-
-    public DbSet<DrainageStrategy> DrainageStrategies { get; set; } = null!;
-    public DbSet<ProductionProfileOil> ProductionProfileOil { get; set; } = null!;
-    public DbSet<AdditionalProductionProfileOil> AdditionalProductionProfileOil { get; set; } = null!;
-
-    public DbSet<ProductionProfileGas> ProductionProfileGas { get; set; } = null!;
-    public DbSet<AdditionalProductionProfileGas> AdditionalProductionProfileGas { get; set; } = null!;
-
-    public DbSet<ProductionProfileWater> ProductionProfileWater { get; set; } = null!;
-    public DbSet<ProductionProfileWaterInjection> ProductionProfileWaterInjection { get; set; } = null!;
-
-    public DbSet<FuelFlaringAndLosses> FuelFlaringAndLosses { get; set; } = null!;
-    public DbSet<FuelFlaringAndLossesOverride> FuelFlaringAndLossesOverride { get; set; } = null!;
-
-    public DbSet<NetSalesGas> NetSalesGas { get; set; } = null!;
-    public DbSet<NetSalesGasOverride> NetSalesGasOverride { get; set; } = null!;
-
-    public DbSet<Co2Emissions> Co2Emissions { get; set; } = null!;
-    public DbSet<Co2EmissionsOverride> Co2EmissionsOverride { get; set; } = null!;
-
-    public DbSet<ProductionProfileNGL> ProductionProfileNGL { get; set; } = null!;
-
-    public DbSet<ImportedElectricity> ImportedElectricity { get; set; } = null!;
-    public DbSet<ImportedElectricityOverride> ImportedElectricityOverride { get; set; } = null!;
-
-    public DbSet<DeferredOilProduction> DeferredOilProduction { get; set; } = null!;
-    public DbSet<DeferredGasProduction> DeferredGasProduction { get; set; } = null!;
-
-    public DbSet<WellProject> WellProjects { get; set; } = null!;
-    public DbSet<OilProducerCostProfileOverride> OilProducerCostProfileOverride { get; set; } = null!;
-    public DbSet<GasProducerCostProfileOverride> GasProducerCostProfileOverride { get; set; } = null!;
-    public DbSet<WaterInjectorCostProfileOverride> WaterInjectorCostProfileOverride { get; set; } = null!;
-    public DbSet<GasInjectorCostProfileOverride> GasInjectorCostProfileOverride { get; set; } = null!;
-
-    public DbSet<DrillingSchedule> DrillingSchedule { get; set; } = null!;
-
-    public DbSet<Exploration> Explorations { get; set; } = null!;
-    public DbSet<GAndGAdminCost> GAndGAdminCost { get; set; } = null!;
-    public DbSet<SeismicAcquisitionAndProcessing> SeismicAcquisitionAndProcessing { get; set; } = null!;
-    public DbSet<CountryOfficeCost> CountryOfficeCost { get; set; } = null!;
-    public DbSet<GAndGAdminCostOverride> GAndGAdminCostOverride { get; set; } = null!;
-    public DbSet<ExplorationWellCostProfile> ExplorationWellCostProfile { get; set; } = null!;
-    public DbSet<AppraisalWellCostProfile> AppraisalWellCostProfile { get; set; } = null!;
-    public DbSet<SidetrackCostProfile> SidetrackCostProfile { get; set; } = null!;
-
-    public DbSet<CalculatedTotalIncomeCostProfile> CalculatedTotalIncomeCostProfile { get; set; } = null!;
-    public DbSet<CalculatedTotalCostCostProfile> CalculatedTotalCostCostProfile { get; set; } = null!;
-
-    public DbSet<ChangeLog> ChangeLogs { get; set; } = null!;
-
+    public DbSet<Project> Projects => Set<Project>();
+    public DbSet<ProjectMember> ProjectMembers => Set<ProjectMember>();
+    public DbSet<RevisionDetails> RevisionDetails => Set<RevisionDetails>();
+    public DbSet<ExplorationOperationalWellCosts> ExplorationOperationalWellCosts => Set<ExplorationOperationalWellCosts>();
+    public DbSet<DevelopmentOperationalWellCosts> DevelopmentOperationalWellCosts => Set<DevelopmentOperationalWellCosts>();
+    public DbSet<Case> Cases => Set<Case>();
+    public DbSet<CessationWellsCostOverride> CessationWellsCostOverride => Set<CessationWellsCostOverride>();
+    public DbSet<CessationOffshoreFacilitiesCostOverride> CessationOffshoreFacilitiesCostOverride => Set<CessationOffshoreFacilitiesCostOverride>();
+    public DbSet<CessationOnshoreFacilitiesCostProfile> CessationOnshoreFacilitiesCostProfile => Set<CessationOnshoreFacilitiesCostProfile>();
+    public DbSet<TotalFeasibilityAndConceptStudiesOverride> TotalFeasibilityAndConceptStudiesOverride => Set<TotalFeasibilityAndConceptStudiesOverride>();
+    public DbSet<TotalFEEDStudiesOverride> TotalFEEDStudiesOverride => Set<TotalFEEDStudiesOverride>();
+    public DbSet<TotalOtherStudiesCostProfile> TotalOtherStudiesCostProfile => Set<TotalOtherStudiesCostProfile>();
+    public DbSet<HistoricCostCostProfile> HistoricCostCostProfile => Set<HistoricCostCostProfile>();
+    public DbSet<WellInterventionCostProfileOverride> WellInterventionCostProfileOverride => Set<WellInterventionCostProfileOverride>();
+    public DbSet<OffshoreFacilitiesOperationsCostProfileOverride> OffshoreFacilitiesOperationsCostProfileOverride => Set<OffshoreFacilitiesOperationsCostProfileOverride>();
+    public DbSet<OnshoreRelatedOPEXCostProfile> OnshoreRelatedOPEXCostProfile => Set<OnshoreRelatedOPEXCostProfile>();
+    public DbSet<AdditionalOPEXCostProfile> AdditionalOPEXCostProfile => Set<AdditionalOPEXCostProfile>();
+    public DbSet<Image> Images => Set<Image>();
+    public DbSet<Well> Wells => Set<Well>();
+    public DbSet<WellProjectWell> WellProjectWell => Set<WellProjectWell>();
+    public DbSet<ExplorationWell> ExplorationWell => Set<ExplorationWell>();
+    public DbSet<Surf> Surfs => Set<Surf>();
+    public DbSet<SurfCostProfile> SurfCostProfile => Set<SurfCostProfile>();
+    public DbSet<SurfCostProfileOverride> SurfCostProfileOverride => Set<SurfCostProfileOverride>();
+    public DbSet<SurfCessationCostProfile> SurfCessationCostProfiles => Set<SurfCessationCostProfile>();
+    public DbSet<Substructure> Substructures => Set<Substructure>();
+    public DbSet<SubstructureCostProfile> SubstructureCostProfiles => Set<SubstructureCostProfile>();
+    public DbSet<SubstructureCostProfileOverride> SubstructureCostProfileOverride => Set<SubstructureCostProfileOverride>();
+    public DbSet<SubstructureCessationCostProfile> SubstructureCessationCostProfiles => Set<SubstructureCessationCostProfile>();
+    public DbSet<Topside> Topsides => Set<Topside>();
+    public DbSet<TopsideCostProfile> TopsideCostProfiles => Set<TopsideCostProfile>();
+    public DbSet<TopsideCostProfileOverride> TopsideCostProfileOverride => Set<TopsideCostProfileOverride>();
+    public DbSet<TopsideCessationCostProfile> TopsideCessationCostProfiles => Set<TopsideCessationCostProfile>();
+    public DbSet<Transport> Transports => Set<Transport>();
+    public DbSet<TransportCostProfile> TransportCostProfile => Set<TransportCostProfile>();
+    public DbSet<TransportCostProfileOverride> TransportCostProfileOverride => Set<TransportCostProfileOverride>();
+    public DbSet<TransportCessationCostProfile> TransportCessationCostProfiles => Set<TransportCessationCostProfile>();
+    public DbSet<OnshorePowerSupply> OnshorePowerSupplies => Set<OnshorePowerSupply>();
+    public DbSet<OnshorePowerSupplyCostProfile> OnshorePowerSupplyCostProfile => Set<OnshorePowerSupplyCostProfile>();
+    public DbSet<OnshorePowerSupplyCostProfileOverride> OnshorePowerSupplyCostProfileOverride => Set<OnshorePowerSupplyCostProfileOverride>();
+    public DbSet<DrainageStrategy> DrainageStrategies => Set<DrainageStrategy>();
+    public DbSet<ProductionProfileOil> ProductionProfileOil => Set<ProductionProfileOil>();
+    public DbSet<AdditionalProductionProfileOil> AdditionalProductionProfileOil => Set<AdditionalProductionProfileOil>();
+    public DbSet<ProductionProfileGas> ProductionProfileGas => Set<ProductionProfileGas>();
+    public DbSet<AdditionalProductionProfileGas> AdditionalProductionProfileGas => Set<AdditionalProductionProfileGas>();
+    public DbSet<ProductionProfileWater> ProductionProfileWater => Set<ProductionProfileWater>();
+    public DbSet<ProductionProfileWaterInjection> ProductionProfileWaterInjection => Set<ProductionProfileWaterInjection>();
+    public DbSet<FuelFlaringAndLosses> FuelFlaringAndLosses => Set<FuelFlaringAndLosses>();
+    public DbSet<FuelFlaringAndLossesOverride> FuelFlaringAndLossesOverride => Set<FuelFlaringAndLossesOverride>();
+    public DbSet<NetSalesGas> NetSalesGas => Set<NetSalesGas>();
+    public DbSet<NetSalesGasOverride> NetSalesGasOverride => Set<NetSalesGasOverride>();
+    public DbSet<Co2Emissions> Co2Emissions => Set<Co2Emissions>();
+    public DbSet<Co2EmissionsOverride> Co2EmissionsOverride => Set<Co2EmissionsOverride>();
+    public DbSet<ProductionProfileNGL> ProductionProfileNGL => Set<ProductionProfileNGL>();
+    public DbSet<ImportedElectricity> ImportedElectricity => Set<ImportedElectricity>();
+    public DbSet<ImportedElectricityOverride> ImportedElectricityOverride => Set<ImportedElectricityOverride>();
+    public DbSet<DeferredOilProduction> DeferredOilProduction => Set<DeferredOilProduction>();
+    public DbSet<DeferredGasProduction> DeferredGasProduction => Set<DeferredGasProduction>();
+    public DbSet<WellProject> WellProjects => Set<WellProject>();
+    public DbSet<OilProducerCostProfileOverride> OilProducerCostProfileOverride => Set<OilProducerCostProfileOverride>();
+    public DbSet<GasProducerCostProfileOverride> GasProducerCostProfileOverride => Set<GasProducerCostProfileOverride>();
+    public DbSet<WaterInjectorCostProfileOverride> WaterInjectorCostProfileOverride => Set<WaterInjectorCostProfileOverride>();
+    public DbSet<GasInjectorCostProfileOverride> GasInjectorCostProfileOverride => Set<GasInjectorCostProfileOverride>();
+    public DbSet<DrillingSchedule> DrillingSchedule => Set<DrillingSchedule>();
+    public DbSet<Exploration> Explorations => Set<Exploration>();
+    public DbSet<GAndGAdminCost> GAndGAdminCost => Set<GAndGAdminCost>();
+    public DbSet<SeismicAcquisitionAndProcessing> SeismicAcquisitionAndProcessing => Set<SeismicAcquisitionAndProcessing>();
+    public DbSet<CountryOfficeCost> CountryOfficeCost => Set<CountryOfficeCost>();
+    public DbSet<GAndGAdminCostOverride> GAndGAdminCostOverride => Set<GAndGAdminCostOverride>();
+    public DbSet<ExplorationWellCostProfile> ExplorationWellCostProfile => Set<ExplorationWellCostProfile>();
+    public DbSet<AppraisalWellCostProfile> AppraisalWellCostProfile => Set<AppraisalWellCostProfile>();
+    public DbSet<SidetrackCostProfile> SidetrackCostProfile => Set<SidetrackCostProfile>();
+    public DbSet<CalculatedTotalIncomeCostProfile> CalculatedTotalIncomeCostProfile => Set<CalculatedTotalIncomeCostProfile>();
+    public DbSet<CalculatedTotalCostCostProfile> CalculatedTotalCostCostProfile => Set<CalculatedTotalCostCostProfile>();
+    public DbSet<ChangeLog> ChangeLogs => Set<ChangeLog>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
