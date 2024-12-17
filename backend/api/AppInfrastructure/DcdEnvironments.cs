954e95bfdc668d13091d39191d63338d6cb8f0ed
kvam <frank.kvam@gmail.com>
2024-12-17 20:48:04 +0100

    further simplification

--- backend/api/AppInfrastructure/DcdEnvironments.cs
+++ backend/api/AppInfrastructure/DcdEnvironments.cs
@@ -14,11 +14,11 @@ public static class DcdEnvironments
     public static bool UseSqlite => CurrentEnvironment is LocalDev;
     public static bool EnableSwagger => CurrentEnvironment is LocalDev or Ci or RadixDev;
     public static bool RunProjectMasterBackgroundServiceHourly => CurrentEnvironment is not RadixProd;
-    public static bool EnableVerboseEntityFrameworkLogging => false; // CurrentEnvironment is LocalDev || CurrentEnvironment is Ci or RadixDev;
-    public static bool ReturnExceptionDetails => CurrentEnvironment is LocalDev or Ci or RadixDev or RadixQa;
+    public static bool EnableVerboseEntityFrameworkLogging => false; // CurrentEnvironment is LocalDev;
+    public static bool ReturnExceptionDetails => CurrentEnvironment is not RadixProd;
     public static bool AllowMigrationsToBeApplied => CurrentEnvironment is RadixDev or RadixQa or RadixProd;
 
     // Feature toggles go here
-    public static bool RevisionEnabled => CurrentEnvironment is LocalDev or Ci or RadixDev or RadixQa;
-    public static bool DisplayAllFusionUsersAsPmt => CurrentEnvironment is LocalDev or Ci or RadixDev or RadixQa;
+    public static bool RevisionEnabled => CurrentEnvironment is not RadixProd;
+    public static bool DisplayAllFusionUsersAsPmt => CurrentEnvironment is not RadixProd;
 }
