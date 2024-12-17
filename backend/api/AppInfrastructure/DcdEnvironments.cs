945670b755d6a5cfff6b036293e645d6d9c6309d
kvam <frank.kvam@gmail.com>
2024-12-17 19:03:32 +0100

    models generated from radixdev

--- backend/api/AppInfrastructure/DcdEnvironments.cs
+++ backend/api/AppInfrastructure/DcdEnvironments.cs
@@ -12,7 +12,7 @@ public static class DcdEnvironments
 
     public static bool EnableVerboseEntityFrameworkLogging => false; // CurrentEnvironment is LocalDev or Ci;
     public static bool UseSqlite => CurrentEnvironment is LocalDev;
-    public static bool DisplaySwagger => CurrentEnvironment is LocalDev or Ci;
+    public static bool DisplaySwagger => CurrentEnvironment is LocalDev or Ci or RadixDev;
     public static bool ReturnExceptionDetails => CurrentEnvironment is LocalDev or Ci or RadixDev or RadixQa;
     public static bool AllowMigrationsToBeApplied => CurrentEnvironment is RadixDev or RadixQa or RadixProd;
     public static bool RunProjectMasterBackgroundServiceHourly => CurrentEnvironment is LocalDev or Ci or RadixDev;
