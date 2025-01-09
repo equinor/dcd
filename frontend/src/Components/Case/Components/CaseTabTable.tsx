aad9263e3a598c5d5d3c6b69fe80163b70be2160
Arian <89781510+arian-garshi@users.noreply.github.com>
2025-01-09 09:38:01 +0100

    refactor

--- frontend/src/Components/Case/Components/CaseTabTable.tsx
+++ frontend/src/Components/Case/Components/CaseTabTable.tsx
@@ -3,13 +3,13 @@ import {
     useState,
     useEffect,
     useCallback,
-    useRef,
 } from "react"
 import { AgGridReact } from "@ag-grid-community/react"
 import useStyles from "@equinor/fusion-react-ag-grid-styles"
 import { useParams } from "react-router"
 import {
-    CellKeyDownEvent, ColDef, GridReadyEvent,
+    ColDef,
+    GridReadyEvent,
     ICellRendererParams,
 } from "@ag-grid-community/core"
 import isEqual from "lodash/isEqual"
@@ -17,13 +17,14 @@ import { CircularProgress } from "@equinor/eds-core-react"
 import styled from "styled-components"
 
 import {
-    extractTableTimeSeriesValues,
+    getValuesFromEntireRow,
     generateProfile,
     tableCellisEditable,
     numberValueParser,
     getCaseRowStyle,
     validateInput,
     formatColumnSum,
+    roundToFourDecimalsAndJoin,
 } from "@/Utils/common"
 import { useAppContext } from "@/Context/AppContext"
 import { useProjectContext } from "@/Context/ProjectContext"
@@ -32,10 +33,10 @@ import profileAndUnitInSameCell from "./ProfileAndUnitInSameCell"
 import ErrorCellRenderer from "./ErrorCellRenderer"
 import ClickableLockIcon from "./ClickableLockIcon"
 import {
-    ITimeSeriesTableData,
     ITimeSeriesTableDataOverrideWithSet,
     ITimeSeriesTableDataWithSet,
 } from "@/Models/ITimeSeries"
+import { gridRefArrayToAlignedGrid, profilesToRowData } from "@/Components/AgGrid/AgGridHelperFunctions"
 
 interface Props {
     timeSeriesData: ITimeSeriesTableDataWithSet[]
@@ -78,8 +79,6 @@ const CaseTabTable = ({
     const styles = useStyles()
     const { caseId, tab } = useParams()
     const [stagedEdit, setStagedEdit] = useState<any>()
-    const firstTriggerRef = useRef<boolean>(true)
-    const timerRef = useRef<NodeJS.Timeout | null>(null)
     const { projectId } = useProjectContext()
 
     useEffect(() => {
@@ -92,73 +91,7 @@ const CaseTabTable = ({
         gridRef.current?.api?.redrawRows()
     }, [ongoingCalculation])
 
-    const profilesToRowData = () => {
-        const tableRows: ITimeSeriesTableData[] = []
-        timeSeriesData.forEach((ts) => {
-            const isOverridden = ts.overrideProfile?.override === true
-            const rowObject: any = {}
-            const { profileName, unit } = ts
-            rowObject.profileName = profileName
-            rowObject.unit = unit
-            rowObject.total = ts.total ?? 0
-            rowObject.set = isOverridden ? ts.overrideProfileSet : ts.set
-            rowObject.profile = isOverridden ? ts.overrideProfile : ts.profile
-            rowObject.override = ts.overrideProfile?.override === true
-            rowObject.resourceId = ts.resourceId
-            rowObject.resourceName = ts.resourceName
-            rowObject.overridable = ts.overridable
-            rowObject.editable = ts.editable
-            rowObject.hideIfEmpty = ts.hideIfEmpty
-
-            rowObject.overrideProfileSet = ts.overrideProfileSet
-            rowObject.overrideProfile = ts.overrideProfile ?? {
-                startYear: 0, values: [], override: false,
-            }
-
-            if (rowObject.profile && rowObject.profile.values?.length > 0) {
-                let j = 0
-                if (tableName === "Production profiles" || tableName === "CO2 emissions") {
-                    for (let i = rowObject.profile.startYear;
-                        i < rowObject.profile.startYear + rowObject.profile.values.length;
-                        i += 1) {
-                        rowObject[(dg4Year + i).toString()] = rowObject.profile.values.map(
-                            (v: number) => Math.round((v + Number.EPSILON) * 10000) / 10000,
-                        )[j]
-                        j += 1
-                        rowObject.total = Math.round(rowObject.profile.values.map(
-                            (v: number) => (v + Number.EPSILON),
-                        ).reduce((x: number, y: number) => x + y) * 10000) / 10000
-                        if (ts.total !== undefined) {
-                            rowObject.total = Math.round(Number(ts.total) * 1000) / 1000
-                        }
-                    }
-                } else {
-                    for (let i = rowObject.profile.startYear;
-                        i < rowObject.profile.startYear + rowObject.profile.values.length;
-                        i += 1) {
-                        rowObject[(dg4Year + i).toString()] = rowObject.profile.values.map(
-                            (v: number) => Math.round((v + Number.EPSILON) * 10) / 10,
-                        )[j]
-                        j += 1
-                        rowObject.total = Math.round(rowObject.profile.values.map(
-                            (v: number) => (v + Number.EPSILON),
-                        ).reduce((x: number, y: number) => x + y) * 10) / 10
-                    }
-                }
-            }
-
-            const isNotHidden = !rowObject.hideIfEmpty
-            const hasProfileValues = rowObject.hideIfEmpty && rowObject.profile?.values.length > 0
-
-            if (editMode || isNotHidden || hasProfileValues) {
-                tableRows.push(rowObject)
-            }
-        })
-
-        return tableRows
-    }
-
-    const gridRowData = useMemo(() => profilesToRowData(), [timeSeriesData, editMode])
+    const gridRowData = useMemo(() => profilesToRowData(timeSeriesData, dg4Year, tableName, editMode), [timeSeriesData, editMode])
 
     useEffect(() => {
         if (gridRef.current?.api) {
@@ -199,7 +132,7 @@ const CaseTabTable = ({
                 field: "profileName",
                 headerName: tableName,
                 cellRenderer: (params: any) => (
-                    profileAndUnitInSameCell(params, profilesToRowData())
+                    profileAndUnitInSameCell(params, profilesToRowData(timeSeriesData, dg4Year, tableName, editMode))
                 ),
                 width: 250,
                 editable: false,
@@ -258,81 +191,61 @@ const CaseTabTable = ({
 
     const [columnDefs, setColumnDefs] = useState<ColDef[]>(generateTableYearColDefs())
 
-    const stageEdit = (params: any) => {
-        const tableTimeSeriesValues = extractTableTimeSeriesValues(params.data)
-        const existingProfile = params.data.profile ? structuredClone(params.data.profile) : {
-            startYear: 0, values: [],
+    const stageEdit = (tableData: any) => {
+        const rowValues = getValuesFromEntireRow(tableData)
+        const existingProfile = tableData.profile ? structuredClone(tableData.profile) : {
+            startYear: 0,
+            values: [],
+            id: tableData.resourceId,
         }
+
         let newProfile
-        if (tableTimeSeriesValues.length > 0) {
-            const firstYear = tableTimeSeriesValues[0].year
-            const lastYear = tableTimeSeriesValues[tableTimeSeriesValues.length - 1].year
+        if (rowValues.length > 0) {
+            const firstYear = rowValues[0].year
+            const lastYear = rowValues[rowValues.length - 1].year
             const startYear = firstYear - dg4Year
-            newProfile = generateProfile(tableTimeSeriesValues, params.data.profile, startYear, firstYear, lastYear)
+            newProfile = generateProfile(rowValues, tableData.profile, startYear, firstYear, lastYear)
         } else {
             newProfile = structuredClone(existingProfile)
             newProfile.values = []
         }
 
-        if (!newProfile || !caseId) {
-            return
-        }
+        if (!caseId || !newProfile) { return }
 
-        const timeSeriesDataIndex = () => timeSeriesData
-            .map((v, i) => (v.profileName === params.data.profileName ? timeSeriesData[i] : undefined))
-            .find((v) => v !== undefined)
+        const getProfileData = () => timeSeriesData.find(
+            (v) => v.profileName === tableData.profileName,
+        )
+
+        const profileInTimeSeriesData = getProfileData()
 
         if (!isEqual(newProfile.values, existingProfile.values)) {
             setStagedEdit({
-                newDisplayValue: newProfile.values.map((value: string) => Math.floor(Number(value) * 10000) / 10000).join(" - "),
-                previousDisplayValue: existingProfile.values.map((value: string) => Math.floor(Number(value) * 10000) / 10000).join(" - "),
-                inputLabel: params.data.profileName,
+                newDisplayValue: roundToFourDecimalsAndJoin(newProfile.values),
+                previousDisplayValue: roundToFourDecimalsAndJoin(existingProfile.values),
+                inputLabel: tableData.profileName,
                 projectId,
-                resourceName: timeSeriesDataIndex()?.resourceName,
-                resourcePropertyKey: timeSeriesDataIndex()?.resourcePropertyKey,
+                resourceName: profileInTimeSeriesData?.resourceName,
+                resourcePropertyKey: profileInTimeSeriesData?.resourcePropertyKey,
                 caseId,
-                resourceId: timeSeriesDataIndex()?.resourceId,
+                resourceId: profileInTimeSeriesData?.resourceId,
                 newResourceObject: newProfile,
                 previousResourceObject: existingProfile,
-                resourceProfileId: timeSeriesDataIndex()?.resourceProfileId,
+                resourceProfileId: profileInTimeSeriesData?.resourceProfileId,
                 tabName: tab,
                 tableName,
             })
         }
     }
 
-    const handleCellValueChange = useCallback((params: any) => {
-        const rule = TABLE_VALIDATION_RULES[params.data.profileName]
-        if (rule && (params.newValue < rule.min || params.newValue > rule.max)) {
+    const handleCellValueChange = useCallback((event: any) => {
+        const rule = TABLE_VALIDATION_RULES[event.data.profileName]
+        if (rule && (event.newValue < rule.min || event.newValue > rule.max)) {
             setSnackBarMessage(`Value must be between ${rule.min} and ${rule.max}. Please correct the input to save the input.`)
-        } else if (firstTriggerRef.current) {
-            firstTriggerRef.current = false
-            stageEdit(params)
-
-            if (timerRef.current) {
-                clearTimeout(timerRef.current)
-            }
-
-            timerRef.current = setTimeout(() => {
-                firstTriggerRef.current = true
-            }, 0) // TODO: Can this be removed?
+            return
         }
-    }, [stageEdit, timeSeriesData, dg4Year, caseId])
 
-    const gridRefArrayToAlignedGrid = () => {
-        if (alignedGridsRef && alignedGridsRef.length > 0) {
-            const refArray: any[] = []
-            alignedGridsRef.forEach((agr: any) => {
-                if (agr && agr.current) {
-                    refArray.push(agr.current)
-                }
-            })
-            if (refArray.length > 0) {
-                return refArray
-            }
-        }
-        return undefined
-    }
+        stageEdit(event.data)
+    }, [stageEdit])
 
     const defaultColDef = useMemo(() => ({
         sortable: true,
@@ -349,9 +262,9 @@ const CaseTabTable = ({
         setColumnDefs(newColDefs)
     }, [tableYears, editMode, ongoingCalculation])
 
-    const onGridReady = useCallback((params: GridReadyEvent) => {
-        const generateRowData = profilesToRowData()
-        params.api.setGridOption("rowData", generateRowData)
+    const onGridReady = useCallback((gridReadyEvent: GridReadyEvent) => {
+        const generateRowData = profilesToRowData(timeSeriesData, dg4Year, tableName, editMode)
+        gridReadyEvent.api.setGridOption("rowData", generateRowData)
     }, [])
 
     const defaultExcelExportParams = useMemo(() => {
@@ -363,51 +276,6 @@ const CaseTabTable = ({
         }
     }, [tableYears])
 
-    const clearCellsInRange = (start: number, end: number, columns: any[]) => {
-        for (let rowIndex = start; rowIndex <= end; rowIndex += 1) {
-            const rowNode = gridRef.current?.api.getRowNode(rowIndex)
-            if (rowNode) {
-                columns.forEach((col: any) => {
-                    rowNode.setDataValue(col, "")
-                })
-            }
-        }
-    }
-
-    const handleDeleteOnRange = useCallback(
-        (e: CellKeyDownEvent) => {
-            const keyboardEvent = e.event as unknown as KeyboardEvent
-            const { key } = keyboardEvent
-
-            const cellRanges = e.api.getCellRanges()
-
-            if (key === "Backspace") {
-                if (!cellRanges || cellRanges.length === 0) {
-                    return
-                }
-
-                cellRanges.forEach((range: any) => {
-                    const { startRow, endRow, columns } = range
-                    const startRowIndex = Math.min(startRow.rowIndex, endRow.rowIndex)
-                    const endRowIndex = Math.max(startRow.rowIndex, endRow.rowIndex)
-
-                    const colIds = columns.map((col: any) => col.getColId())
-
-                    if (startRowIndex === endRowIndex && colIds.length === 1) {
-                        const colId = colIds[0]
-                        const cellValue = e.api.getValue(colId, e.node)
-                        if (cellValue !== undefined && cellValue !== null && typeof cellValue === "string") {
-                            const newValue = cellValue.slice(0, -1)
-                            e.node.setDataValue(colId, newValue)
-                        }
-                    } else {
-                        clearCellsInRange(startRowIndex, endRowIndex, colIds)
-                    }
-                })
-            }
-        },
-        [clearCellsInRange],
-    )
     return (
         <div className={styles.root}>
             <div
@@ -433,14 +301,13 @@ const CaseTabTable = ({
                     cellSelection
                     suppressMovableColumns
                     enableCharts
-                    alignedGrids={gridRefArrayToAlignedGrid()}
+                    alignedGrids={alignedGridsRef ? gridRefArrayToAlignedGrid(alignedGridsRef) : undefined}
                     grandTotalRow={includeFooter ? "bottom" : undefined}
                     getRowStyle={getCaseRowStyle}
                     suppressLastEmptyLineOnPaste
                     stopEditingWhenCellsLoseFocus
                     onGridReady={onGridReady}
                     defaultExcelExportParams={defaultExcelExportParams}
-                    onCellKeyDown={editMode ? handleDeleteOnRange : undefined}
                 />
             </div>
         </div>
