1baffc458cb196ed6123ab738a9e339a046899aa
JonasABach <jobac@equinor.com>
2025-01-15 10:32:48 +0100

    Update types.d.ts

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1988,21 +1988,6 @@ declare namespace Paths {
             }
         }
     }
-    namespace Projects$ProjectIdCases$CaseIdCo2intensity {
-        namespace Get {
-            namespace Parameters {
-                export type CaseId = string; // uuid
-                export type ProjectId = string; // uuid
-            }
-            export interface PathParameters {
-                projectId: Parameters.ProjectId /* uuid */;
-                caseId: Parameters.CaseId /* uuid */;
-            }
-            namespace Responses {
-                export type $200 = Components.Schemas.Co2IntensityDto;
-            }
-        }
-    }
     namespace Projects$ProjectIdCases$CaseIdDrainageStrategies$DrainageStrategyId {
         namespace Put {
             namespace Parameters {
