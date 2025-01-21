950c679c1c97e33ed1edf78c768114bbe89e534f
Frank Kvam <frank.kvam@gmail.com>
2025-01-21 11:49:22 +0100

    Actually recalculate wells and drilling schedules in background job (#1470)
    * Actually recalculate wells and drilling schedules in background job
    
    * exploration wells
    
    * Remove one way to navigate and recalculate wells

--- backend/api/Features/Cases/Recalculation/Types/WellCostProfile/WellCostProfileService.cs
+++ backend/api/Features/Cases/Recalculation/Types/WellCostProfile/WellCostProfileService.cs
@@ -24,6 +24,45 @@ public class WellCostProfileService(DcdDbContext context)
         await UpdateExplorationCostProfiles(explorationWells.ToList());
     }
 
+    public async Task UpdateCostProfilesForWells(Guid caseId)
+    {
+        var wellIds = new List<Guid>();
+
+        wellIds.AddRange(await context.Cases
+            .Where(w => w.Id == caseId)
+            .SelectMany(x => x.WellProject!.WellProjectWells)
+            .Select(x => x.WellId)
+            .ToListAsync());
+
+        wellIds.AddRange(await context.Cases
+            .Where(w => w.Id == caseId)
+            .SelectMany(x => x.Exploration!.ExplorationWells)
+            .Select(x => x.WellId)
+            .ToListAsync());
+
+        var explorationWells = await context.ExplorationWell
+            .Include(ew => ew.DrillingSchedule)
+            .Include(ew => ew.Well)
+            .Include(ew => ew.Exploration).ThenInclude(e => e.ExplorationWellCostProfile)
+            .Include(ew => ew.Exploration).ThenInclude(e => e.AppraisalWellCostProfile)
+            .Include(ew => ew.Exploration).ThenInclude(e => e.SidetrackCostProfile)
+            .Where(x => wellIds.Contains(x.WellId))
+            .ToListAsync();
+
+        var wellProjectWells = await context.WellProjectWell
+            .Include(ew => ew.DrillingSchedule)
+            .Include(ew => ew.Well)
+            .Include(ew => ew.WellProject).ThenInclude(e => e.OilProducerCostProfile)
+            .Include(ew => ew.WellProject).ThenInclude(e => e.GasProducerCostProfile)
+            .Include(ew => ew.WellProject).ThenInclude(e => e.WaterInjectorCostProfile)
+            .Include(ew => ew.WellProject).ThenInclude(e => e.GasInjectorCostProfile)
+            .Where(x => wellIds.Contains(x.WellId))
+            .ToListAsync();
+
+        await UpdateExplorationCostProfiles(explorationWells);
+        await UpdateWellProjectCostProfiles(wellProjectWells);
+    }
+
     public async Task UpdateCostProfilesForWells(List<Well> wells)
     {
         if (wells.Count == 0)
