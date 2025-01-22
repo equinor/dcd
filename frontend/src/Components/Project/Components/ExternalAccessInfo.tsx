04895601a7859876710077508a8253e3c387ad0f
Trym Helle <112863201+H-Trym@users.noreply.github.com>
2025-01-22 16:26:02 +0100

    removed accessIT infotext from access management and warning screen (#1481)

--- frontend/src/Components/Project/Components/ExternalAccessInfo.tsx
+++ frontend/src/Components/Project/Components/ExternalAccessInfo.tsx
@@ -70,41 +70,6 @@ const ExternalAccessInfo = () => {
                                 </Typography>
                                 <Icon color="#007079" size={16} data={external_link} />
                             </EditorViewerHeading>
-                            <Grid item marginTop="50px" display="flex" flexDirection="column" gap="15px">
-                                <Typography variant="body_short">
-                                    The following AccessIT groups also have view access in the app:
-                                </Typography>
-                                <EditorViewerHeading $smallGap>
-                                    <Typography
-                                        link
-                                        href="https://accessit.equinor.com/Search/Search?term=Chief+Engineers+%28FUSION%29"
-                                        target="_blank"
-                                    >
-                                        Chief Engineers (FUSION)
-                                    </Typography>
-                                    <Icon color="#007079" size={16} data={external_link} />
-                                </EditorViewerHeading>
-                                <EditorViewerHeading $smallGap>
-                                    <Typography
-                                        link
-                                        href="https://accessit.equinor.com/Search/Search?term=Leading+Advisors+Project+Management+%26+Control+%28FUSION%29"
-                                        target="_blank"
-                                    >
-                                        Leading Advisors Project Management & Control (FUSION)
-                                    </Typography>
-                                    <Icon color="#007079" size={16} data={external_link} />
-                                </EditorViewerHeading>
-                                <EditorViewerHeading $smallGap>
-                                    <Typography
-                                        link
-                                        href="https://accessit.equinor.com/Search/Search?term=Project+Development+Center+-+Management+%28FUSION%29"
-                                        target="_blank"
-                                    >
-                                        Project Development Center - Management (FUSION)
-                                    </Typography>
-                                    <Icon color="#007079" size={16} data={external_link} />
-                                </EditorViewerHeading>
-                            </Grid>
                         </Grid>
                     </Grid>
                 )}
