42692dd31d568750496eb09f650b9b1e96294c98
kvam <frank.kvam@gmail.com>
2025-01-29 15:21:52 +0100

    Disable lazy loading in case with assets

--- backend/api/Features/Cases/GetWithAssets/CaseWithAssetsController.cs
+++ backend/api/Features/Cases/GetWithAssets/CaseWithAssetsController.cs
@@ -9,6 +9,7 @@ public class CaseWithAssetsController(CaseWithAssetsService caseWithAssetsServic
 {
     [HttpGet("projects/{projectId:guid}/cases/{caseId:guid}/case-with-assets")]
     [AuthorizeActionType(ActionType.Read)]
+    [DisableLazyLoading]
     public async Task<CaseWithAssetsDto> GetCaseWithAssets([FromRoute] Guid projectId, [FromRoute] Guid caseId)
     {
         return await caseWithAssetsService.GetCaseWithAssetsNoTracking(projectId, caseId);
