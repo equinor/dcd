6a891f6f75efbcba62cbbdb103a75be9f0c1229b
Tord Austad <toaus@equinor.com>
2025-02-06 15:14:45 +0100

    feat: connect the date data to the front end

--- backend/api/Features/Cases/GetWithAssets/Dtos/AssetDtos/ExplorationWellDto.cs
+++ backend/api/Features/Cases/GetWithAssets/Dtos/AssetDtos/ExplorationWellDto.cs
@@ -9,4 +9,5 @@ public class ExplorationWellDto
     [Required] public required TimeSeriesScheduleDto DrillingSchedule { get; set; }
     [Required] public required Guid ExplorationId { get; set; }
     [Required] public required Guid WellId { get; set; }
+    [Required] public DateTime UpdatedUtc { get; set; }
 }
