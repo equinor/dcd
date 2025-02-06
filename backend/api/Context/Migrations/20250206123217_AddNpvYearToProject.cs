50f9b09162e16950c522e3bc29f0e2e5e832410b
kvam <frank.kvam@gmail.com>
2025-02-06 13:32:32 +0100

    Add migration

---
+++ backend/api/Context/Migrations/20250206123217_AddNpvYearToProject.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNpvYearToProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NpvYear",
+                table: "Projects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NpvYear",
+                table: "Projects");
+        }
+    }
+}
