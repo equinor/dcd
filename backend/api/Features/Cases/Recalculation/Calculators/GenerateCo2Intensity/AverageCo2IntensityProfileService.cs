d0f930aa8643fec1486fe32e401337d35ffe7761
kvam <frank.kvam@gmail.com>
2025-02-14 10:11:44 +0100

    Move nullhandling out of calculation

--- backend/api/Features/Cases/Recalculation/Calculators/GenerateCo2Intensity/AverageCo2IntensityProfileService.cs
+++ backend/api/Features/Cases/Recalculation/Calculators/GenerateCo2Intensity/AverageCo2IntensityProfileService.cs
@@ -9,12 +9,12 @@ public static class AverageCo2IntensityProfileService
     {
         var co2IntensityData = caseItem.GetProfileOrNull(ProfileTypes.Co2Intensity);
         var oilProductionSum = Co2IntensityProfileService.GetOilProfile(caseItem).Values.Sum();
-        var netSalesGasSum = caseItem.GetProfileOrNull(ProfileTypes.NetSalesGas)?.Values.Sum() / 1_000_000_000;
+        var netSalesGasSum = caseItem.GetProfileOrNull(ProfileTypes.NetSalesGas)?.Values.Sum() / 1_000_000_000 ?? 0;
         var co2EmissionsProfile = Co2IntensityProfileService.GetCo2EmissionsProfile(caseItem).Values.Sum() / 1_000_000;
 
         if (co2IntensityData != null && co2IntensityData.Values.Any())
         {
-            var denominator = (oilProductionSum + (netSalesGasSum ?? 0)) * 6.29;
+            var denominator = (oilProductionSum + netSalesGasSum) * 6.29;
 
             if (denominator > 0)
             {
