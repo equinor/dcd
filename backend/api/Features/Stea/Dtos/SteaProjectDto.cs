8825bc7727a2e215c52c01c73f0e6de993abe63d
kvam <frank.kvam@gmail.com>
2025-03-03 12:40:25 +0100

    Improve export to excel

--- backend/api/Features/Stea/Dtos/SteaProjectDto.cs
+++
@@ -1,8 +0,0 @@
-namespace api.Features.Stea.Dtos;
-
-public class SteaProjectDto
-{
-    public required string Name { get; set; }
-    public required int StartYear { get; set; }
-    public required List<SteaCaseDto> SteaCases { get; set; }
-}
--- backend/api/Features/Stea/SteaService.cs
+++ backend/api/Features/Stea/SteaService.cs
@@ -9,28 +9,13 @@ public class SteaService(DcdDbContext context, SteaRepository steaRepository)
 {
     public async Task<(byte[] excelFileContents, string filename)> GetExcelFile(Guid projectId)
     {
-        var project = await GetInputToStea(projectId);
-        var businessCases = ExportToExcelService.CreateExcelCells(project);
-        var bytes = ExportToExcelService.WriteExcelCellsToExcelDocument(businessCases, project.Name);
-
-        return (bytes, project.Name + "ExportToSTEA.xlsx");
-    }
-
-    private async Task<SteaProjectDto> GetInputToStea(Guid projectId)
-    {
         var projectPk = await context.GetPrimaryKeyForProjectIdOrRevisionId(projectId);
-
         var project = await steaRepository.GetProjectWithCasesAndProfiles(projectPk);
 
         var steaCaseDtos = project.Cases.Select(SteaCaseDtoBuilder.Build).ToList();
 
-        var startYears = steaCaseDtos.Where(x => x.StartYear > 0).Select(x => x.StartYear).ToList();
+        var bytes = ExportToExcelService.ExportToExcel(project.Name, steaCaseDtos);
 
-        return new SteaProjectDto
-        {
-            Name = project.Name,
-            SteaCases = steaCaseDtos,
-            StartYear = startYears.Any() ? startYears.Min() : 0
-        };
+        return (bytes, project.Name + "ExportToSTEA.xlsx");
     }
 }
