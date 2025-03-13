77dc33465663fca161fe4028e105cffc3e142ead
JonasTreD <136380432+JonasTreD@users.noreply.github.com>
2025-03-13 11:34:19 +0100

    Fix: forntend expects a 4 image limit, do not allow more images to be added backend

--- backend/api/Features/Images/Upload/UploadImageValidator.cs
+++ backend/api/Features/Images/Upload/UploadImageValidator.cs
@@ -5,6 +5,7 @@ namespace api.Features.Images.Upload;
 public static class UploadImageValidator
 {
     private const int MaxFileSize = 5 * 1024 * 1024; // 5MB
+    private const int MaxImageCount = 4;
     private static readonly string[] PermittedExtensions = [".jpg", ".jpeg", ".png", ".gif"];
 
     public static void EnsureIsValid(IFormFile image)
@@ -24,6 +25,14 @@ public static class UploadImageValidator
         if (string.IsNullOrEmpty(ext) || !PermittedExtensions.Contains(ext))
         {
             throw new UnprocessableContentException($"File {image.FileName} has an invalid extension. Only image files are allowed.");
+        }
+    }
+
+    public static void EnsureImageLimit(int imagesCount)
+    {
+        if (imagesCount >= MaxImageCount)
+        {
+            throw new UnprocessableContentException($"Maximum number of images allowed is {MaxImageCount}.");
         }
     }
 }
