19ee627ea88ef0048cd1c45938f3fe03cf889436
kvam <frank.kvam@gmail.com>
2025-03-24 16:37:42 +0100

    Remove Co2 columns from project

---
+++ backend/api/Context/Migrations/20250324153711_RemoveCo2FromProject.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCo2FromProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageDevelopmentDrillingDays",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "Co2EmissionFromFuelGas",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "Co2EmissionsFromFlaredGas",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "Co2RemovedFromGas",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "Co2Vented",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "DailyEmissionFromDrillingRig",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "FlaredGasPerProducedVolume",
+                table: "Projects");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "AverageDevelopmentDrillingDays",
+                table: "Projects",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Co2EmissionFromFuelGas",
+                table: "Projects",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Co2EmissionsFromFlaredGas",
+                table: "Projects",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Co2RemovedFromGas",
+                table: "Projects",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Co2Vented",
+                table: "Projects",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "DailyEmissionFromDrillingRig",
+                table: "Projects",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "FlaredGasPerProducedVolume",
+                table: "Projects",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
